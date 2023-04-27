05c5bd8d6c3384a33fb50627849ef20191fad2f1
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-28 06:58:02 +0800

    init.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            }
+        ],
+    },
+    performance: {
+        hints: false,
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+};
