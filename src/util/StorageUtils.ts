cf189ccdc49a52a8c8f89f65ee0458515576bb6a
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-28 14:34:54 +0800

    StorageUtils

---
+++ src/util/StorageUtils.ts
@@ -0,0 +1,51 @@
+class StorageUtils {
+
+    static set(key: string, value: string) {
+        localStorage.setItem(key, value);
+    }
+
+    static get(key: string): string | null {
+        return localStorage.getItem(key);
+    }
+
+    static remove(key: string) {
+        localStorage.removeItem(key);
+    }
+
+    static getString(key: string) {
+        const value = StorageUtils.get(key);
+        if (value === undefined ||
+            value === null ||
+            value === "undefined" ||
+            value === "null") {
+            return "";
+        }
+        return value;
+    }
+
+    static getBoolean(key: string): boolean {
+        const value = StorageUtils.getString(key);
+        if (value === "") {
+            return false;
+        }
+        return value !== "0";
+    }
+
+    getInt(key: string, defaultValue: number): number {
+        const value = StorageUtils.getString(key);
+        if (value === "") {
+            return defaultValue;
+        }
+        return parseInt(value);
+    }
+
+    getFloat(key: string, defaultValue: number): number {
+        const value = StorageUtils.getString(key);
+        if (value === "") {
+            return defaultValue;
+        }
+        return parseFloat(value);
+    }
+}
+
+export = StorageUtils;
