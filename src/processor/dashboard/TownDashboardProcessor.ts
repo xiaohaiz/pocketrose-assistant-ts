db4fa898deebf5af4ae0376a09bfcbda9d207c4b
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-28 20:05:42 +0800

    战斗相关菜单的重组及按键辅助功能。

--- src/processor/dashboard/TownDashboardProcessor.ts
+++ src/processor/dashboard/TownDashboardProcessor.ts
@@ -1,5 +1,7 @@
 import PocketroseProcessor from "../PocketroseProcessor";
 import PageUtils from "../../util/PageUtils";
+import Credential from "../../util/Credential";
+import SetupLoader from "../../pocket/SetupLoader";
 
 class TownDashboardProcessor extends PocketroseProcessor {
 
@@ -7,8 +9,138 @@ class TownDashboardProcessor extends PocketroseProcessor {
         PageUtils.removeUnusedHyperLinks();
         PageUtils.removeGoogleAnalyticsScript();
 
+        $("input:text:first").attr("id", "messageInputText");
+        const credential = PageUtils.currentCredential();
+
+        doRenderBattleMenu(credential);
         doRenderMenu();
     }
+
+}
+
+function doRenderBattleMenu(credential: Credential) {
+    const preference = SetupLoader.getBattlePlacePreference(credential.id);
+    let count = 0;
+    // @ts-ignore
+    if (preference["primary"]) {
+        count++;
+    }
+    // @ts-ignore
+    if (preference["junior"]) {
+        count++;
+    }
+    // @ts-ignore
+    if (preference["senior"]) {
+        count++;
+    }
+    // @ts-ignore
+    if (preference["zodiac"]) {
+        count++;
+    }
+    if (count === 0) {
+        // 没有设置战斗场所偏好，忽略
+        return;
+    }
+
+    // 设置了战斗场所偏好
+    $("select[name='level']").find("option").each(function (_idx, option) {
+        const text = $(option).text();
+        if (text.startsWith("秘宝之岛")) {
+            // do nothing, keep
+        } else if (text.startsWith("初级之森")) {
+            // do nothing, keep
+        } else if (text.startsWith("中级之塔")) {
+            // do nothing, keep
+        } else if (text.startsWith("上级之洞")) {
+            // do nothing, keep
+        } else if (text.startsWith("十二神殿")) {
+            // do nothing, keep
+        } else if (text.startsWith("------")) {
+            // do nothing, keep
+        } else {
+            $(option).remove();
+        }
+    });
+    $("select[name='level']").find("option").each(function (_idx, option) {
+        const text = $(option).text();
+        if (text.startsWith("初级之森")) {
+            // @ts-ignore
+            if (!preference["primary"]) {
+                $(option).remove();
+            }
+        } else if (text.startsWith("中级之塔")) {
+            // @ts-ignore
+            if (!preference["junior"]) {
+                $(option).remove();
+            }
+        } else if (text.startsWith("上级之洞")) {
+            // @ts-ignore
+            if (!preference["senior"]) {
+                $(option).remove();
+            }
+        } else if (text.startsWith("十二神殿")) {
+            // @ts-ignore
+            if (!preference["zodiac"]) {
+                $(option).remove();
+            }
+        }
+    });
+    // 删除连续的分隔线
+    let delimMatch = false;
+    $("select[name='level']").find("option").each(function (_idx, option) {
+        const text = $(option).text();
+        if (text.startsWith("------")) {
+            if (!delimMatch) {
+                delimMatch = true;
+            } else {
+                $(option).remove();
+            }
+        } else {
+            delimMatch = false;
+        }
+    });
+    // 删除头尾的分隔线
+    if ($("select[name='level']").find("option:last").text().startsWith("------")) {
+        $("select[name='level']").find("option:last").remove();
+    }
+    if ($("select[name='level']").find("option:first").text().startsWith("------")) {
+        $("select[name='level']").find("option:first").remove();
+    }
+
+    if (count === 1) {
+        // 只设置了一处战斗场所偏好
+        let formBattle = $("form[action='battle.cgi']");
+        let selectBattle = formBattle.find('select[name="level"]');
+        let btnBattle = formBattle.parent().next().find('input');
+        let inputDigits = '';
+        $(document).off('keydown.city').on('keydown.city', function (e) {
+            if ($("#messageInputText:focus").length > 0) {
+                // 当前的焦点在消息框，禁用按键辅助
+                return;
+            }
+            const key = e.key;
+            if (key !== undefined && !isNaN(parseInt(key))) {
+                inputDigits += key;
+            }
+            if (inputDigits.length === 2) {
+                switch (inputDigits) {
+                    case '11':
+                        selectBattle.find('option').eq(0).prop('selected', true);
+                        break;
+                    case '22':
+                        selectBattle.find('option').eq(1).prop('selected', true);
+                        break;
+                    default:
+                        inputDigits = '';
+                        break;
+                }
+                btnBattle.focus();
+                // 重置 inputDigits
+                inputDigits = '';
+            }
+        });
+    }
+
 
 }
 
