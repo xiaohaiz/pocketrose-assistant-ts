842a74d32f959183023bd4aa27267d996a35a399
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-28 16:27:47 +0800

    临时增加一些方便的按钮。

--- src/processor/dashboard/TownDashboardProcessor.ts
+++ src/processor/dashboard/TownDashboardProcessor.ts
@@ -7,11 +7,39 @@ class TownDashboardProcessor extends PocketroseProcessor {
         PageUtils.removeUnusedHyperLinks();
         PageUtils.removeGoogleAnalyticsScript();
 
-        $("option[value='LETTER']").text("口袋助手设置");
-        $("option[value='LETTER']").css("background-color", "yellow");
+        doRenderMenu();
     }
 
 }
 
+function doRenderMenu() {
+    $("option[value='LETTER']").text("口袋助手设置");
+    $("option[value='LETTER']").css("background-color", "yellow");
+
+    const th = $("th:contains('看不到图片按这里')")
+        .filter(function () {
+            return $(this).text() === "看不到图片按这里";
+        });
+
+    let html = $(th).html();
+    html += "<input type='button' value='图片'>";
+    html += "<input type='button' id='setupButton' value='助手'>";
+    html += "<input type='button' id='equipmentButton' value='装备'>";
+    html += "<input type='button' id='petButton' value='宠物'>";
+    $(th).html(html);
+
+    $("#setupButton").on("click", function () {
+        $("option[value='LETTER']").prop("selected", true);
+        $("option[value='LETTER']").closest("td").next().find("input:submit:first").trigger("click");
+    });
+    $("#equipmentButton").on("click", function () {
+        $("option[value='USE_ITEM']").prop("selected", true);
+        $("option[value='USE_ITEM']").closest("td").next().find("input:submit:first").trigger("click");
+    });
+    $("#petButton").on("click", function () {
+        $("option[value='PETSTATUS']").prop("selected", true);
+        $("option[value='PETSTATUS']").closest("td").next().find("input:submit:first").trigger("click");
+    });
+}
 
 export = TownDashboardProcessor;
