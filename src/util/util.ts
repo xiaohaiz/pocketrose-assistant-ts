c545f0df6953ab0854efeec610865afa1933a6bc
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-28 08:03:40 +0800

    Rename refactorings.

--- src/util/util.ts
+++
@@ -1,7 +0,0 @@
-export function substringAfterLast(text: string, searchString: string): string {
-    const idx = text.lastIndexOf(searchString);
-    if (idx === -1) {
-        return text;
-    }
-    return text.substring(idx + searchString.length);
-}
