458d76bc0d74684a384fbed0bcf687952ecf2f12
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-28 09:08:18 +0800

    TownRequestInterceptor

--- src/interceptor/StatusRequestInterceptor.ts
+++ src/interceptor/StatusRequestInterceptor.ts
@@ -1,3 +1,6 @@
+import PageUtils from "../util/PageUtils";
+import TownDashboardProcessor from "../processor/dashboard/TownDashboardProcessor";
+
 export = StatusRequestInterceptor;
 
 class StatusRequestInterceptor implements RequestInterceptor {
@@ -5,6 +8,11 @@ class StatusRequestInterceptor implements RequestInterceptor {
     readonly cgi: string = "status.cgi";
 
     process(): void {
+        const pageHtml = PageUtils.currentPageHtml();
+        const pageText = PageUtils.currentPageText();
+        if (pageText.includes("城市支配率")) {
+            new TownDashboardProcessor(pageHtml, pageText).process();
+        }
     }
 
 }
--- src/interceptor/TownRequestInterceptor.ts
+++ src/interceptor/TownRequestInterceptor.ts
@@ -1,3 +1,6 @@
+import PageUtils from "../util/PageUtils";
+import TownDashboardProcessor from "../processor/dashboard/TownDashboardProcessor";
+
 export = TownRequestInterceptor;
 
 class TownRequestInterceptor implements RequestInterceptor {
@@ -5,6 +8,11 @@ class TownRequestInterceptor implements RequestInterceptor {
     readonly cgi: string = "town.cgi";
 
     process(): void {
+        const pageHtml = PageUtils.currentPageHtml();
+        const pageText = PageUtils.currentPageText();
+        if (pageText.includes("城市支配率")) {
+            new TownDashboardProcessor(pageHtml, pageText).process();
+        }
     }
 
 
