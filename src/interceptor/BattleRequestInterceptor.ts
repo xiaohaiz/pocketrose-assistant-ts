950e14c6c543e98039266a159acafbd43fb2822c
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-28 18:32:31 +0800

    BattleProcessor

--- src/interceptor/BattleRequestInterceptor.ts
+++ src/interceptor/BattleRequestInterceptor.ts
@@ -1,8 +1,22 @@
+import PageUtils from "../util/PageUtils";
+import BattleProcessor from "../processor/battle/BattleProcessor";
+
 class BattleRequestInterceptor implements RequestInterceptor {
 
     readonly cgi: string = "/battle.cgi";
 
     process(): void {
+        const pageHtml = PageUtils.currentPageHtml();
+        const pageText = PageUtils.currentPageText()
+
+        if (pageText.includes("＜＜ - 秘宝之岛 - ＞＞") ||
+            pageText.includes("＜＜ - 初级之森 - ＞＞") ||
+            pageText.includes("＜＜ - 中级之塔 - ＞＞") ||
+            pageText.includes("＜＜ - 上级之洞窟 - ＞＞") ||
+            pageText.includes("＜＜ - 十二神殿 - ＞＞")) {
+            $('a[target="_blank"]').attr('tabIndex', -1);
+            new BattleProcessor(pageHtml, pageText).process();
+        }
     }
 
 }
---
+++ src/processor/battle/BattleProcessor.ts
@@ -0,0 +1,11 @@
+import PocketroseProcessor from "../PocketroseProcessor";
+
+class BattleProcessor extends PocketroseProcessor {
+
+
+    process(): void {
+
+    }
+}
+
+export = BattleProcessor;
