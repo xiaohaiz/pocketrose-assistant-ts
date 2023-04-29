3302be2938fa11dfefb4e187740c7ca726988ef4
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-29 11:14:35 +0800

    NetworkUtils.sendGetRequest

--- src/util/NetworkUtils.ts
+++ src/util/NetworkUtils.ts
@@ -1,5 +1,25 @@
 class NetworkUtils {
 
+    static sendGetRequest(cgi: string, handler?: (html: string) => void) {
+        fetch(cgi, {method: "GET"})
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("RESPONSE was not ok");
+                }
+                return response.arrayBuffer();
+            })
+            .then((arrayBuffer) => {
+                const decoder = new TextDecoder("gb2312");
+                const html = decoder.decode(new Uint8Array(arrayBuffer));
+                if (handler !== undefined) {
+                    handler(html);
+                }
+            })
+            .catch((error) => {
+                console.error("Error raised:", error);
+            });
+    }
+
     static sendPostRequest(cgi: string, request: {}, handler?: (html: string) => void) {
         fetch(cgi, {
             method: "POST",
