15ba261ea7730a248aab6f7b8f5e0fe3d46e7c09
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-29 12:38:56 +0800

    CastleStatusRequestInterceptor

---
+++ src/interceptor/internal/CastleStatusRequestInterceptor.ts
@@ -0,0 +1,10 @@
+class CastleStatusRequestInterceptor implements RequestInterceptor {
+
+    readonly cgi: string = "castlestatus.cgi";
+
+    process(): void {
+    }
+
+}
+
+export = CastleStatusRequestInterceptor;
