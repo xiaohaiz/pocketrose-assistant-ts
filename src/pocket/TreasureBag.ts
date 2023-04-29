b9740f6b0c7550fd59d86890be7d73ff89ce2986
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-29 19:04:53 +0800

    EquipmentSetLoader

---
+++ src/pocket/TreasureBag.ts
@@ -0,0 +1,61 @@
+import Credential from "../util/Credential";
+import Equipment from "./Equipment";
+import NetworkUtils from "../util/NetworkUtils";
+import EquipmentParser from "./EquipmentParser";
+import MessageBoard from "../util/MessageBoard";
+
+class TreasureBag {
+
+    readonly #credential: Credential;
+    readonly #treasureBagIndex: number;
+
+    constructor(credential: Credential, treasureBagIndex: number) {
+        this.#credential = credential;
+        this.#treasureBagIndex = treasureBagIndex;
+    }
+
+    async open(): Promise<Equipment[]> {
+        const action = (credential: Credential, treasureBagIndex: number) => {
+            return new Promise<Equipment[]>(resolve => {
+                const request = credential.asRequest();
+                // @ts-ignore
+                request["chara"] = "1";
+                // @ts-ignore
+                request["item" + treasureBagIndex] = treasureBagIndex;
+                // @ts-ignore
+                request["mode"] = "USE";
+                NetworkUtils.sendPostRequest("mydata.cgi", request, function (html) {
+                    const equipmentList = EquipmentParser.parseTreasureBagItemList(html);
+                    MessageBoard.publishMessage("在百宝袋中检索到" + equipmentList.length + "件装备。")
+                    resolve(equipmentList);
+                });
+            });
+        };
+        return await action(this.#credential, this.#treasureBagIndex);
+    }
+
+    async takeOut(indexList: number[]): Promise<void> {
+        const action = (credential: Credential) => {
+            return new Promise<void>(resolve => {
+                if (indexList.length === 0) {
+                    resolve();
+                } else {
+                    const request = credential.asRequest();
+                    // @ts-ignore
+                    request["mode"] = "GETOUTBAG";
+                    for (const index of indexList) {
+                        // @ts-ignore
+                        request["item" + index] = index;
+                    }
+                    NetworkUtils.sendPostRequest("mydata.cgi", request, function (html) {
+                        MessageBoard.processResponseMessage(html);
+                        resolve();
+                    });
+                }
+            });
+        };
+        return await action(this.#credential);
+    }
+}
+
+export = TreasureBag;
