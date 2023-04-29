7ffc64f153b9cc09371279c59c102995ec68f0ef
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-29 16:50:49 +0800

    初步渲染宠物管理界面。

--- src/processor/personal/PersonalPetManagementProcessor.ts
+++ src/processor/personal/PersonalPetManagementProcessor.ts
@@ -55,6 +55,131 @@ function doProcess(credential: Credential, petList: Pet[], studyStatus: number[]
             MessageBoard.createMessageBoard("message_board_container", imageHtml);
             MessageBoard.resetMessageBoard("全新的宠物管理UI为您带来不一样的感受。");
         });
+
+    doRender(petList, studyStatus);
+}
+
+function doRender(petList: Pet[], studyStatus: number[]) {
+    let html = "";
+    html += "<table style='border-width:0;background-color:#888888;text-align:center;width:100%'>";
+    html += "<tbody style='background-color:#F8F0E0'>";
+    html += "<tr>";
+    html += "<td style='background-color:#EFE0C0'></td>";
+    html += "<td style='background-color:#EFE0C0'>使用</td>";
+    html += "<td style='background-color:#E8E8D0'>宠物名</td>";
+    html += "<td style='background-color:#E8E8D0'>性别</td>";
+    html += "<td style='background-color:#E8E8D0'>Ｌｖ</td>";
+    html += "<td style='background-color:#E8E8D0'>ＨＰ</td>";
+    html += "<td style='background-color:#E8E8D0'>攻击力</td>";
+    html += "<td style='background-color:#E8E8D0'>防御力</td>";
+    html += "<td style='background-color:#E8E8D0'>智力</td>";
+    html += "<td style='background-color:#E8E8D0'>精神力</td>";
+    html += "<td style='background-color:#E8E8D0'>速度</td>";
+    html += "<td style='background-color:#E8E8D0'>技1</td>";
+    html += "<td style='background-color:#E8E8D0'>技2</td>";
+    html += "<td style='background-color:#E8E8D0'>技3</td>";
+    html += "<td style='background-color:#E8E8D0'>技4</td>";
+    html += "<td style='background-color:#E8E8D0'>亲密度</td>";
+    html += "<td style='background-color:#E8E8D0'>种类</td>";
+    html += "<td style='background-color:#E8E8D0'>属性1</td>";
+    html += "<td style='background-color:#E8E8D0'>属性2</td>";
+    html += "</tr>";
+    for (const pet of petList) {
+        html += "<tr>";
+        html += "<td style='background-color:#EFE0C0' rowspan='2'>" +
+            pet.imageHTML +
+            "</td>";
+        html += "<td style='background-color:#EFE0C0' rowspan='2'>" +
+            (pet.using ? "★" : "") +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            "<b>" + pet.name + "</b>" +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            pet.gender +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            pet.level +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            pet.health + "/" + pet.maxHealth +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            (pet.attack! >= 375 ? "<b style='color:red'>" + pet.attack + "</b>" : pet.attack) +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            (pet.defense! >= 375 ? "<b style='color:red'>" + pet.defense + "</b>" : pet.defense) +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            (pet.specialAttack! >= 375 ? "<b style='color:red'>" + pet.specialAttack + "</b>" : pet.specialAttack) +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            (pet.specialDefense! >= 375 ? "<b style='color:red'>" + pet.specialDefense + "</b>" : pet.specialDefense) +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            (pet.speed! >= 375 ? "<b style='color:red'>" + pet.speed + "</b>" : pet.speed) +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            "<input type='button' class='PetUIButton' id='pet_" + pet.index + "_spell_1' value='" + pet.spell1 + "' title='" + pet.spell1Description + "'>" +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            "<input type='button' class='PetUIButton' id='pet_" + pet.index + "_spell_2' value='" + pet.spell2 + "' title='" + pet.spell2Description + "'>" +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            "<input type='button' class='PetUIButton' id='pet_" + pet.index + "_spell_3' value='" + pet.spell3 + "' title='" + pet.spell3Description + "'>" +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            "<input type='button' class='PetUIButton' id='pet_" + pet.index + "_spell_4' value='" + pet.spell4 + "' title='" + pet.spell4Description + "'>" +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            (pet.love! >= 100 ? "<b style='color:red'>" + pet.love + "</b>" : pet.love) +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            pet.race +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            pet.attribute1 +
+            "</td>";
+        html += "<td style='background-color:#E8E8D0'>" +
+            (pet.attribute2 === "无" ? "-" : pet.attribute2) +
+            "</td>";
+        html += "</tr>";
+        html += "<tr>";
+        html += "<td colspan='17' style='text-align: left'>";        // 当前宠物的操作位置
+        html += "<input type='button' class='PetUIButton' value='卸下' id='pet_" + pet.index + "_uninstall'>";
+        html += "<input type='button' class='PetUIButton' value='使用' id='pet_" + pet.index + "_install'>";
+        html += "<input type='button' class='PetUIButton' value='入笼' id='pet_" + pet.index + "_cage'>";
+        html += "<input type='button' class='PetUIButton' value='亲密' id='pet_" + pet.index + "_love'>";
+        html += "<input type='button' class='PetUIButton' value='参赛' id='pet_" + pet.index + "_league'>";
+        html += "<input type='button' class='PetUIButton' value='献祭' id='pet_" + pet.index + "_consecrate'>";
+        html += "<input type='button' class='PetUIButton' value='发送' id='pet_" + pet.index + "_send'>";
+        html += "<input type='button' class='PetUIButton' value='改名' id='pet_" + pet.index + "_rename'>&nbsp;";
+        html += "<input type='text' id='pet_" + pet.index + "_name_text' size='15' maxlength='20'>";
+        html += "</td>";
+        html += "</tr>";
+    }
+    html += "<tr><td style='background-color:#EFE0C0;text-align:right' colspan='19'>";
+    html += "<input type='text' id='receiverName' size='15' maxlength='20'>";
+    html += "<input type='button' class='PetUIButton' id='searchReceiverButton' value='找人'>";
+    html += "<select name='eid' id='receiverCandidates'><option value=''>选择发送对象</select>";
+    html += "</td></tr>";
+    html += "<tr><td style='background-color:#EFE0C0;text-align:center' colspan='19'>";
+    html += "<b style='color:navy'>设置宠物升级时学习技能情况</b>";
+    html += "</td></tr>";
+    html += "<tr><td style='background-color:#E8E8D0;text-align:center' colspan='19'>";
+    html += "<input type='button' class='PetUIButton' value='第１技能位' id='pet_spell_study_1'>";
+    html += "<input type='button' class='PetUIButton' value='第２技能位' id='pet_spell_study_2'>";
+    html += "<input type='button' class='PetUIButton' value='第３技能位' id='pet_spell_study_3'>";
+    html += "<input type='button' class='PetUIButton' value='第４技能位' id='pet_spell_study_4'>";
+    html += "</td></tr>";
+    html += "<tr><td style='background-color:#E8E8D0;text-align:center' colspan='19'>";
+    html += "<input type='button' class='PetUIButton' value='刷新宠物管理' id='refreshButton'>";
+    html += "<input type='button' class='PetUIButton' value='打开黄金笼子' id='goldenCageButton'>";
+    html += "</td></tr>";
+    html += "</tbody>";
+    html += "</table>";
+
+    $("#pet_management_container").html(html);
 }
 
 export = PersonalPetManagementProcessor;
