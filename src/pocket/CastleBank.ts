38f958e538267c1300ced067b38184ba5b2bf095
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-29 14:10:27 +0800

    CastleBank.withdraw

--- src/pocket/CastleBank.ts
+++ src/pocket/CastleBank.ts
@@ -1,4 +1,6 @@
 import Credential from "../util/Credential";
+import NetworkUtils from "../util/NetworkUtils";
+import MessageBoard from "../util/MessageBoard";
 
 class CastleBank {
 
@@ -6,6 +8,32 @@ class CastleBank {
 
     constructor(credential: Credential) {
         this.#credential = credential;
+    }
+
+    async withdraw(amount: number): Promise<boolean> {
+        const action = (credential: Credential, amount: number) => {
+            return new Promise<boolean>(resolve => {
+                if (amount === undefined || amount === null || amount <= 0) {
+                    resolve(true);
+                } else {
+                    const request = credential.asRequest();
+                    // @ts-ignore
+                    request["mode"] = "CASTLEBANK_BUY";
+                    // @ts-ignore
+                    request["dasu"] = amount;
+                    NetworkUtils.sendPostRequest("town.cgi", request, function (html: string) {
+                        if ($(html).text().includes("您在钱庄里没那么多存款")) {
+                            MessageBoard.publishWarning("真可怜，银行里面连" + amount + "万存款都没有。");
+                            resolve(false);
+                        } else {
+                            MessageBoard.publishMessage("从城堡银行提取了" + amount + "万现金。");
+                            resolve(true);
+                        }
+                    });
+                }
+            });
+        };
+        return await action(this.#credential, amount);
     }
 }
 
