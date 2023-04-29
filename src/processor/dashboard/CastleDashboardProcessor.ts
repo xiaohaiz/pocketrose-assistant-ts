df6d53387fdb15291f4d5659c39dca7d7638868f
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-29 15:19:32 +0800

    doRenderPostHouseMenu

--- src/processor/dashboard/CastleDashboardProcessor.ts
+++ src/processor/dashboard/CastleDashboardProcessor.ts
@@ -1,9 +1,19 @@
 import PageProcessor from "../PageProcessor";
+import PageUtils from "../../util/PageUtils";
 
 class CastleDashboardProcessor extends PageProcessor {
 
     process() {
+        PageUtils.removeUnusedHyperLinks();
+        PageUtils.removeGoogleAnalyticsScript();
+
+        doRenderPostHouseMenu();
     }
+}
+
+function doRenderPostHouseMenu() {
+    $("option[value='CASTLE_BUILDMACHINE']").text("城堡驿站");
+    $("option[value='CASTLE_BUILDMACHINE']").css("background-color", "yellow");
 }
 
 export = CastleDashboardProcessor;
