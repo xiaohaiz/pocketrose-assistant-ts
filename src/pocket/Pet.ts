2830035e1fc533ab41ad3b72cdc1691ea55b169d
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-29 15:22:53 +0800

    Pet

--- src/pocket/Pet.ts
+++ src/pocket/Pet.ts
@@ -1,5 +1,35 @@
 class Pet {
 
+    index?: number;                  // 下标（唯一性）
+    name?: string;
+    gender?: string;
+    using?: boolean;
+    level?: number;
+    picture?: string;
+    health?: number;
+    maxHealth?: number;
+    spell1?: string;                 // 技能1
+    spell2?: string;                 // 技能2
+    spell3?: string;                 // 技能3
+    spell4?: string;                 // 技能4
+    usingSpell1?: boolean;           // 是否使用技能1
+    usingSpell2?: boolean;           // 是否使用技能2
+    usingSpell3?: boolean;           // 是否使用技能3
+    usingSpell4?: boolean;           // 是否使用技能4
+    spell1Description?: string;      // 技能1描述
+    spell2Description?: string;      // 技能2描述
+    spell3Description?: string;      // 技能3描述
+    spell4Description?: string;      // 技能4描述
+    attack?: number;
+    defense?: number;
+    specialAttack?: number;
+    specialDefense?: number;
+    speed?: number;
+    love?: number;
+    attribute1?: string;
+    attribute2?: string;
+    race?: string;
+    code?: string;
 }
 
 export = Pet;
