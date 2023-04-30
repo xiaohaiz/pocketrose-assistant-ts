cfa144a2eafc674419449b000c88a3d522d95d8e
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-30 20:02:49 +0800

    重新绘制冒险家公会的页面。

--- src/processor/town/TownAdventureGuildProcessor.ts
+++ src/processor/town/TownAdventureGuildProcessor.ts
@@ -14,6 +14,55 @@ class TownAdventureGuildProcessor extends PageProcessor {
 }
 
 function doProcess(credential: Credential) {
+    const t1 = $("table:eq(1)");
+    const t3 = $("table:eq(3)");
+    const t4 = $("table:eq(4)");
+
+    $(t1).find("td:first")
+        .removeAttr("bgcolor")
+        .removeAttr("width")
+        .removeAttr("height")
+        .css("width", "100%")
+        .css("text-align", "center")
+        .css("font-size", "150%")
+        .css("font-weight", "bold")
+        .css("background-color", "navy")
+        .css("color", "yellowgreen")
+        .text("＜＜  冒 险 家 公 会  ＞＞");
+
+    $(t3).find("tr:last")
+        .after($("" +
+            "<tr>" +
+            "<td style='background-color:#E0D0B0'>坐标点</td>" +
+            "<td id='roleLocation' style='background-color:#E8E8D0;color:red;font-weight:bold;text-align:right' colspan='3'>-</td>" +
+            "</tr>" +
+            "<tr>" +
+            "<td style='background-color:#E0D0B0'>计时器</td>" +
+            "<td id='countDownTimer' style='background-color:#E8E8D0;color:red;font-weight:bold;text-align:right' colspan='3'>-</td>" +
+            "</tr>"
+        ));
+
+    $(t4).find("td:first")
+        .removeAttr("width")
+        .removeAttr("bgcolor")
+        .css("width", "100%")
+        .css("background-color", "black")
+        .css("color", "white")
+        .attr("id", "messageBoard");
+
+    $("form:first").parent()
+        .parent()
+        .after($("" +
+            "<tr style='display:none'>" +
+            "<td id='eden' style='width:100%'></td>" +
+            "</tr>" +
+            "<tr>" +
+            "<td id='move' style='width:100%;background-color:#E8E8D0;text-align:center'></td>" +
+            "</tr>" +
+            "<tr style='display:none'>" +
+            "<td id='treasure' style='width:100%;background-color:#E8E8D0;text-align:center'></td>" +
+            "</tr>"
+        ));
 
 }
 
