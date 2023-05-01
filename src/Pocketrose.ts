13f347bc4a6f93a15ab0ea63ac5e66ca0b75511c
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-01 22:49:19 +0800

    入口执行文件极大简化。

--- src/Pocketrose.ts
+++ src/Pocketrose.ts
@@ -1,43 +1,10 @@
-import StringUtils from "./util/StringUtils";
-import BattleRequestInterceptor from "./interceptor/internal/BattleRequestInterceptor";
-import PersonalRequestInterceptor from "./interceptor/internal/PersonalRequestInterceptor";
-import StatusRequestInterceptor from "./interceptor/internal/StatusRequestInterceptor";
-import TownRequestInterceptor from "./interceptor/internal/TownRequestInterceptor";
-import CastleRequestInterceptor from "./interceptor/internal/CastleRequestInterceptor";
-import CastleStatusRequestInterceptor from "./interceptor/internal/CastleStatusRequestInterceptor";
-import RequestInterceptor from "./interceptor/RequestInterceptor";
+import RequestInterceptorManager from "./interceptor/RequestInterceptorManager";
+
+const interceptorManager = new RequestInterceptorManager();
 
 $(function () {
-    if (!location.href.includes("pocketrose")) {
-        return;
+    const href = location.href;
+    if (href.includes("pocketrose")) {
+        interceptorManager.lookupInterceptor(href)?.process();
     }
-    pocketrose();
-})
-
-const interceptorList: RequestInterceptor[] = [
-    new BattleRequestInterceptor(),
-    new CastleRequestInterceptor(),
-    new CastleStatusRequestInterceptor(),
-    new PersonalRequestInterceptor(),
-    new StatusRequestInterceptor(),
-    new TownRequestInterceptor()
-]
-
-function pocketrose() {
-    $(function () {
-        let request = StringUtils.substringAfterLast(location.href, "/")
-        if (request.includes("?")) {
-            request = StringUtils.substringBefore(request, "?");
-        }
-        let interceptor = null;
-        for (const it of interceptorList) {
-            if (it.cgi === request) {
-                interceptor = it;
-                break;
-            }
-        }
-        if (interceptor !== null) {
-            interceptor.process();
-        }
-    });
-}
+});
