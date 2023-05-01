cc65645c10c1efb501250e8a4bc64d967ad8582a
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-01 12:20:11 +0800

    冒险家公会改版中。

--- src/pocket/TreasureHint.ts
+++ src/pocket/TreasureHint.ts
@@ -1,4 +1,5 @@
 import Coordinate from "../util/Coordinate";
+import TownLoader from "./TownLoader";
 
 class TreasureHint {
 
@@ -6,6 +7,17 @@ class TreasureHint {
     name?: string;
     coordinate?: Coordinate;
 
+    get commentHtml(): string {
+        if (!this.coordinate!.isAvailable) {
+            return "<span style='color:red'>活动图</span>";
+        }
+        const town = TownLoader.getTownByCoordinate(this.coordinate!);
+        if (town === null) {
+            return "-";
+        } else {
+            return "<span style='color:red'>" + town.name + " " + this.coordinate!.asText() + "</span>";
+        }
+    }
 }
 
 export = TreasureHint;
--- src/processor/town/TownAdventureGuildProcessor.ts
+++ src/processor/town/TownAdventureGuildProcessor.ts
@@ -1,6 +1,11 @@
 import PageProcessor from "../PageProcessor";
 import PageUtils from "../../util/PageUtils";
 import Credential from "../../util/Credential";
+import TreasureHintParser from "../../pocket/TreasureHintParser";
+import TreasureHint from "../../pocket/TreasureHint";
+import NpcLoader from "../../pocket/NpcLoader";
+import MessageBoard from "../../util/MessageBoard";
+import NetworkUtils from "../../util/NetworkUtils";
 
 class TownAdventureGuildProcessor extends PageProcessor {
 
@@ -8,12 +13,13 @@ class TownAdventureGuildProcessor extends PageProcessor {
         PageUtils.removeUnusedHyperLinks();
         PageUtils.removeGoogleAnalyticsScript();
         const credential = PageUtils.currentCredential();
-        doProcess(credential);
+        const treasureHintList = TreasureHintParser.parseTreasureHintList(this.pageHtml);
+        doProcess(credential, treasureHintList);
     }
 
 }
 
-function doProcess(credential: Credential) {
+function doProcess(credential: Credential, treasureHintList: TreasureHint[]) {
     const t1 = $("table:eq(1)");
     const t3 = $("table:eq(3)");
     const t4 = $("table:eq(4)");
@@ -31,6 +37,8 @@ function doProcess(credential: Credential) {
         .attr("id", "title")
         .text("＜＜  冒 险 家 公 会  ＞＞");
 
+    $(t3).find("tr:last td:eq(1)")
+        .attr("id", "roleCash");
     $(t3).find("tr:last")
         .after($("" +
             "<tr>" +
@@ -49,11 +57,15 @@ function doProcess(credential: Credential) {
         .css("width", "100%")
         .css("background-color", "black")
         .css("color", "white")
-        .attr("id", "messageBoard");
+        .attr("id", "messageBoard")
+        .next()
+        .html(NpcLoader.getNpcImageHtml("花子")!);
+    MessageBoard.resetMessageBoard("S、S、SHIT，隔壁驿站推出了新的业务，抢、抢走了我的生意，幸亏挖宝的还得来我这儿！");
 
     $("form:first").parent()
         .attr("id", "treasureHint")
         .parent()
+        .css("display", "none")
         .after($("" +
             "<tr style='display:none'>" +
             "<td id='eden' style='width:100%'></td>" +
@@ -69,6 +81,95 @@ function doProcess(credential: Credential) {
             "</tr>"
         ));
 
+    // 清空之前的藏宝图交换的表单
+    $("#treasureHint").html("");
+
+    doRenderEden(credential);
+    doRenderMenu();
+    doRenderTreasureHint(credential, treasureHintList);
+
+    console.log(PageUtils.currentPageHtml());
+
+}
+
+function doRenderEden(credential: Credential) {
+    let html = "";
+    // noinspection HtmlUnknownTarget
+    html += "<form action='status.cgi' method='post' id='returnForm'>";
+    html += "<input type='hidden' name='id' value='" + credential.id + "'>";
+    html += "<input type='hidden' name='pass' value='" + credential.pass + "'>";
+    html += "<input type='hidden' name='mode' value='STATUS'>";
+    html += "<input type='submit' id='returnSubmit'>";
+    html += "</form>";
+    $("#eden").html(html);
+}
+
+function doRenderMenu() {
+    let html = "";
+    html += "<input type='button' value='更换藏宝图' id='exchangeButton'>";
+    html += "<input type='button' value='离开冒险家公会' id='returnButton'>";
+    $("#menu").html(html);
+
+    $("#returnButton").on("click", function () {
+        $("#returnSubmit").trigger("click");
+    });
+}
+
+function doRenderTreasureHint(credential: Credential, treasureHintList: TreasureHint[]) {
+    if (treasureHintList.length === 0) {
+        $("#exchangeButton")
+            .prop("disabled", true)
+            .hide();
+        return;
+    }
+    let html = "";
+    html += "<table style='background-color:#888888;margin:auto;border-width:0;text-align:center'>";
+    html += "<tbody style='background-color:#F8F0E0'>";
+    html += "<tr>";
+    html += "<th style='background-color:#E8E8D0'>选择</th>";
+    html += "<th style='background-color:#F8F0E0'>物品</th>";
+    html += "<th style='background-color:#EFE0C0'>X坐标</th>";
+    html += "<th style='background-color:#E0D0B0'>Y坐标</th>";
+    html += "<th style='background-color:#EFE0C0'>备注</th>";
+    html += "</tr>";
+    for (const treasureHint of treasureHintList) {
+        html += "<tr>";
+        html += "<td style='background-color:#E8E8D0'><input type='checkbox' name='item" + treasureHint.index + "' value='" + treasureHint.index + "'></td>";
+        html += "<td style='background-color:#F8F0E0'>" + treasureHint.name + "</td>";
+        html += "<td style='background-color:#EFE0C0'>" + treasureHint.coordinate?.x + "</td>";
+        html += "<td style='background-color:#E0D0B0'>" + treasureHint.coordinate?.y + "</td>";
+        html += "<td style='background-color:#EFE0C0'>" + treasureHint.commentHtml + "</td>";
+        html += "</tr>";
+    }
+    html += "</tbody>";
+    html += "</table>";
+
+    $("#treasureHint")
+        .html(html)
+        .parent()
+        .show();
+    $("#exchangeButton")
+        .prop("disabled", false)
+        .show();
+    doBindExchangeButton(credential);
+}
+
+function doBindExchangeButton(credential: Credential) {
+
+}
+
+function doRefresh(credential: Credential) {
+    $("#treasureHint").parent().hide();
+    $("#exchangeButton").off("click");
+    const request = credential.asRequest();
+    // @ts-ignore
+    request["con_str"] = "50";
+    // @ts-ignore
+    request["mode"] = "CHANGEMAP";
+    NetworkUtils.sendPostRequest("town.cgi", request, function (html) {
+        const hints = TreasureHintParser.parseTreasureHintList(html);
+        doRenderTreasureHint(credential, hints);
+    });
 }
 
 export = TownAdventureGuildProcessor;
