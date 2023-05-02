bed1ee9ff6666026cd6882b00b2f78c739e038c8
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-02 13:00:20 +0800

    解析角色状态的代码实现，从主页面解析。

--- src/pocket/RoleStatusParser.ts
+++ src/pocket/RoleStatusParser.ts
@@ -12,58 +12,42 @@ class RoleStatusParser {
 function doParseRoleStatus(pageHtml: string) {
     const page = $(pageHtml);
 
-    // 读取角色当前的能力值
-    const text = page
-        .find("#c_001")
-        .find("table:last")
-        .find("td:first")
-        .text();
-    let idx = text.indexOf("Lv：");
-    let s = text.substring(idx);
-    const level = parseInt(s.substring(3, s.indexOf(" ")));
-    idx = text.indexOf("攻击力：");
-    s = text.substring(idx);
-    const attack = parseInt(s.substring(4, s.indexOf(" ")));
-    idx = s.indexOf("防御力：");
-    s = s.substring(idx);
-    const defense = parseInt(s.substring(4, s.indexOf(" ")));
-    idx = s.indexOf("智力：");
-    s = s.substring(idx);
-    const specialAttack = parseInt(s.substring(3, s.indexOf(" ")));
-    idx = s.indexOf("精神力：");
-    s = s.substring(idx);
-    const specialDefense = parseInt(s.substring(4, s.indexOf(" ")));
-    idx = s.indexOf("速度：");
-    s = s.substring(idx);
-    const speed = parseInt(s.substring(3));
-    s = page
-        .find("input:hidden[name='ktotal']")
-        .val() as string;
-    const battleCount = parseInt(s);
-
     const status = new RoleStatus();
-    status.level = level;
-    status.attack = attack;
-    status.defense = defense;
-    status.specialAttack = specialAttack;
-    status.specialDefense = specialDefense;
-    status.speed = speed;
-    status.battleCount = battleCount;
-
     status.canConsecrate = page.text().includes("可以进行下次祭奠了");
-
-    s = page.find("option[value='LOCAL_RULE']").text();
-    status.country = StringUtils.substringBefore(s, "国法");
-
-    status.townCountry = page
-        .find("th:contains('支配下')")
+    status.battleCount = parseInt(page.find("input:hidden[name='ktotal']").val() as string);
+    status.country = StringUtils.substringBefore(page.find("option[value='LOCAL_RULE']").text(), "国法");
+    status.townCountry = page.find("th:contains('支配下')")
         .filter(function () {
             return $(this).text() === "支配下";
         })
         .next()
         .text();
-
     status.townId = page.find("input:hidden[name='townid']").val() as string;
+
+    // 读取角色当前的能力值
+    // 奇怪了，读不到指定id的div元素？但是可以读到里面的td子元素
+    page.find("td[bgcolor='#006000']:last")
+        .each(function (_idx, td) {
+            const text = $(td).text();
+            let idx = text.indexOf("Lv：");
+            let s = text.substring(idx);
+            status.level = parseInt(s.substring(3, s.indexOf(" ")));
+            idx = text.indexOf("攻击力：");
+            s = text.substring(idx);
+            status.attack = parseInt(s.substring(4, s.indexOf(" ")));
+            idx = s.indexOf("防御力：");
+            s = s.substring(idx);
+            status.defense = parseInt(s.substring(4, s.indexOf(" ")));
+            idx = s.indexOf("智力：");
+            s = s.substring(idx);
+            status.specialAttack = parseInt(s.substring(3, s.indexOf(" ")));
+            idx = s.indexOf("精神力：");
+            s = s.substring(idx);
+            status.specialDefense = parseInt(s.substring(4, s.indexOf(" ")));
+            idx = s.indexOf("速度：");
+            s = s.substring(idx);
+            status.speed = parseInt(s.substring(3));
+        });
 
     return status;
 }
