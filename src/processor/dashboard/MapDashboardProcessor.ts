317b364e53d853c04ae6fcf3cc0d4406b6971561
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-02 23:23:15 +0800

    在地图主页面渲染经验进度条。

--- src/processor/dashboard/MapDashboardProcessor.ts
+++ src/processor/dashboard/MapDashboardProcessor.ts
@@ -1,6 +1,7 @@
 import Processor from "../Processor";
 import PageUtils from "../../util/PageUtils";
 import StringUtils from "../../util/StringUtils";
+import SetupLoader from "../../pocket/SetupLoader";
 
 class MapDashboardProcessor implements Processor {
 
@@ -21,7 +22,31 @@ class MapDashboardProcessor implements Processor {
 }
 
 function doProcess() {
+    doRenderExperienceProgressBar();
     doRenderEventBoard();
+}
+
+function doRenderExperienceProgressBar() {
+    if (!SetupLoader.isExperienceProgressBarEnabled()) {
+        return;
+    }
+    $("td:contains('经验值')")
+        .filter(function () {
+            return $(this).text() === "经验值";
+        })
+        .next()
+        .each(function (_idx, th) {
+            const expText = $(th).text();
+            const experience = parseInt(StringUtils.substringBefore(expText, " EX"));
+            if (experience >= 14900) {
+                $(th).css("color", "blue").text("MAX");
+            } else {
+                const level = Math.ceil(experience / 100) + 1;
+                const ratio = level / 150;
+                const progressBar = PageUtils.generateProgressBarHTML(ratio);
+                $(th).html("<span title='" + expText + "'>" + progressBar + "</span>");
+            }
+        });
 }
 
 function doRenderEventBoard() {
