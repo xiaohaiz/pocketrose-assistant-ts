99820986f87dfa6d619baf9031c8bdeb25bd63ef
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-02 11:07:58 +0800

    Parse role status from main dashboard.

--- src/pocket/RoleStatus.ts
+++ src/pocket/RoleStatus.ts
@@ -1,6 +1,9 @@
 class RoleStatus {
 
-    canConsecrate?: boolean;
+    canConsecrate?: boolean;        // 是否可以祭奠
+    country?: string;               // 角色自身所属的国家
+    townCountry?: string;           // 所在城市所属的国家
+    townId?: string;                // 所在城市的ID
 
 }
 
--- src/pocket/RoleStatusParser.ts
+++ src/pocket/RoleStatusParser.ts
@@ -1,4 +1,5 @@
 import RoleStatus from "./RoleStatus";
+import StringUtils from "../util/StringUtils";
 
 class RoleStatusParser {
 
@@ -6,6 +7,19 @@ class RoleStatusParser {
         const status = new RoleStatus();
         const text = $(pageHtml).text();
         status.canConsecrate = text.includes("可以进行下次祭奠了");
+
+        let s = $("option[value='LOCAL_RULE']").text();
+        status.country = StringUtils.substringBefore(s, "国法");
+
+        status.townCountry = $("th:contains('支配下')")
+            .filter(function () {
+                return $(this).text() === "支配下";
+            })
+            .next()
+            .text();
+
+        status.townId = $("input:hidden[name='townid']").val() as string;
+
         return status;
     }
 
