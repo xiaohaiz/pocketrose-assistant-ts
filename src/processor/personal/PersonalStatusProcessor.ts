99833d1920bddcd2cc55323aeedb2c9dbbdff0c1
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-02 08:38:24 +0800

    渲染个人状态页面的经验值。

--- src/processor/personal/PersonalStatusProcessor.ts
+++ src/processor/personal/PersonalStatusProcessor.ts
@@ -1,17 +1,41 @@
 import PageUtils from "../../util/PageUtils";
 import PageProcessor from "../PageProcessor";
+import RoleParser from "../../pocket/RoleParser";
+import Role from "../../pocket/Role";
+import SetupLoader from "../../pocket/SetupLoader";
 
 class PersonalStatusProcessor extends PageProcessor {
 
     process() {
         PageUtils.removeUnusedHyperLinks();
         PageUtils.removeGoogleAnalyticsScript();
-        doProcess();
+        doProcess(this.pageHtml);
     }
 }
 
-function doProcess() {
+function doProcess(pageHtml: string) {
+    const role = RoleParser.parseRole(pageHtml);
+
+    doRenderExperience(role);
     doRenderHonor();
+}
+
+function doRenderExperience(role: Role) {
+    if (!SetupLoader.isExperienceProgressBarEnabled()) {
+        return;
+    }
+    const t1 = $("table:eq(1)");
+    let tr = $(t1).find("tr:eq(16)");
+    let td = $(tr).find("td:eq(2)");
+    if (role.level === 150) {
+        $(td).attr("style", "color: blue").text("MAX");
+    } else {
+        const ratio = role.level! / 150;
+        const progressBar = PageUtils.generateProgressBarHTML(ratio);
+        const exp = $(td).text() + " EX";
+        $(td).html("<span title='" + exp + "'>" + progressBar + "</span>");
+    }
+
 }
 
 function doRenderHonor() {
