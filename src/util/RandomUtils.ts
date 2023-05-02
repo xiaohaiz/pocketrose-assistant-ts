0eeba7667597640fc5ab61afa7df1b3591b40d8d
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-02 15:48:31 +0800

    RandomUtils.randomElement

--- src/util/RandomUtils.ts
+++ src/util/RandomUtils.ts
@@ -4,6 +4,16 @@ class RandomUtils {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
+    static randomElement<T>(list: T[]): T | null {
+        if (list.length === 0) {
+            return null;
+        }
+        if (list.length === 1) {
+            return list[0];
+        }
+        const idx = RandomUtils.randomInt(0, list.length - 1);
+        return list[idx];
+    }
 }
 
 export = RandomUtils;
