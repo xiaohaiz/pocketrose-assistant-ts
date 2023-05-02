f854731d8e372d6d01505133eef6df7a80a614c9
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-02 22:44:56 +0800

    解析事件面板中的原始事件。

--- src/processor/dashboard/EventDashboardProcessor.ts
+++ src/processor/dashboard/EventDashboardProcessor.ts
@@ -1,5 +1,6 @@
 import Processor from "../Processor";
 import PageUtils from "../../util/PageUtils";
+import StringUtils from "../../util/StringUtils";
 
 class EventDashboardProcessor implements Processor {
 
@@ -15,11 +16,30 @@ class EventDashboardProcessor implements Processor {
         PageUtils.removeGoogleAnalyticsScript();
         PageUtils.fixCurrentPageBrokerImages();
         doProcess();
+        console.log(PageUtils.currentPageHtml());
     }
 
 }
 
 function doProcess() {
+    $("table:eq(3) tr:eq(1) td:first")
+        .attr("id", "event_container");
+
+    const originalEventHtmlList = doParseOriginalEventHtmlList();
+    originalEventHtmlList.forEach(it => console.log(it));
+}
+
+function doParseOriginalEventHtmlList(): string[] {
+    const originalEventHtmlList: string[] = [];
+    $("#event_container").html()
+        .split("<br>")
+        .filter(it => it.endsWith(")"))
+        .map(function (it) {
+            const header = "<font color=\"green\">●</font>";
+            return StringUtils.substringAfter(it, header);
+        })
+        .forEach(it => originalEventHtmlList.push(it));
+    return originalEventHtmlList;
 }
 
 
