676efadecf7b93b95d9395fdd0091c816ab9f089
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-02 23:35:47 +0800

    城堡主页面渲染事件面板。

--- src/processor/dashboard/CastleDashboardProcessor.ts
+++ src/processor/dashboard/CastleDashboardProcessor.ts
@@ -1,5 +1,7 @@
 import PageUtils from "../../util/PageUtils";
 import Processor from "../Processor";
+import StringUtils from "../../util/StringUtils";
+import SetupLoader from "../../pocket/SetupLoader";
 
 class CastleDashboardProcessor implements Processor {
 
@@ -13,14 +15,81 @@ class CastleDashboardProcessor implements Processor {
     process() {
         PageUtils.removeUnusedHyperLinks();
         PageUtils.removeGoogleAnalyticsScript();
-        doRenderPostHouseMenu();
+        PageUtils.fixCurrentPageBrokerImages();
+        doProcess();
     }
+}
+
+function doProcess() {
+    doRenderPostHouseMenu();
+    doRenderExperienceProgressBar();
+    doRenderEventBoard();
 }
 
 function doRenderPostHouseMenu() {
     $("option[value='CASTLE_BUILDMACHINE']")
         .css("background-color", "yellow")
         .text("城堡驿站");
+}
+
+function doRenderExperienceProgressBar() {
+    if (!SetupLoader.isExperienceProgressBarEnabled()) {
+        return;
+    }
+    $("td:contains('经验值')")
+        .filter(function () {
+            return $(this).text() === "经验值";
+        })
+        .next()
+        .each(function (_idx, th) {
+            const expText = $(th).text();
+            const experience = parseInt(StringUtils.substringBefore(expText, " EX"));
+            if (experience >= 14900) {
+                $(th).css("color", "blue").text("MAX");
+            } else {
+                const level = Math.ceil(experience / 100) + 1;
+                const ratio = level / 150;
+                const progressBar = PageUtils.generateProgressBarHTML(ratio);
+                $(th).html("<span title='" + expText + "'>" + progressBar + "</span>");
+            }
+        });
+}
+
+function doRenderEventBoard() {
+    $("td:contains('最近发生的事件')")
+        .filter(function () {
+            return $(this).text() === "最近发生的事件";
+        })
+        .parent()
+        .next()
+        .find("td:first")
+        .attr("id", "eventBoard");
+
+    const eventHtmlList: string[] = [];
+    $("#eventBoard").html()
+        .split("<br>")
+        .filter(it => it.endsWith(")"))
+        .map(function (it) {
+            const header = "<font color=\"navy\">●</font>";
+            return StringUtils.substringAfter(it, header);
+        })
+        .forEach(it => eventHtmlList.push(it));
+
+    let html = "";
+    html += "<table style='border-width:0;width:100%;margin:auto'>";
+    html += "<tbody>";
+    eventHtmlList.forEach(it => {
+        html += "<tr>";
+        html += "<th style='color:navy;vertical-align:top'>●</th>";
+        html += "<td style='width:100%'>";
+        html += it;
+        html += "</td>";
+        html += "</tr>";
+    });
+    html += "</tbody>";
+    html += "</table>";
+
+    $("#eventBoard").html(html);
 }
 
 export = CastleDashboardProcessor;
