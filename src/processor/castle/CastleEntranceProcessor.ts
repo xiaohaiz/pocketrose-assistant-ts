db08c18b8c03c8befe1b460eacae3757039c0747
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-03 11:12:20 +0800

    城堡操作跳过中间确认页面快速返回。

--- src/processor/castle/CastleEntranceProcessor.ts
+++ src/processor/castle/CastleEntranceProcessor.ts
@@ -7,6 +7,11 @@ class CastleEntranceProcessor implements Processor {
         if (cgi === "status.cgi") {
             return pageText.includes("城堡") && pageText.includes("入口。");
         }
+        if (cgi === "castle.cgi") {
+            return pageText.includes("已经顺利存入您的账户！") ||
+                pageText.includes("请善加利用，欢迎您再来！") ||
+                pageText.includes("体力完全回复。");
+        }
         return false;
     }
 
@@ -20,6 +25,10 @@ class CastleEntranceProcessor implements Processor {
 
 function doProcess() {
     let submit = $("input:submit[value='进入城堡']");
+    if (submit.length > 0) {
+        submit.trigger("click");
+    }
+    submit = $("input:submit[value='返回城堡']");
     if (submit.length > 0) {
         submit.trigger("click");
     }
