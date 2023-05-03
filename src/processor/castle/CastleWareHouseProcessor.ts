55675c6f87215f36d505947c672316870240b6f7
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-03 12:43:20 +0800

    创建城堡仓库的消息面板。

--- src/processor/castle/CastleWareHouseProcessor.ts
+++ src/processor/castle/CastleWareHouseProcessor.ts
@@ -1,6 +1,10 @@
 import Processor from "../Processor";
 import PageUtils from "../../util/PageUtils";
 import EquipmentParser from "../../pocket/EquipmentParser";
+import Credential from "../../util/Credential";
+import Equipment from "../../pocket/Equipment";
+import MessageBoard from "../../util/MessageBoard";
+import NpcLoader from "../../pocket/NpcLoader";
 
 class CastleWareHouseProcessor implements Processor {
 
@@ -39,6 +43,25 @@ function doProcess() {
         .css("color", "yellowgreen")
         .text("＜＜  城 堡 仓 库  ＞＞");
 
+    // 创建消息面板
+    $("tr:first")
+        .next()
+        .after("" +
+            "<tr><td id='messageBoardContainer' style='background-color:#E8E8D0'></td></tr>" +
+            "<tr style='display:none'><td></td></tr>" +
+            "<tr style='display:none'><td></td></tr>");
+    MessageBoard.createMessageBoardStyleB("messageBoardContainer", NpcLoader.randomNpcImageHtml());
+    $("#messageBoard")
+        .css("background-color", "black")
+        .css("color", "white")
+        .text("请管理您的城堡仓库。");
+
+    doRender(credential, personalEquipmentList, storageEquipmentList);
+}
+
+function doRender(credential: Credential,
+                  personalEquipmentList: Equipment[],
+                  storageEquipmentList: Equipment[]) {
 
 }
 
--- src/util/MessageBoard.ts
+++ src/util/MessageBoard.ts
@@ -13,6 +13,19 @@ class MessageBoard {
         $("#" + containerId).html(html);
     }
 
+    static createMessageBoardStyleB(containerId: string, imageHtml: string) {
+        let html = "";
+        html += "<table style='background-color:#888888;border-width:0;width:100%'>";
+        html += "<tbody>";
+        html += "<tr>";
+        html += "<td style='background-color:#F8F0E0;width:100%' id='messageBoard'></td>"
+        html += "<td style='background-color:#F8F0E0'>" + imageHtml + "</td>"
+        html += "</tr>";
+        html += "</tbody>";
+        html += "</table>";
+        $("#" + containerId).html(html);
+    }
+
     static resetMessageBoard(message: string) {
         if ($("#messageBoard").length === 0) {
             return;
