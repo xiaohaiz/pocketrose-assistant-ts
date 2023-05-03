c3bdf21d8be6ecd6aee40b4f1e41baead4e529b3
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-03 14:48:02 +0800

    渲染仓库的装备

--- src/processor/castle/CastleWareHouseProcessor.ts
+++ src/processor/castle/CastleWareHouseProcessor.ts
@@ -281,7 +281,148 @@ function doRenderPersonalEquipmentList(credential: Credential, personalEquipment
         .show();
 }
 
-function doRenderStorageEquipmentList(credential: Credential, personalEquipmentList: Equipment[]) {
+function doRenderStorageEquipmentList(credential: Credential, storageEquipmentList: Equipment[]) {
+    if (storageEquipmentList.length === 0) {
+        return;
+    }
+    let html = "";
+    html += "<table style='border-width:0;width:100%;background-color:#888888'>";
+    html += "<tbody style='background-color:#F8F0E0;text-align:center'>";
+    html += "<tr>";
+    html += "<th style='background-color:#E8E8D0'>选择</th>";
+    html += "<th style='background-color:#E0D0B0'>名字</th>";
+    html += "<th style='background-color:#EFE0C0'>种类</th>";
+    html += "<th style='background-color:#E0D0B0'>效果</th>";
+    html += "<th style='background-color:#EFE0C0'>重量</th>";
+    html += "<th style='background-color:#EFE0C0'>耐久</th>";
+    html += "<th style='background-color:#E0D0B0'>职业</th>";
+    html += "<th style='background-color:#E0D0B0'>攻击</th>";
+    html += "<th style='background-color:#E0D0B0'>防御</th>";
+    html += "<th style='background-color:#E0D0B0'>智力</th>";
+    html += "<th style='background-color:#E0D0B0'>精神</th>";
+    html += "<th style='background-color:#E0D0B0'>速度</th>";
+    html += "<th style='background-color:#E0D0B0'>威力</th>";
+    html += "<th style='background-color:#E0D0B0'>重量</th>";
+    html += "<th style='background-color:#E0D0B0'>幸运</th>";
+    html += "<th style='background-color:#E0D0B0'>经验</th>";
+    html += "<th style='background-color:#EFE0C0'>属性</th>";
+    html += "<th style='background-color:#E0D0B0'>出库</th>";
+    html += "</tr>";
+
+    for (const equipment of storageEquipmentList) {
+        html += "<tr>";
+        html += "<td style='background-color:#E8E8D0'>";
+        html += "<input type='checkbox' class='storage_checkbox_class' " +
+            "name='item" + equipment.index + "' value='" + equipment.index + "'>";
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>" + equipment.nameHTML + "</td>";
+        html += "<td style='background-color:#EFE0C0'>" + equipment.category + "</td>";
+        html += "<td style='background-color:#E0D0B0'>" + equipment.power + "</td>";
+        html += "<td style='background-color:#EFE0C0'>" + equipment.weight + "</td>";
+        html += "<td style='background-color:#EFE0C0'>";
+        html += equipment.endureHtml;
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.requiredCareer === "所有职业") {
+            html += "-";
+        } else {
+            html += equipment.requiredCareer;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.requiredAttack === 0) {
+            html += "-";
+        } else {
+            html += equipment.requiredAttack;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.requiredDefense === 0) {
+            html += "-";
+        } else {
+            html += equipment.requiredDefense;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.requiredSpecialAttack === 0) {
+            html += "-";
+        } else {
+            html += equipment.requiredSpecialAttack;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.requiredSpecialDefense === 0) {
+            html += "-";
+        } else {
+            html += equipment.requiredSpecialDefense;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.requiredSpeed === 0) {
+            html += "-";
+        } else {
+            html += equipment.requiredSpeed;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.additionalPower === 0) {
+            html += "-";
+        } else {
+            html += equipment.additionalPower;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.additionalWeight === 0) {
+            html += "-";
+        } else {
+            html += equipment.additionalWeight;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.additionalLuck === 0) {
+            html += "-";
+        } else {
+            html += equipment.additionalLuck;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (SetupLoader.isExperienceProgressBarEnabled()) {
+            html += equipment.experienceHTML;
+        } else {
+            html += equipment.experience;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#EFE0C0'>";
+        if (equipment.attribute === "无") {
+            html += "-";
+        } else {
+            html += equipment.attribute;
+        }
+        html += "</td>";
+        html += "<td style='background-color:#E0D0B0'>";
+        if (equipment.selectable) {
+            html += "<input type='button' class='button_class' " +
+                "id='takeFromWareHouseButton_" + equipment.index + "' value='出库'>";
+        } else {
+            html += "<input type='button' value='　　' disabled " +
+                "style='background-color:#E0D0B0;border-width:0'>";
+        }
+        html += "</td>";
+        html += "</tr>";
+    }
+    html += "<tr>";
+    html += "<td colspan='18' style='text-align:left'>";
+    html += "<input type='button' class='button_class' " +
+        "id='takeFromWareHouseButton' value='从城堡仓库中取出'>";
+    html += "</td>";
+    html += "</tr>";
+    html += "</tbody>";
+    html += "</table>"
+
+    $("#storageEquipmentList")
+        .html(html)
+        .parent()
+        .show();
 }
 
 function doRefresh(credential: Credential) {
