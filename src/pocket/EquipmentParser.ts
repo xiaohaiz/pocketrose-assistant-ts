755dd9140e9b8fc8ec7d3f754d0b59cfef420790
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-03 11:45:59 +0800

    EquipmentParser.parseCastleWareHousePersonalEquipmentList

--- src/pocket/EquipmentParser.ts
+++ src/pocket/EquipmentParser.ts
@@ -175,7 +175,73 @@ class EquipmentParser {
         return equipmentList;
     }
 
-    static parseCastleWarehouseItemList(pageHTML: string) {
+    /**
+     * 解析城堡仓库页面的手持物品
+     * @param pageHtml
+     */
+    static parseCastleWareHousePersonalEquipmentList(pageHtml: string): Equipment[] {
+        const equipmentList: Equipment[] = [];
+        const t3 = $("table:eq(3)");
+        t3.find("input:checkbox")
+            .each(function (_idx, checkbox) {
+                const c0 = $(checkbox).parent();
+                const c1 = c0.next();
+                const c2 = c1.next();
+                const c3 = c2.next();
+                const c4 = c3.next();
+                const c5 = c4.next();
+                const c6 = c5.next();
+                const c7 = c6.next();
+                const c8 = c7.next();
+                const c9 = c8.next();
+                const c10 = c9.next();
+                const c11 = c10.next();
+                const c12 = c11.next();
+                const c13 = c12.next();
+                const c14 = c13.next();
+                const c15 = c14.next();
+                const c16 = c15.next();
+                const c17 = c16.next();
+
+                const equipment = new Equipment();
+                equipment.index = parseInt($(checkbox).val() as string);
+                equipment.selectable = !$(checkbox).prop("disabled");
+                equipment.using = c1.text() === "★";
+                equipment.nameHTML = c2.html();
+                let s = c2.text();
+                if (s.startsWith("齐心★")) {
+                    equipment.star = true;
+                    equipment.name = StringUtils.substringAfter(s, "齐心★");
+                } else {
+                    equipment.star = false;
+                    equipment.name = s;
+                }
+                equipment.category = c3.text();
+                equipment.power = parseInt(c4.text());
+                equipment.weight = parseInt(c5.text());
+                equipment.endure = parseInt(c6.text());
+                equipment.requiredCareer = c7.text();
+                equipment.requiredAttack = parseInt(c8.text());
+                equipment.requiredDefense = parseInt(c9.text());
+                equipment.requiredSpecialAttack = parseInt(c10.text());
+                equipment.requiredSpecialDefense = parseInt(c11.text());
+                equipment.requiredSpeed = parseInt(c12.text());
+                equipment.additionalPower = parseInt(c13.text());
+                equipment.additionalWeight = parseInt(c14.text());
+                equipment.additionalLuck = parseInt(c15.text());
+                equipment.experience = parseInt(c16.text());
+                equipment.attribute = c17.text();
+
+                equipmentList.push(equipment);
+            });
+        return equipmentList;
+    }
+
+    /**
+     * 解析城堡仓库页面的库存物品
+     * @param pageHTML
+     */
+    static parseCastleWareHouseStorageEquipmentList(pageHTML: string) {
         const equipmentList: Equipment[] = [];
         const table = $(pageHTML).find("input:checkbox:last").closest("table");
         $(table).find("input:checkbox").each(function (_idx, checkbox) {
