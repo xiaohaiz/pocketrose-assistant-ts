8600affb1c37a4c0bfedc54ce135d4d6d46ad589
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-03 22:01:12 +0800

    快速登陆功能，设置中启用，然后自行设置登陆账号后在登录页可以快捷登陆。

--- src/processor/dashboard/LoginDashboardProcessor.ts
+++ src/processor/dashboard/LoginDashboardProcessor.ts
@@ -1,5 +1,7 @@
 import Processor from "../Processor";
 import PageUtils from "../../util/PageUtils";
+import SetupLoader from "../../pocket/SetupLoader";
+import FastLoginLoader from "../../pocket/FastLoginLoader";
 
 class LoginDashboardProcessor implements Processor {
 
@@ -19,6 +21,101 @@ class LoginDashboardProcessor implements Processor {
 }
 
 function doProcess() {
+    if (!SetupLoader.isFastLoginEnabled()) {
+        return;
+    }
+    const configs = new Map<number, {}>();
+    for (let i = 0; i < 10; i++) {
+        const config = FastLoginLoader.loadFastLoginConfig(i);
+        if (doCheckConfigAvailability(config)) {
+            configs.set(i, config);
+        }
+    }
+    if (configs.size === 0) {
+        return;
+    }
+
+    $("input:text[name='id']").attr("id", "loginId");
+    $("input:password[name='pass']").attr("id", "loginPass");
+    $("input:submit[value='登陆']").attr("id", "loginButton");
+
+    let html = "";
+    html += "<tr>";
+    html += "<td id='fastLogin' style='background-color:#E8E8D0;text-align:center' colspan='2'></td>"
+    html += "</tr>";
+    $("#loginButton")
+        .closest("tr")
+        .after($(html));
+
+    doRender(configs);
+}
+
+function doCheckConfigAvailability(config: {}): boolean {
+    // @ts-ignore
+    return config.name !== undefined && config.id !== undefined && config.pass !== undefined;
+}
+
+function doRender(configs: Map<number, {}>) {
+    let html = "";
+    html += "<table style='border-width:0;margin:auto;text-align:center;width:100%'>";
+    html += "<tbody>";
+    html += "</tbody>";
+    html += "<tr>";
+    html += "<td colspan='5' style='color:navy;font-weight:bold'>快速登陆时请自行输入图形验证码</td>";
+    html += "</tr>";
+    html += "<tr>";
+    for (let i = 0; i < 5; i++) {
+        html += doGenerateCell(configs, i);
+    }
+    html += "</tr>";
+    html += "<tr>";
+    for (let i = 5; i < 10; i++) {
+        html += doGenerateCell(configs, i);
+    }
+    html += "</tr>";
+    html += "</table>";
+
+    $("#fastLogin").html(html);
+
+    doBindFastLoginButton();
+}
+
+function doGenerateCell(configs: Map<number, {}>, code: number) {
+    let html = "";
+    html += "<td>";
+    let config = configs.get(code);
+    if (config !== undefined) {
+        // @ts-ignore
+        const name = config.name;
+        html += "<input type='button' class='fastLoginButton' id='fastLogin_" + code + "' value='" + name + "'>";
+    } else {
+        html += "<input type='button' disabled " +
+            "style='background-color:#E8E8D0;border-width:0' value='　　　'>";
+    }
+    html += "</td>";
+    return html;
+}
+
+function doBindFastLoginButton() {
+    for (let i = 0; i < 10; i++) {
+        const buttonId = "fastLogin_" + i;
+        if ($("#" + buttonId).length === 0) {
+            continue;
+        }
+        $("#" + buttonId).on("click", function () {
+            const code = parseInt(($(this).attr("id") as string).split("_")[1]);
+            const config = FastLoginLoader.loadFastLoginConfig(code);
+            if (!doCheckConfigAvailability(config)) {
+                return;
+            }
+            // @ts-ignore
+            $("#loginId").val(config.id);
+            // @ts-ignore
+            $("#loginPass").val(config.pass);
+
+            $("#loginButton").trigger("click");
+        });
+    }
 }
 
 export = LoginDashboardProcessor;
