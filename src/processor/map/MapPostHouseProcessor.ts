1a1cf16489ffa8a38d670be55816b8f3ca63961a
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-04 08:29:52 +0800

    野外简易驿站功能，能移动到地图上任意选定的坐标。

--- src/processor/map/MapPostHouseProcessor.ts
+++ src/processor/map/MapPostHouseProcessor.ts
@@ -3,6 +3,14 @@ import PageUtils from "../../util/PageUtils";
 import Credential from "../../util/Credential";
 import NpcLoader from "../../pocket/NpcLoader";
 import MapBuilder from "../../pocket/MapBuilder";
+import RoleLocationLoader from "../../pocket/RoleLocationLoader";
+import StringUtils from "../../util/StringUtils";
+import CastleLoader from "../../pocket/CastleLoader";
+import Coordinate from "../../util/Coordinate";
+import MessageBoard from "../../util/MessageBoard";
+import NetworkUtils from "../../util/NetworkUtils";
+import TravelPlanBuilder from "../../pocket/TravelPlanBuilder";
+import TravelPlanExecutor from "../../pocket/TravelPlanExecutor";
 
 class MapPostHouseProcessor implements Processor {
 
@@ -39,6 +47,13 @@ function doProcess(credential: Credential) {
         .attr("id", "title")
         .text("＜＜　 住 所 & 简 易 驿 站 　＞＞");
 
+    const player = $("table:eq(4)")
+        .find("tr:first")
+        .next()
+        .find("td:first")
+        .text();
+    console.log(player);
+
     $("table:eq(4)")
         .find("tr:last")
         .after($("" +
@@ -66,21 +81,143 @@ function doProcess(credential: Credential) {
         .html(NpcLoader.randomNpcImageHtml());
 
     let html = "";
-    html += "<tr>";
+    html += "<tr style='display:none'>";
+    html += "<td id='menu' style='background-color:#F8F0E0;text-align:center'>";
+    html += "</td>"
+    html += "</tr>";
+    html += "<tr style='display:none'>";
     html += "<td id='postHouse' style='background-color:#F8F0E0;text-align:center'>";
     html += "</td>"
     html += "</tr>";
 
     $("#returnButton")
         .closest("tr")
+        .attr("id", "visitRow")
         .after($(html));
 
-    doRender(credential);
+    doGenerateEdenForm(credential);
+    doGenerateMenu();
+    doRender(credential, player);
 }
 
-function doRender(credential: Credential) {
+function doGenerateEdenForm(credential: Credential) {
+    let html = "";
+    // noinspection HtmlUnknownTarget
+    html += "<form action='status.cgi' method='post' id='returnForm'>";
+    html += "<input type='hidden' name='id' value='" + credential.id + "'>";
+    html += "<input type='hidden' name='pass' value='" + credential.pass + "'>";
+    html += "<input type='hidden' name='mode' value='STATUS'>";
+    html += "<input type='submit' id='returnSubmit'>";
+    html += "</form>";
+    $("#eden").html(html);
+}
+
+function doGenerateMenu() {
+    $("#menu").html("<input type='button' id='travelButton' " +
+        "disabled value='旅途中请耐心等候......'>");
+    $("#travelButton").on("click", function () {
+        $("#returnSubmit").trigger("click");
+    });
+}
+
+function doRender(credential: Credential, player: string) {
     $("#postHouse").html(MapBuilder.buildMapTable());
     MapBuilder.updateTownBackgroundColor();
+
+    new RoleLocationLoader(credential).load()
+        .then(location => {
+            const x = location.coordinate!.x;
+            const y = location.coordinate!.y;
+            const buttonId = "location_" + x + "_" + y;
+            $("#" + buttonId)
+                .closest("td")
+                .css("background-color", "black")
+                .css("color", "yellow")
+                .css("text-align", "center")
+                .html("我");
+
+            $(".location_button_class")
+                .on("mouseenter", function () {
+                    $(this).css("background-color", "red");
+                })
+                .on("mouseleave", function () {
+                    const s = $(this).parent().attr("class")!;
+                    const c = StringUtils.substringAfter(s, "_");
+                    if (c !== "none") {
+                        $(this).css("background-color", c);
+                    } else {
+                        $(this).removeAttr("style");
+                    }
+                });
+
+            $("#postHouse").parent().show();
+
+            CastleLoader.loadCastle(player)
+                .then(castle => {
+                    if (castle === null) {
+                        return;
+                    }
+                    const coordinate = castle.coordinate!;
+                    if (location.coordinate!.equals(coordinate)) {
+                        return;
+                    }
+                    const buttonId = "location_" + coordinate.x + "_" + coordinate.y;
+                    $("#" + buttonId)
+                        .attr("value", "堡")
+                        .css("background-color", "fuchsia")
+                        .parent()
+                        .attr("title", "城堡" + coordinate.asText() + " " + castle.name)
+                        .attr("class", "color_fuchsia");
+                });
+
+            doBind(credential);
+        });
+}
+
+function doBind(credential: Credential) {
+    $(".location_button_class").on("click", function () {
+        const ss = ($(this).attr("id") as string).split("_");
+        const x = parseInt(ss[1]);
+        const y = parseInt(ss[2]);
+        const coordinate = new Coordinate(x, y);
+
+        const confirmation = confirm("确认移动到坐标" + coordinate.asText() + "？");
+        if (!confirmation) {
+            return;
+        }
+
+        document.getElementById("title")?.scrollIntoView();
+        MessageBoard.resetMessageBoard("虽然我们的驿站很简陋，但是也有实时旅途动态播报：<br>");
+        $("#visitRow").hide();
+        $(".location_button_class")
+            .prop("disabled", true)
+            .off("mouseenter")
+            .off("mouseleave");
+        $("#menu").parent().show();
+
+        doTravelToLocation(credential, coordinate);
+    });
+}
+
+function doTravelToLocation(credential: Credential, location: Coordinate) {
+    MessageBoard.publishMessage("目的地坐标：<span style='color:greenyellow'>" + location.asText() + "</span>");
+
+    const request = credential.asRequest();
+    // @ts-ignore
+    request.mode = "STATUS";
+    NetworkUtils.sendPostRequest("status.cgi", request, function (pageHtml) {
+        const plan = TravelPlanBuilder.initializeTravelPlan(pageHtml);
+        plan.destination = location;
+        plan.credential = credential;
+        const executor = new TravelPlanExecutor(plan);
+        executor.execute()
+            .then(() => {
+                MessageBoard.publishMessage("旅途愉快，下次再见。");
+                $("#travelButton")
+                    .prop("disabled", false)
+                    .val("到达了目的地" + location.asText());
+            });
+    });
 }
 
 export = MapPostHouseProcessor;
