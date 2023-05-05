b2c184ca42943b718e7b7a253746deb3b36df280
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-05 14:32:13 +0800

    TownGemMeltHouse

---
+++ src/pocket/house/TownGemMeltHousePage.ts
@@ -0,0 +1,13 @@
+import Credential from "../../util/Credential";
+
+class TownGemMeltHousePage {
+
+    readonly credential: Credential;
+
+    constructor(credential: Credential) {
+        this.credential = credential;
+    }
+
+}
+
+export = TownGemMeltHousePage;
