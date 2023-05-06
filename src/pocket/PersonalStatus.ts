4341a217a00d53f816c0770257dc5757fb50dcfd
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-06 20:54:09 +0800

    PersonalStatus.parsePage

--- src/pocket/PersonalStatus.ts
+++ src/pocket/PersonalStatus.ts
@@ -1,4 +1,7 @@
 import Credential from "../util/Credential";
+import PageUtils from "../util/PageUtils";
+import PersonalStatusPage from "./PersonalStatusPage";
+import RoleParser from "./RoleParser";
 
 class PersonalStatus {
 
@@ -8,7 +11,16 @@ class PersonalStatus {
         this.#credential = credential;
     }
 
+    static parsePage(pageHtml: string): PersonalStatusPage {
+        return doParsePage(pageHtml);
+    }
+}
 
+function doParsePage(pageHtml: string): PersonalStatusPage {
+    const credential = PageUtils.parseCredential(pageHtml);
+    const page = new PersonalStatusPage(credential);
+    page.role = RoleParser.parseRole(pageHtml);
+    return page;
 }
 
 export = PersonalStatus;
--- src/pocket/PersonalStatusPage.ts
+++ src/pocket/PersonalStatusPage.ts
@@ -1,8 +1,11 @@
 import Credential from "../util/Credential";
+import Role from "./Role";
 
 class PersonalStatusPage {
 
     readonly #credential: Credential;
+
+    role?: Role;
 
     constructor(credential: Credential) {
         this.#credential = credential;
