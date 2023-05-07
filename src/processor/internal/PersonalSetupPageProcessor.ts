39ca43c8143878afed93a552be24063116c691c6
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-07 21:54:40 +0800

    城堡版助手设置页面。

--- src/processor/internal/PersonalSetupPageProcessor.ts
+++ src/processor/internal/PersonalSetupPageProcessor.ts
@@ -1,5 +1,3 @@
-import PageProcessor from "../PageProcessor";
-import PageUtils from "../../util/PageUtils";
 import SetupItemManager from "../personal/setup/SetupItemManager";
 import EquipmentLoader from "../../pocket/EquipmentLoader";
 import Equipment from "../../pocket/Equipment";
@@ -7,20 +5,20 @@ import Credential from "../../util/Credential";
 import NetworkUtils from "../../util/NetworkUtils";
 import EquipmentParser from "../../pocket/EquipmentParser";
 import TreasureBag from "../../pocket/TreasureBag";
+import PageProcessorSupport from "../PageProcessorSupport";
+import RoleLoader from "../../pocket/RoleLoader";
+import MessageBoard from "../../util/MessageBoard";
 
-abstract class PersonalSetupPageProcessor implements PageProcessor {
+abstract class PersonalSetupPageProcessor extends PageProcessorSupport {
 
     readonly #setupItemManager: SetupItemManager;
 
     constructor() {
+        super();
         this.#setupItemManager = new SetupItemManager();
     }
 
-    process(): void {
-        PageUtils.removeUnusedHyperLinks();
-        PageUtils.removeGoogleAnalyticsScript();
-        const credential = PageUtils.currentCredential();
-
+    doProcess(credential: Credential) {
         // 整个页面是放在一个大form里面，删除重组
         const lastDivHtml = $("div:last").html();
         $("form:first").remove();
@@ -68,55 +66,96 @@ abstract class PersonalSetupPageProcessor implements PageProcessor {
         $("#accessory_list").text(EquipmentLoader.loadAccessoryList().join(","));
 
 
-        _bindLoadButton(credential);
+        this.#bindLoadButton(credential);
+        this.#bindRefreshButton(credential);
         this.doBindReturnButton("returnButton");
+
+        new RoleLoader(credential).load()
+            .then(role => {
+                MessageBoard.createMessageBoard("message_board_container", role.imageHtml);
+
+                $("#roleImage").on("dblclick", function () {
+                    // Get setup 001-005 status
+                    const s1 = $("#select_001").val();
+                    const s2 = $("#select_002").val();
+                    const s3 = $("#select_003").val();
+                    const s4 = $("#select_004").val();
+                    const s5 = $("#select_005").val();
+                    if (s1 === "1" &&
+                        s2 === "0.2" &&
+                        s3 === "20" &&
+                        s4 === "20" &&
+                        s5 === "2") {
+                        if ($("#battle_field_setup").length > 0) {
+                            $("#battle_field_setup").toggle();
+                        }
+                    }
+
+                    $(".hidden_setup_item").toggle();
+                });
+            });
+
+        this.#render(credential);
     }
 
-    abstract doGenerateHiddenForm(credential: Credential, containerId: string): void;
+    #render(credential: Credential) {
+        let html = "";
+        html += "<table style='background-color:#888888;width:100%;text-align:center'>";
+        html += "<tbody style='background-color:#F8F0E0' id='setup_item_table'>";
+        html += "<tr>";
+        html += "<th style='background-color:#E8E8D0'>名字</th>";
+        html += "<th style='background-color:#E8E8D0'>专属</th>";
+        html += "<th style='background-color:#EFE0C0'>设置</th>";
+        html += "<th style='background-color:#E0D0B0'>选择</th>";
+        html += "</tr>";
+        html += "</tbody>";
+        html += "</table>";
+        $("#setup_item_container").html(html);
 
-    abstract doBindReturnButton(returnButtonId: string): void;
-}
+        for (const it of this.#setupItemManager.getSetupItem()) {
+            it.render(credential.id);
+        }
 
-function _bindLoadButton(credential: Credential) {
-    $("#loadButton").on("click", function () {
-        const request = credential.asRequest();
-        // @ts-ignore
-        request["mode"] = "USE_ITEM";
-        NetworkUtils.sendPostRequest("mydata.cgi", request, function (html) {
-            const equipmentList = EquipmentParser.parsePersonalItemList(html);
-            const treasureBag = EquipmentParser.findTreasureBag(equipmentList);
-            if (treasureBag !== null) {
-                new TreasureBag(credential, treasureBag.index!).open()
-                    .then(bagEquipmentList => {
-                        _loadEquipments(equipmentList, bagEquipmentList);
-                        $("#refreshButton").trigger("click");
-                    });
-            } else {
-                _loadEquipments(equipmentList, undefined);
-                $("#refreshButton").trigger("click");
-            }
+    }
+
+    #bindLoadButton(credential: Credential) {
+        const instance = this;
+        $("#loadButton").on("click", function () {
+            const request = credential.asRequest();
+            // @ts-ignore
+            request["mode"] = "USE_ITEM";
+            NetworkUtils.sendPostRequest("mydata.cgi", request, function (html) {
+                const equipmentList = EquipmentParser.parsePersonalItemList(html);
+                const treasureBag = EquipmentParser.findTreasureBag(equipmentList);
+                if (treasureBag !== null) {
+                    new TreasureBag(credential, treasureBag.index!).open()
+                        .then(bagEquipmentList => {
+                            instance.#loadEquipments(equipmentList, bagEquipmentList);
+                            $("#refreshButton").trigger("click");
+                        });
+                } else {
+                    instance.#loadEquipments(equipmentList, undefined);
+                    $("#refreshButton").trigger("click");
+                }
+            });
         });
-    });
-}
-
-function _loadEquipments(itemList: Equipment[], bagItemList?: Equipment[]) {
-    const weaponLib = [];
-    const armorLib = [];
-    const accessoryLib = [];
-
-    for (const it of itemList) {
-        if (it.isWeapon) {
-            weaponLib.push(it.name);
-        }
-        if (it.isArmor) {
-            armorLib.push(it.name);
-        }
-        if (it.isAccessory) {
-            accessoryLib.push(it.name);
-        }
     }
-    if (bagItemList !== undefined) {
-        for (const it of bagItemList) {
+
+    #bindRefreshButton(credential: Credential) {
+        const instance = this;
+        $("#refreshButton").on("click", function () {
+            $("#setup_item_table").html("");
+            $(".dynamic_button").off("click");
+            instance.#render(credential);
+        });
+    }
+
+    #loadEquipments(itemList: Equipment[], bagItemList?: Equipment[]) {
+        const weaponLib = [];
+        const armorLib = [];
+        const accessoryLib = [];
+
+        for (const it of itemList) {
             if (it.isWeapon) {
                 weaponLib.push(it.name);
             }
@@ -127,31 +166,48 @@ function _loadEquipments(itemList: Equipment[], bagItemList?: Equipment[]) {
                 accessoryLib.push(it.name);
             }
         }
+        if (bagItemList !== undefined) {
+            for (const it of bagItemList) {
+                if (it.isWeapon) {
+                    weaponLib.push(it.name);
+                }
+                if (it.isArmor) {
+                    armorLib.push(it.name);
+                }
+                if (it.isAccessory) {
+                    accessoryLib.push(it.name);
+                }
+            }
+        }
+
+        const weaponCandidates = [];
+        const armorCandidates = [];
+        const accessoryCandidates = [];
+
+        for (const it of EquipmentLoader.loadWeaponList()) {
+            if (weaponLib.includes(it)) {
+                weaponCandidates.push(it);
+            }
+        }
+        for (const it of EquipmentLoader.loadArmorList()) {
+            if (armorLib.includes(it)) {
+                armorCandidates.push(it);
+            }
+        }
+        for (const it of EquipmentLoader.loadAccessoryList()) {
+            if (accessoryLib.includes(it)) {
+                accessoryCandidates.push(it);
+            }
+        }
+
+        $("#weapon_list").text(weaponCandidates.join(","));
+        $("#armor_list").text(armorCandidates.join(","));
+        $("#accessory_list").text(accessoryCandidates.join(","));
     }
 
-    const weaponCandidates = [];
-    const armorCandidates = [];
-    const accessoryCandidates = [];
+    abstract doGenerateHiddenForm(credential: Credential, containerId: string): void;
 
-    for (const it of EquipmentLoader.loadWeaponList()) {
-        if (weaponLib.includes(it)) {
-            weaponCandidates.push(it);
-        }
-    }
-    for (const it of EquipmentLoader.loadArmorList()) {
-        if (armorLib.includes(it)) {
-            armorCandidates.push(it);
-        }
-    }
-    for (const it of EquipmentLoader.loadAccessoryList()) {
-        if (accessoryLib.includes(it)) {
-            accessoryCandidates.push(it);
-        }
-    }
-
-    $("#weapon_list").text(weaponCandidates.join(","));
-    $("#armor_list").text(armorCandidates.join(","));
-    $("#accessory_list").text(accessoryCandidates.join(","));
+    abstract doBindReturnButton(returnButtonId: string): void;
 }
 
 export = PersonalSetupPageProcessor;
