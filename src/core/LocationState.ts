bac9904b4d0f900f9a4d90b58ca1d6f63c09789e
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-07 11:17:34 +0800

    LocationState

--- src/core/LocationState.ts
+++ src/core/LocationState.ts
@@ -2,8 +2,37 @@ class LocationState {
 
     readonly #location: string | null;
 
+    inTownHandler?: () => void;
+    inCastleHandler?: () => void;
+    inWildHandler?: () => void;
+
     constructor(location: string | null) {
         this.#location = location;
+    }
+
+    whenInTown(handler: () => void) {
+        this.inTownHandler = handler;
+    }
+
+    whenInCastle(handler: () => void) {
+        this.inCastleHandler = handler;
+    }
+
+    whenInWild(handler: () => void) {
+        this.inWildHandler = handler;
+    }
+
+    fork() {
+        if (this.#location === "TOWN") {
+            this.inTownHandler?.();
+        }
+        if (this.#location === "CASTLE") {
+            this.inCastleHandler?.();
+        }
+        if (this.#location === "WILD") {
+            this.inWildHandler?.();
+        }
+        // Do nothing in case of location is null.
     }
 }
 
