ea501d2b813b41b94bfefa01554a9991b65835ac
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-08 22:56:57 +0800

    TownBankPageProcessor

---
+++ src/processor/internal/TownBankPageProcessor.ts
@@ -0,0 +1,12 @@
+import Credential from "../../util/Credential";
+import PageProcessorContext from "../PageProcessorContext";
+import PageProcessorSupport from "../PageProcessorSupport";
+
+class TownBankPageProcessor extends PageProcessorSupport {
+
+    doProcess(credential: Credential, context?: PageProcessorContext): void {
+    }
+
+}
+
+export = TownBankPageProcessor;
