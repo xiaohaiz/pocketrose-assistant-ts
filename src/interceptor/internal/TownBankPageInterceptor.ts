314da0f6024adc7b1e2e6cbf0c3912e7866117d5
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-08 23:02:37 +0800

    TownBankPageInterceptor

---
+++ src/interceptor/internal/TownBankPageInterceptor.ts
@@ -0,0 +1,34 @@
+import LocationStateMachine from "../../core/LocationStateMachine";
+import SetupLoader from "../../core/SetupLoader";
+import TownBankPageProcessor from "../../processor/internal/TownBankPageProcessor";
+import PageProcessorContext from "../../processor/PageProcessorContext";
+import PageInterceptor from "../PageInterceptor";
+
+class TownBankPageInterceptor implements PageInterceptor {
+
+    readonly #processor = new TownBankPageProcessor();
+
+    accept(cgi: string, pageText: string): boolean {
+        if (cgi === "town.cgi") {
+            return pageText.includes("存入或取出请输入数额后按下确认键");
+        }
+        return false;
+    }
+
+    intercept(): void {
+        if (!SetupLoader.isPocketBankEnabled()) {
+            return;
+        }
+        LocationStateMachine.create()
+            .load()
+            .whenInTown(townId => {
+                const context = new PageProcessorContext();
+                context.set("townId", townId!);
+                this.#processor.process(context);
+            })
+            .fork();
+    }
+
+}
+
+export = TownBankPageInterceptor;
