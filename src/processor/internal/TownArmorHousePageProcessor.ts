73d23f22ff34c68b87a17bd4c2a2c9c759e02ecc
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-09 17:51:46 +0800

    绘制防具屋的基本页面架构。

--- src/processor/internal/TownArmorHousePageProcessor.ts
+++ src/processor/internal/TownArmorHousePageProcessor.ts
@@ -17,6 +17,85 @@ class TownArmorHousePageProcessor extends PageProcessorCredentialSupport {
     }
 
     #renderImmutablePage(credential: Credential, town: Town) {
+        // 重新绘制页面框架
+        $("table:eq(1)")
+            .attr("id", "t1")
+            .find("td:first")
+            .attr("id", "title_cell")
+            .removeAttr("width")
+            .removeAttr("height")
+            .removeAttr("bgcolor")
+            .css("text-align", "center")
+            .css("font-size", "150%")
+            .css("font-weight", "bold")
+            .css("background-color", "navy")
+            .css("color", "yellowgreen")
+            .text("＜＜  " + town.nameTitle + " 防 具 屋  ＞＞")
+            .parent()
+            .next()
+            .find("table:first")
+            .find("td:eq(1)")
+            .find("table:first")
+            .find("td:last")
+            .attr("id", "roleCash");
+
+        // 删除原有页面所有的表单
+        $("form").remove();
+
+        // 消息面板
+        $("#t1")
+            .find("tr:first")
+            .next()
+            .next()
+            .find("table:first")
+            .find("td:first")
+            .attr("id", "messageBoard")
+            .css("color", "white")
+            .next()
+            .attr("id", "messageBoardManager");
+
+        // 清空原来的商品栏，保留行作为新的UI的位置
+        $("#t1")
+            .find("tr:first")
+            .next()
+            .next()
+            .next()
+            .html("<td id='armor_store_UI' style='width:100%;background-color:#F8F0E0'></td>");
+
+        // 绘制新的内容
+        let html = "";
+        html += "<table style='width:100%;border-width:0;background-color:#888888;margin:auto'>";
+        html += "<tbody>";
+        // ------------------------------------------------------------------------
+        // 隐藏的表单
+        // ------------------------------------------------------------------------
+        html += "<tr style='display:none'>";
+        html += "<td id='hidden_form_cell'></td>";
+        html += "</tr>";
+        // ------------------------------------------------------------------------
+        // 主菜单
+        // ------------------------------------------------------------------------
+        html += "<tr>";
+        html += "<td style='background-color:#F8F0E0;text-align:center'>";
+        html += "<input type='button' id='refresh_button' value='刷新" + town.name + "防具屋'>";
+        html += "<input type='button' id='return_button' value='离开" + town.name + "防具屋'>";
+        html += "</td>";
+        // ------------------------------------------------------------------------
+        // 个人物品栏
+        // ------------------------------------------------------------------------
+        html += "<tr style='display:none'>";
+        html += "<td id='personal_equipment_list_cell'></td>";
+        html += "</tr>";
+        // ------------------------------------------------------------------------
+        // 武器屋商品栏
+        // ------------------------------------------------------------------------
+        html += "<tr style='display:none'>";
+        html += "<td id='armor_merchandise_list_cell'></td>";
+        html += "</tr>";
+        html += "</tbody>";
+        html += "</table>";
+
+        $("#armor_store_UI").html(html);
     }
 
     #renderMutablePage(credential: Credential, page: TownArmorHousePage, town: Town) {
