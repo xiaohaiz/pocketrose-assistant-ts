55ed354135bedf4bbe410b77f2f65911ddec15c8
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-09 09:01:46 +0800

    TownBank.withdraw

--- src/pocketrose/TownBank.ts
+++ src/pocketrose/TownBank.ts
@@ -60,6 +60,35 @@ class TownBank {
         return await action();
     }
 
+    async withdraw(amount: number): Promise<void> {
+        const action = () => {
+            return new Promise<void>((resolve, reject) => {
+                if (!BankUtils.checkAmountAvailability(amount)) {
+                    MessageBoard.publishWarning("无效的金额，必须是零或者正整数！");
+                    reject();
+                } else {
+                    if (amount === 0) {
+                        resolve();
+                    } else {
+                        const request = this.#credential.asRequestMap();
+                        request.set("dasu", amount.toString());
+                        request.set("mode", "BANK_BUY");
+                        NetworkUtils.post("town.cgi", request).then(html => {
+                            if (html.includes("您在钱庄里没那么多存款")) {
+                                MessageBoard.publishWarning("并没有那么多存款！");
+                                reject();
+                            } else {
+                                MessageBoard.publishMessage("从" + this.bankTitle + "取现了" + amount + "万现金。");
+                                resolve();
+                            }
+                        });
+                    }
+                }
+            });
+        };
+        return await action();
+    }
+
     async deposit(amount?: number): Promise<void> {
         const action = () => {
             return new Promise<void>((resolve, reject) => {
