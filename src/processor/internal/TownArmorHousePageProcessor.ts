b171fa5287ef6046878aa522e933caad1459f4a8
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-09 17:42:24 +0800

    TownArmorHousePageProcessor

--- src/processor/internal/TownArmorHousePageProcessor.ts
+++ src/processor/internal/TownArmorHousePageProcessor.ts
@@ -1,5 +1,7 @@
+import Town from "../../core/Town";
 import TownLoader from "../../core/TownLoader";
 import TownArmorHouse from "../../pocketrose/TownArmorHouse";
+import TownArmorHousePage from "../../pocketrose/TownArmorHousePage";
 import Credential from "../../util/Credential";
 import PageUtils from "../../util/PageUtils";
 import PageProcessorContext from "../PageProcessorContext";
@@ -10,8 +12,15 @@ class TownArmorHousePageProcessor extends PageProcessorCredentialSupport {
     doProcess(credential: Credential, context?: PageProcessorContext): void {
         const page = TownArmorHouse.parsePage(PageUtils.currentPageHtml());
         const town = TownLoader.getTownById(page.townId!)!;
+        this.#renderImmutablePage(credential, town);
+        this.#renderMutablePage(credential, page, town);
     }
 
+    #renderImmutablePage(credential: Credential, town: Town) {
+    }
+
+    #renderMutablePage(credential: Credential, page: TownArmorHousePage, town: Town) {
+    }
 }
 
 export = TownArmorHousePageProcessor;
