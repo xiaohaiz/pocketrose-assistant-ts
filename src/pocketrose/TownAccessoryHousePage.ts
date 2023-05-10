ee49b82f5901714f9c3eb66cb5557f2869dc59a7
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-10 08:00:04 +0800

    TownAccessoryHouse

---
+++ src/pocketrose/TownAccessoryHousePage.ts
@@ -0,0 +1,39 @@
+import Equipment from "../common/Equipment";
+import Merchandise from "../common/Merchandise";
+import Role from "../common/Role";
+import Town from "../core/Town";
+import TownLoader from "../core/TownLoader";
+
+class TownAccessoryHousePage {
+
+    townId?: string;
+    discount?: number;
+    role?: Role;
+    equipmentList?: Equipment[];
+    merchandiseList?: Merchandise[];
+
+    get town(): Town {
+        return TownLoader.getTownById(this.townId!)!;
+    }
+
+    findEquipment(index: number) {
+        for (const equipment of this.equipmentList!) {
+            if (equipment.index === index) {
+                return equipment;
+            }
+        }
+        return null;
+    }
+
+    findMerchandise(index: number) {
+        for (const merchandise of this.merchandiseList!) {
+            if (merchandise.index === index) {
+                return merchandise;
+            }
+        }
+        return null;
+    }
+
+}
+
+export = TownAccessoryHousePage;
