c4b9b98824f428d959ec0d95e312140a4477aa8e
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-11 10:06:36 +0800

    解析随时装备信息。

--- src/pocketrose/PersonalEquipmentManagement.ts
+++ src/pocketrose/PersonalEquipmentManagement.ts
@@ -1,3 +1,4 @@
+import Equipment from "../common/Equipment";
 import Role from "../common/Role";
 import Credential from "../util/Credential";
 import StringUtils from "../util/StringUtils";
@@ -64,7 +65,74 @@ function __parsePage(html: string): PersonalEquipmentManagementPage {
 
     const page = new PersonalEquipmentManagementPage();
     page.role = role;
+    page.equipmentList = __parseEquipmentList(html);
     return page;
+}
+
+function __parseEquipmentList(html: string) {
+    const equipmentList: Equipment[] = [];
+    $(html).find("input:checkbox").each(function (_idx, checkbox) {
+        const equipment = new Equipment();
+        const tr = $(checkbox).parent().parent();
+
+        // index & selectable
+        equipment.index = parseInt($(checkbox).val() as string);
+        equipment.selectable = !$(checkbox).prop("disabled");
+
+        // using
+        let s = $(tr).find("th:first").text();
+        equipment.using = (s === "★");
+
+        // name & star
+        equipment.parseName($(tr).find("td:eq(1)").html());
+
+        // category
+        s = $(tr).find("td:eq(2)").text();
+        equipment.category = s;
+
+        // power & weight & endure
+        s = $(tr).find("td:eq(3)").text();
+        equipment.power = parseInt(s);
+        s = $(tr).find("td:eq(4)").text();
+        equipment.weight = parseInt(s);
+        s = $(tr).find("td:eq(5)").text();
+        equipment.endure = parseInt(s);
+
+        // required career
+        s = $(tr).find("td:eq(6)").text();
+        equipment.requiredCareer = s;
+
+        // required stats
+        s = $(tr).find("td:eq(7)").text();
+        equipment.requiredAttack = parseInt(s);
+        s = $(tr).find("td:eq(8)").text();
+        equipment.requiredDefense = parseInt(s);
+        s = $(tr).find("td:eq(9)").text();
+        equipment.requiredSpecialAttack = parseInt(s);
+        s = $(tr).find("td:eq(10)").text();
+        equipment.requiredSpecialDefense = parseInt(s);
+        s = $(tr).find("td:eq(11)").text();
+        equipment.requiredSpeed = parseInt(s);
+
+        // additional
+        s = $(tr).find("td:eq(12)").text();
+        equipment.additionalPower = parseInt(s);
+        s = $(tr).find("td:eq(13)").text();
+        equipment.additionalWeight = parseInt(s);
+        s = $(tr).find("td:eq(14)").text();
+        equipment.additionalLuck = parseInt(s);
+
+        // experience
+        s = $(tr).find("td:eq(15)").text();
+        equipment.experience = parseInt(s);
+
+        // attribute
+        s = $(tr).find("td:eq(16)").text();
+        equipment.attribute = s;
+
+        equipmentList.push(equipment);
+    });
+    return equipmentList;
 }
 
 export = PersonalEquipmentManagement;
--- src/pocketrose/PersonalEquipmentManagementPage.ts
+++ src/pocketrose/PersonalEquipmentManagementPage.ts
@@ -1,8 +1,10 @@
+import Equipment from "../common/Equipment";
 import Role from "../common/Role";
 
 class PersonalEquipmentManagementPage {
 
     role?: Role;
+    equipmentList?: Equipment[];
 
 }
 
