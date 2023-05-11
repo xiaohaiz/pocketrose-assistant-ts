48129d932d4830fa817ba5f84cf0a6ae76f74f9b
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-11 19:49:10 +0800

    AbstractPersonalEquipmentManagementPageProcessor

--- src/processor/internal/AbstractPersonalEquipmentManagementPageProcessor.ts
+++ src/processor/internal/AbstractPersonalEquipmentManagementPageProcessor.ts
@@ -1,12 +1,40 @@
+import PersonalEquipmentManagement from "../../pocketrose/PersonalEquipmentManagement";
+import PersonalEquipmentManagementPage from "../../pocketrose/PersonalEquipmentManagementPage";
 import Credential from "../../util/Credential";
+import PageUtils from "../../util/PageUtils";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
 
 abstract class AbstractPersonalEquipmentManagementPageProcessor extends PageProcessorCredentialSupport {
 
     doProcess(credential: Credential, context?: PageProcessorContext) {
+        const page = PersonalEquipmentManagement.parsePage(PageUtils.currentPageHtml());
+        this.#renderImmutablePage(credential, page, context);
     }
 
+    #renderImmutablePage(credential: Credential, page: PersonalEquipmentManagementPage, context?: PageProcessorContext) {
+        // 删除不必要的属性，可能会造成显示格式混乱。
+        $("table[height='100%']").removeAttr("height");
+
+        // 定位第一个表格
+        const t0 = $("table:first")
+            .attr("id", "t0");
+
+        // 标题栏 : pageTitle
+        t0.find("td:first")
+            .attr("id", "pageTitle")
+            .removeAttr("width")
+            .removeAttr("height")
+            .removeAttr("bgcolor")
+            .css("text-align", "center")
+            .css("font-size", "150%")
+            .css("font-weight", "bold")
+            .css("background-color", "navy")
+            .css("color", "yellowgreen")
+            .html(this.doGeneratePageTitleHtml(context));
+    }
+
+    abstract doGeneratePageTitleHtml(context?: PageProcessorContext): string;
 }
 
 export = AbstractPersonalEquipmentManagementPageProcessor;
--- src/processor/internal/PersonalEquipmentManagementPageProcessor_Castle.ts
+++ src/processor/internal/PersonalEquipmentManagementPageProcessor_Castle.ts
@@ -1,39 +1,18 @@
-import PersonalEquipmentManagement from "../../pocketrose/PersonalEquipmentManagement";
-import PersonalEquipmentManagementPage from "../../pocketrose/PersonalEquipmentManagementPage";
-import Credential from "../../util/Credential";
-import PageUtils from "../../util/PageUtils";
 import StringUtils from "../../util/StringUtils";
 import PageProcessorContext from "../PageProcessorContext";
-import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
+import AbstractPersonalEquipmentManagementPageProcessor from "./AbstractPersonalEquipmentManagementPageProcessor";
 
-class PersonalEquipmentManagementPageProcessor_Castle extends PageProcessorCredentialSupport {
+class PersonalEquipmentManagementPageProcessor_Castle extends AbstractPersonalEquipmentManagementPageProcessor {
 
-    doProcess(credential: Credential, context?: PageProcessorContext): void {
-        const page = PersonalEquipmentManagement.parsePage(PageUtils.currentPageHtml());
-        this.#renderImmutablePage(credential, page, context!.get("castleName")!);
+    doGeneratePageTitleHtml(context?: PageProcessorContext): string {
+        if (context === undefined) {
+            return "＜＜  装 备 管 理 （ 城 堡 模 式 ）  ＞＞";
+        } else {
+            const castleName = context.get("castleName")!;
+            return "＜＜  装 备 管 理 （ " + StringUtils.toTitleString(castleName) + " 城 堡 ）  ＞＞";
+        }
     }
 
-    #renderImmutablePage(credential: Credential, page: PersonalEquipmentManagementPage, castleName: string) {
-        // 删除不必要的属性，可能会造成显示格式混乱。
-        $("table[height='100%']").removeAttr("height");
-
-        // 定位第一个表格
-        const t0 = $("table:first")
-            .attr("id", "t0");
-
-        // 标题栏 : pageTitle
-        t0.find("td:first")
-            .attr("id", "pageTitle")
-            .removeAttr("width")
-            .removeAttr("height")
-            .removeAttr("bgcolor")
-            .css("text-align", "center")
-            .css("font-size", "150%")
-            .css("font-weight", "bold")
-            .css("background-color", "navy")
-            .css("color", "yellowgreen")
-            .text("＜＜  装 备 管 理 （ " + StringUtils.toTitleString(castleName) + " 城 堡 ）  ＞＞");
-    }
 
 }
 
