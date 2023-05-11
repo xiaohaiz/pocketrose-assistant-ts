61b85f9bc060191fc90a7407c80deda16f1ac920
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-11 10:13:27 +0800

    标题栏 : pageTitle

--- src/processor/internal/PersonalEquipmentManagementPageProcessor_Castle.ts
+++ src/processor/internal/PersonalEquipmentManagementPageProcessor_Castle.ts
@@ -1,6 +1,8 @@
 import PersonalEquipmentManagement from "../../pocketrose/PersonalEquipmentManagement";
+import PersonalEquipmentManagementPage from "../../pocketrose/PersonalEquipmentManagementPage";
 import Credential from "../../util/Credential";
 import PageUtils from "../../util/PageUtils";
+import StringUtils from "../../util/StringUtils";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
 
@@ -8,6 +10,29 @@ class PersonalEquipmentManagementPageProcessor_Castle extends PageProcessorCrede
 
     doProcess(credential: Credential, context?: PageProcessorContext): void {
         const page = PersonalEquipmentManagement.parsePage(PageUtils.currentPageHtml());
+        this.#renderImmutablePage(credential, page, context!.get("castleName")!);
+    }
+
+    #renderImmutablePage(credential: Credential, page: PersonalEquipmentManagementPage, castleName: string) {
+        // 删除不必要的属性，可能会造成显示格式混乱。
+        $("table[height='100%']").removeAttr("height");
+
+        // 定位第一个表格
+        const t0 = $("table:first")
+            .attr("id", "t0");
+
+        // 标题栏 : pageTitle
+        t0.find("td:first")
+            .attr("id", "pageTitle")
+            .removeAttr("width")
+            .removeAttr("height")
+            .removeAttr("bgcolor")
+            .css("text-align", "center")
+            .css("font-size", "150%")
+            .css("font-weight", "bold")
+            .css("background-color", "navy")
+            .css("color", "yellowgreen")
+            .text("＜＜  装 备 管 理 （ " + StringUtils.toTitleString(castleName) + " 城 堡 ）  ＞＞");
     }
 
 }
