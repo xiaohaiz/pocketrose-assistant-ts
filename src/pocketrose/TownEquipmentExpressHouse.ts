8cdaad389c310835fdcb4c0289daa95e78c04100
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-12 23:31:19 +0800

    解析role

--- src/pocketrose/TownEquipmentExpressHouse.ts
+++ src/pocketrose/TownEquipmentExpressHouse.ts
@@ -1,9 +1,43 @@
+import Role from "../common/Role";
+import StringUtils from "../util/StringUtils";
 import TownEquipmentExpressHousePage from "./TownEquipmentExpressHousePage";
 
 class TownEquipmentExpressHouse {
 
     static parsePage(html: string): TownEquipmentExpressHousePage {
+        const role = new Role();
+        $(html).find("td:contains('ＬＶ')")
+            .filter((idx, td) => $(td).text() === "ＬＶ")
+            .closest("tr")
+            .next()
+            .find("td:first")
+            .each((idx, td) => {
+                role.name = $(td).text();
+            })
+            .next()
+            .each((idx, td) => {
+                role.level = parseInt($(td).text());
+            })
+            .next()
+            .each((idx, td) => {
+                role.attribute = StringUtils.substringBefore($(td).text(), "属");
+            })
+            .next()
+            .each((idx, td) => {
+                role.attribute = $(td).text();
+            })
+            .parent()
+            .next()
+            .find("td:first")
+            .next()
+            .each((idx, td) => {
+                let s = $(td).text();
+                s = StringUtils.substringBefore(s, " GOLD");
+                role.cash = parseInt(s);
+            })
+
         const page = new TownEquipmentExpressHousePage();
+        page.role = role;
         return page;
     }
 }
