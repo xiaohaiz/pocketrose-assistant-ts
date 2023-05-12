347bdb2d3edf48bd599c07ccf879b301fece03a2
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-13 07:32:24 +0800

    TownEquipmentExpressHouse.open

--- src/pocketrose/TownEquipmentExpressHouse.ts
+++ src/pocketrose/TownEquipmentExpressHouse.ts
@@ -1,8 +1,36 @@
+import Equipment from "../common/Equipment";
 import Role from "../common/Role";
+import Credential from "../util/Credential";
+import NetworkUtils from "../util/NetworkUtils";
 import StringUtils from "../util/StringUtils";
 import TownEquipmentExpressHousePage from "./TownEquipmentExpressHousePage";
 
 class TownEquipmentExpressHouse {
+
+    readonly #credential: Credential;
+    readonly #townId?: string;
+
+    constructor(credential: Credential, townId?: string) {
+        this.#credential = credential;
+        this.#townId = townId;
+    }
+
+    async open(): Promise<TownEquipmentExpressHousePage> {
+        return await (() => {
+            return new Promise<TownEquipmentExpressHousePage>(resolve => {
+                const request = this.#credential.asRequestMap();
+                if (this.#townId !== undefined) {
+                    request.set("town", this.#townId);
+                }
+                request.set("con_str", "50");
+                request.set("mode", "ITEM_SEND");
+                NetworkUtils.post("town.cgi", request).then(html => {
+                    const page = TownEquipmentExpressHouse.parsePage(html);
+                    resolve(page);
+                });
+            });
+        })();
+    }
 
     static parsePage(html: string): TownEquipmentExpressHousePage {
         const role = new Role();
@@ -34,10 +62,34 @@ class TownEquipmentExpressHouse {
                 let s = $(td).text();
                 s = StringUtils.substringBefore(s, " GOLD");
                 role.cash = parseInt(s);
-            })
+            });
+
+        const equipmentList: Equipment[] = [];
+        $(html).find("input:checkbox")
+            .each((idx, checkbox) => {
+                const equipment = new Equipment();
+                let s = $(checkbox).val() as string;
+                equipment.index = parseInt(s);
+                equipment.selectable = !$(checkbox).prop("disabled");
+                const c1 = $(checkbox).parent();
+                const c2 = c1.next();
+                const c3 = c2.next();
+                const c4 = c3.next();
+                const c5 = c4.next();
+                const c6 = c5.next();
+
+                equipment.using = c2.text() === "★";
+                equipment.parseName(c3.html());
+                equipment.category = c4.text();
+                equipment.power = parseInt(c5.text());
+                equipment.weight = parseInt(c6.text());
+
+                equipmentList.push(equipment);
+            });
 
         const page = new TownEquipmentExpressHousePage();
         page.role = role;
+        page.equipmentList = equipmentList;
         return page;
     }
 }
