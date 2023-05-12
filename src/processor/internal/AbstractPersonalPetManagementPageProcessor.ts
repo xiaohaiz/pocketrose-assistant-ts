c79c9569b7db1a38cf7dc726b35cf33ef3ad44b0
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-12 22:40:18 +0800

    AbstractPersonalPetManagementPageProcessor

--- src/processor/internal/AbstractPersonalPetManagementPageProcessor.ts
+++ src/processor/internal/AbstractPersonalPetManagementPageProcessor.ts
@@ -1,10 +1,13 @@
+import PersonalPetManagement from "../../pocketrose/PersonalPetManagement";
 import Credential from "../../util/Credential";
+import PageUtils from "../../util/PageUtils";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
 
 abstract class AbstractPersonalPetManagementPageProcessor extends PageProcessorCredentialSupport {
 
     doProcess(credential: Credential, context?: PageProcessorContext) {
+        const page = PersonalPetManagement.parsePage(PageUtils.currentPageHtml());
     }
 
 }
