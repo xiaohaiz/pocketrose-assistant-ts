286e16326fd65f9ce5d0df862ff179ac7e512a68
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-13 20:34:40 +0800

    PersonalPetManagementPageInterceptor

--- src/interceptor/internal/PersonalPetManagementPageInterceptor.ts
+++ src/interceptor/internal/PersonalPetManagementPageInterceptor.ts
@@ -1,9 +1,16 @@
-import PageInterceptor from "../PageInterceptor";
-import SetupLoader from "../../core/SetupLoader";
 import LocationStateMachine from "../../core/LocationStateMachine";
-import PersonalPetManagementPageProcessor from "../../processor/internal/PersonalPetManagementPageProcessor";
+import SetupLoader from "../../core/SetupLoader";
+import PersonalPetManagementPageProcessor_Castle
+    from "../../processor/internal/PersonalPetManagementPageProcessor_Castle";
+import PersonalPetManagementPageProcessor_Town from "../../processor/internal/PersonalPetManagementPageProcessor_Town";
+import PageProcessorContext from "../../processor/PageProcessorContext";
+import PageInterceptor from "../PageInterceptor";
 
 class PersonalPetManagementPageInterceptor implements PageInterceptor {
+
+    readonly #inTownProcessor = new PersonalPetManagementPageProcessor_Town();
+    readonly #inCastleProcessor = new PersonalPetManagementPageProcessor_Castle();
+
     accept(cgi: string, pageText: string): boolean {
         if (cgi === "mydata.cgi") {
             return pageText.includes("宠物现在升级时学习新技能情况一览");
@@ -17,11 +24,15 @@ class PersonalPetManagementPageInterceptor implements PageInterceptor {
         }
         LocationStateMachine.create()
             .load()
-            .whenInTown(() => {
-                new PersonalPetManagementPageProcessor().process();
+            .whenInTown(townId => {
+                const context = new PageProcessorContext();
+                context.set("townId", townId!);
+                this.#inTownProcessor.process(context);
             })
-            .whenInCastle(() => {
-                new PersonalPetManagementPageProcessor().process();
+            .whenInCastle(castleName => {
+                const context = new PageProcessorContext();
+                context.set("castleName", castleName!);
+                this.#inCastleProcessor.process(context);
             })
             .fork();
     }
