3251c48a27028747b218ab35dd5e8abdd3e7c733
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-13 20:52:27 +0800

    CastlePetExpressHouse

---
+++ src/pocketrose/CastlePetExpressHouse.ts
@@ -0,0 +1,28 @@
+import Credential from "../util/Credential";
+import NetworkUtils from "../util/NetworkUtils";
+
+class CastlePetExpressHouse {
+
+    readonly #credential: Credential;
+
+    constructor(credential: Credential) {
+        this.#credential = credential;
+    }
+
+    async search(searchName: string): Promise<string> {
+        return await (() => {
+            return new Promise<string>((resolve, reject) => {
+                const request = this.#credential.asRequestMap();
+                // noinspection JSDeprecatedSymbols
+                request.set("serch", escape(searchName.trim()));
+                request.set("mode", "CASTLE_SENDPET");
+                NetworkUtils.post("castle.cgi", request).then(html => {
+                    const selectHtml = $(html).find("select:file").html();
+                    resolve(selectHtml);
+                });
+            });
+        })();
+    }
+}
+
+export = CastlePetExpressHouse;
