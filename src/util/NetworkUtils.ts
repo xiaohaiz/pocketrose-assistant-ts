21949ff47d1bf0b7e97456a8a88aa90068828eaa
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-14 18:10:36 +0800

    __internalSendPostRequest

--- src/util/NetworkUtils.ts
+++ src/util/NetworkUtils.ts
@@ -74,4 +74,35 @@ class NetworkUtils {
     }
 }
 
+function __internalSendPostRequest(count: number, cgi: string, request: {}, handler?: (html: string) => void) {
+    if (count === 3) {
+        MessageBoard.publishWarning("请求" + cgi + "达到最大重试次数，依然失败，看起来口袋出问题了，请联系GM！");
+        return;
+    }
+    fetch(cgi, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams(request),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                MessageBoard.publishWarning("请求" + cgi + "时返回错误[status=" + response.status + "]，尝试重试！");
+                throw new Error("RESPONSE was not ok");
+            }
+            return response.arrayBuffer();
+        })
+        .then((arrayBuffer) => {
+            const decoder = new TextDecoder("gb2312");
+            const html = decoder.decode(new Uint8Array(arrayBuffer));
+            if (handler !== undefined) {
+                handler(html);
+            }
+        })
+        .catch((error) => {
+            __internalSendPostRequest(count + 1, cgi, request, handler);
+        });
+}
+
 export = NetworkUtils;
