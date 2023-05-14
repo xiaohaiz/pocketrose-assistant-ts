e5ee4e161709e4ab563c1f4cca3b278c5ec55741
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-14 18:13:47 +0800

    __internalSendPostRequest

--- src/util/NetworkUtils.ts
+++ src/util/NetworkUtils.ts
@@ -25,30 +25,7 @@ class NetworkUtils {
     }
 
     static sendPostRequest(cgi: string, request: {}, handler?: (html: string) => void) {
-        fetch(cgi, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: new URLSearchParams(request),
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    MessageBoard.publishWarning("网络请求发生错误，请自行重试！");
-                    throw new Error("RESPONSE was not ok");
-                }
-                return response.arrayBuffer();
-            })
-            .then((arrayBuffer) => {
-                const decoder = new TextDecoder("gb2312");
-                const html = decoder.decode(new Uint8Array(arrayBuffer));
-                if (handler !== undefined) {
-                    handler(html);
-                }
-            })
-            .catch((error) => {
-                console.error("Error raised:", error);
-            });
+        __internalSendPostRequest(0, cgi, request, handler);
     }
 
     static async get(cgi: string): Promise<string> {
