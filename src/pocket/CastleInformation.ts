3b43c8e3d39917a250538aca622a1da1413e6ca8
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-14 12:08:51 +0800

    CastleInformation.parsePage

---
+++ src/pocket/CastleInformation.ts
@@ -0,0 +1,30 @@
+import Castle from "../common/Castle";
+import Coordinate from "../util/Coordinate";
+import StringUtils from "../util/StringUtils";
+
+class CastleInformation {
+
+    static parsePage(html: string) {
+        const castleList: Castle[] = [];
+        $(html).find("td").each(function (_idx, td) {
+            const text = $(td).text();
+            if (text.endsWith(" (自购)")) {
+                const name = $(td).prev().text();
+                const owner = text.substring(0, text.indexOf(" (自购)"));
+                let location = $(td).next().text();
+                location = StringUtils.substringBetween(location, "(", ")");
+                let x = StringUtils.substringBefore(location, ",");
+                let y = StringUtils.substringAfter(location, ",");
+                const coordinate = new Coordinate(parseInt(x), parseInt(y));
+                const castle = new Castle();
+                castle.name = name;
+                castle.owner = owner;
+                castle.coordinate = coordinate;
+                castleList.push(castle);
+            }
+        });
+    }
+
+}
+
+export = CastleInformation;
