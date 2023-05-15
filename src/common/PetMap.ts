27e87e54f05b74a91119965ac797384bd9678f25
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-15 08:45:30 +0800

    PetMap.imageHtml

--- src/common/PetMap.ts
+++ src/common/PetMap.ts
@@ -1,9 +1,15 @@
+import Constants from "../util/Constants";
+
 class PetMap {
 
     code?: string;
     picture?: string;
     count?: number;
 
+    get imageHtml() {
+        const src = Constants.POCKET_DOMAIN + "/image/386/" + this.picture!;
+        return "<img src='" + src + "' alt='" + this.code! + "'>";
+    }
 }
 
 export = PetMap;
