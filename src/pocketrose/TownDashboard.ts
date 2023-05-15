f2e102d4b932cd263333e28f7fe747207cec15ee
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-15 23:13:55 +0800

    TownDashboard.parsePage

--- src/pocketrose/TownDashboard.ts
+++ src/pocketrose/TownDashboard.ts
@@ -21,29 +21,27 @@ class TownDashboard {
     }
 
     static parsePage(html: string) {
-        const page = $(html);
-
-        const p = new TownDashboardPage();
+        const page = new TownDashboardPage();
         const role = new Role();
-        p.role = role;
-        role.canConsecrate = page.text().includes("可以进行下次祭奠了");
-        role.battleCount = parseInt(page.find("input:hidden[name='ktotal']").val() as string);
-        role.country = StringUtils.substringBefore(page.find("option[value='LOCAL_RULE']").text(), "国法");
+        page.role = role;
+        role.canConsecrate = $(html).text().includes("可以进行下次祭奠了");
+        role.battleCount = parseInt($(html).find("input:hidden[name='ktotal']").val() as string);
+        role.country = StringUtils.substringBefore($(html).find("option[value='LOCAL_RULE']").text(), "国法");
 
-        p.townCountry = page.find("th:contains('支配下')")
+        page.townCountry = $(html).find("th:contains('支配下')")
             .filter(function () {
                 return $(this).text() === "支配下";
             })
             .next()
             .text();
-        p.townId = page.find("input:hidden[name='townid']").val() as string;
+        page.townId = $(html).find("input:hidden[name='townid']").val() as string;
 
-        role.town = TownLoader.getTownById(p.townId!)!;
+        role.town = TownLoader.getTownById(page.townId!)!;
         role.location = "TOWN";
 
         // 读取角色当前的能力值
         // 奇怪了，读不到指定id的div元素？但是可以读到里面的td子元素
-        page.find("td:last")
+        $(html).find("td:last")
             .each(function (_idx, td) {
                 const text = $(td).text();
                 let idx = text.indexOf("Lv：");
@@ -66,7 +64,7 @@ class TownDashboard {
                 role.speed = parseInt(s.substring(3));
             });
 
-        return p;
+        return page;
     }
 }
 
