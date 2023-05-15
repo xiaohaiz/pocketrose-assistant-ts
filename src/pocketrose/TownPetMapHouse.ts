ad595810a431d54f72f6cf6a443ad8f71e8110d3
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-15 08:30:44 +0800

    解析宠物图鉴的页面信息

---
+++ src/pocketrose/TownPetMapHouse.ts
@@ -0,0 +1,46 @@
+import Role from "../common/Role";
+import StringUtils from "../util/StringUtils";
+import TownPetMapHousePage from "./TownPetMapHousePage";
+
+class TownPetMapHouse {
+
+    static parsePage(html: string) {
+        const role = new Role();
+        $(html).find("td:contains('ＬＶ')")
+            .filter((idx, td) => $(td).text() === "ＬＶ")
+            .parent()
+            .next()
+            .find("td:first")
+            .each((idx, td) => {
+                role.name = $(td).text();
+            })
+            .next()
+            .each((idx, td) => {
+                role.level = parseInt($(td).text());
+            })
+            .next()
+            .each((idx, td) => {
+                let s = $(td).text();
+                role.attribute = StringUtils.substringBefore(s, "属");
+            })
+            .next()
+            .each((idx, td) => {
+                role.career = $(td).text();
+            })
+            .parent()
+            .next()
+            .find("td:first")
+            .next()
+            .each((idx, td) => {
+                let s = $(td).text();
+                s = StringUtils.substringBefore(s, " GOLD");
+                role.cash = parseInt(s);
+            });
+
+        const page = new TownPetMapHousePage();
+        page.role = role;
+        return page;
+    }
+}
+
+export = TownPetMapHouse;
---
+++ src/pocketrose/TownPetMapHousePage.ts
@@ -0,0 +1,9 @@
+import Role from "../common/Role";
+
+class TownPetMapHousePage {
+
+    role?: Role;
+
+}
+
+export = TownPetMapHousePage;
