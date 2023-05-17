aff699bb8f6129d52fb69624c85c5312b9a33d2a
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-17 23:04:50 +0800

    解析分身的信息。

---
+++ src/common/Mirror.ts
@@ -0,0 +1,24 @@
+class Mirror {
+
+    index?: number;
+    category?: string;
+    image?: string;
+    name?: string;
+    gender?: string;
+    health?: number;
+    maxHealth?: number;
+    mana?: number;
+    maxMana?: number;
+    attribute?: string;
+    attack?: number;
+    defense?: number;
+    specialAttack?: number;
+    specialDefense?: number;
+    speed?: number;
+    career?: string;
+    spell?: string;
+    experience?: number;
+
+}
+
+export = Mirror;
