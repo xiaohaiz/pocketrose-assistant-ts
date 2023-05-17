200e86a7166e3762001e78dfe60814c79f7c198e
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-17 23:13:07 +0800

    PersonalMirror.open

--- src/pocketrose/PersonalMirror.ts
+++ src/pocketrose/PersonalMirror.ts
@@ -1,4 +1,32 @@
+import Credential from "../util/Credential";
+import NetworkUtils from "../util/NetworkUtils";
+import PersonalMirrorPage from "./PersonalMirrorPage";
+
 class PersonalMirror {
+
+    readonly #credential: Credential;
+    readonly #townId?: string;
+
+    constructor(credential: Credential, townId?: string) {
+        this.#credential = credential;
+        this.#townId = townId;
+    }
+
+    async open(): Promise<PersonalMirrorPage> {
+        return await (() => {
+            return new Promise<PersonalMirrorPage>(resolve => {
+                const request = this.#credential.asRequestMap();
+                if (this.#townId !== undefined) {
+                    request.set("town", this.#townId);
+                }
+                request.set("mode", "FENSHENSHIGUAN");
+                NetworkUtils.post("mydata.cgi", request).then(html => {
+                    const page = PersonalMirrorPage.parse(html);
+                    resolve(page);
+                });
+            });
+        })();
+    }
 }
 
 export = PersonalMirror;
