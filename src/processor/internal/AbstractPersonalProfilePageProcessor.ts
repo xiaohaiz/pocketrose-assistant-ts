a2170d6a8d35eb20e2d3acdfd2eb724d3ac62527
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-19 12:25:41 +0800

    ...

--- src/processor/internal/AbstractPersonalProfilePageProcessor.ts
+++ src/processor/internal/AbstractPersonalProfilePageProcessor.ts
@@ -1,4 +1,6 @@
+import NpcLoader from "../../core/NpcLoader";
 import Credential from "../../util/Credential";
+import MessageBoard from "../../util/MessageBoard";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
 
@@ -6,10 +8,72 @@ abstract class AbstractPersonalProfilePageProcessor extends PageProcessorCredent
 
     doProcess(credential: Credential, context?: PageProcessorContext) {
         // 删除老页面的所有元素
-        $("center:first").html("");
+        $("center:first").html("").hide();
 
+        // 老页面没有什么有价值的数据，直接渲染新页面
+        this.#renderImmutablePage(credential, context);
     }
 
+    #renderImmutablePage(credential: Credential, context?: PageProcessorContext) {
+        let html = "";
+        html += "<table style='width:100%;border-width:0;background-color:#888888'>";
+        html += "<tbody>";
+        html += "<tr>";
+        html += "<td id='pageTitleCell'></td>"
+        html += "</tr>";
+        html += "<tr>";
+        html += "<td id='messageBoardCell'></td>"
+        html += "</tr>";
+        html += "<tr style='display:none'>";
+        html += "<td id='hiddenCell-1'></td>"       // 预留给返回城市/城堡功能
+        html += "</tr>";
+        html += "<tr style='display:none'>";
+        html += "<td id='hiddenCell-2'></td>"
+        html += "</tr>";
+        html += "<tr style='display:none'>";
+        html += "<td id='hiddenCell-3'></td>"
+        html += "</tr>";
+        html += "<tr style='display:none'>";
+        html += "<td id='hiddenCell-4'></td>"
+        html += "</tr>";
+        html += "<tr style='display:none'>";
+        html += "<td id='hiddenCell-5'></td>"
+        html += "</tr>";
+        html += "<tr>";
+        html += "<td id='personalStatus'></td>"
+        html += "</tr>";
+        html += "<tr>";
+        html += "<td id='menuCell' style='background-color:#F8F0E0;text-align:center'>";
+        html += "<button role='button' id='returnButton'>返回</button>";
+        html += "</td>";
+        html += "</tr>";
+        html += "</tbody>";
+        html += "</table>";
+
+        $("center:first").after($(html));
+
+        $("#pageTitleCell")
+            .css("text-align", "center")
+            .css("font-size", "150%")
+            .css("font-weight", "bold")
+            .css("background-color", "navy")
+            .css("color", "yellowgreen")
+            .text("＜＜  个 人 面 板  ＞＞");
+
+        MessageBoard.createMessageBoardStyleB("messageBoardCell", NpcLoader.randomNpcImageHtml());
+        $("#messageBoard")
+            .css("background-color", "black")
+            .css("color", "wheat")
+            .html(this.#welcomeMessageHtml());
+
+        this.doBindReturnButton(credential, context);
+    }
+
+    #welcomeMessageHtml() {
+        return "<b style='font-size:120%'>见贤思齐焉，见不贤而内自省也。</b>";
+    }
+
+    abstract doBindReturnButton(credential: Credential, context?: PageProcessorContext): void;
 }
 
 export = AbstractPersonalProfilePageProcessor;
--- src/processor/internal/PersonalProfilePageProcessor_Town.ts
+++ src/processor/internal/PersonalProfilePageProcessor_Town.ts
@@ -1,7 +1,23 @@
+import TownLoader from "../../core/TownLoader";
+import Credential from "../../util/Credential";
+import PageUtils from "../../util/PageUtils";
+import PageProcessorContext from "../PageProcessorContext";
 import AbstractPersonalProfilePageProcessor from "./AbstractPersonalProfilePageProcessor";
 
 class PersonalProfilePageProcessor_Town extends AbstractPersonalProfilePageProcessor {
 
+    doBindReturnButton(credential: Credential, context?: PageProcessorContext): void {
+        const html = PageUtils.generateReturnTownForm(credential);
+        $("#hiddenCell-1").html(html);
+        const townId = context?.get("townId");
+        if (townId !== undefined) {
+            const town = TownLoader.getTownById(townId)!;
+            $("#returnButton").text("返回" + town.name);
+        }
+        $("#returnButton").on("click", () => {
+            $("#returnTown").trigger("click");
+        });
+    }
 
 }
 
