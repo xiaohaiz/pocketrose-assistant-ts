656267f908d8b255564d0ae99c40c6d9e0eb8aae
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-19 11:03:32 +0800

    PersonalProfilePageProcessor_Town

---
+++ src/processor/internal/PersonalProfilePageProcessor_Town.ts
@@ -0,0 +1,8 @@
+import AbstractPersonalProfilePageProcessor from "./AbstractPersonalProfilePageProcessor";
+
+class PersonalProfilePageProcessor_Town extends AbstractPersonalProfilePageProcessor {
+
+
+}
+
+export = PersonalProfilePageProcessor_Town;
