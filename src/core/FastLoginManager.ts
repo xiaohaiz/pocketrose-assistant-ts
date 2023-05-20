681ce2cb056c6137a050739af06b1d85c4d847ce
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-20 20:12:17 +0800

    ### 版本 3.3.6

---
+++ src/core/FastLoginManager.ts
@@ -0,0 +1,19 @@
+import FastLogin from "../common/FastLogin";
+import FastLoginLoader from "./FastLoginLoader";
+
+class FastLoginManager {
+
+    static getAllFastLogins() {
+        const configs: FastLogin[] = [];
+        for (let i = 0; i < 50; i++) {
+            const config = FastLoginLoader.loadFastLogin(i);
+            if (config !== null) {
+                configs.push(config);
+            }
+        }
+        return configs;
+    }
+
+}
+
+export = FastLoginManager;
