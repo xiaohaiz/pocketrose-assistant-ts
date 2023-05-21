9b94c579bb128e400139d0f2b9fa7a5d36d590e4
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-21 11:05:41 +0800

    PetLocalStorage.#findAllPets

--- src/core/PetLocalStorage.ts
+++ src/core/PetLocalStorage.ts
@@ -1,3 +1,11 @@
+import Castle from "../common/Castle";
+import Equipment from "../common/Equipment";
+import Pet from "../common/Pet";
+import CastleInformation from "../pocketrose/CastleInformation";
+import CastleRanch from "../pocketrose/CastleRanch";
+import GoldenCage from "../pocketrose/GoldenCage";
+import PersonalEquipmentManagement from "../pocketrose/PersonalEquipmentManagement";
+import PersonalPetManagement from "../pocketrose/PersonalPetManagement";
 import TownPetMapHouse from "../pocketrose/TownPetMapHouse";
 import Credential from "../util/Credential";
 import StorageUtils from "../util/StorageUtils";
@@ -21,6 +29,80 @@ class PetLocalStorage {
                 });
             });
         })();
+    }
+
+    async #findAllPets(): Promise<Pet[]> {
+        return await (() => {
+            return new Promise<Pet[]>(resolve => {
+                const allPetList: Pet[] = [];
+
+                new PersonalEquipmentManagement(this.#credential).open().then(equipmentPage => {
+                    const roleName = equipmentPage.role!.name!;
+                    const goldenCage = equipmentPage.findGoldenCage();
+
+                    new CastleInformation().load(roleName)
+                        .then(castle => {
+                            __parsePersonalPets(this.#credential, allPetList, goldenCage, castle, () => {
+                                resolve(allPetList);
+                            });
+                        })
+                        .catch(() => {
+                            __parsePersonalPets(this.#credential, allPetList, goldenCage, null, () => {
+                                resolve(allPetList);
+                            });
+                        });
+                });
+            });
+        })();
+    }
+}
+
+function __parsePersonalPets(credential: Credential,
+                             allPetList: Pet[],
+                             goldenCage: Equipment | null,
+                             castle: Castle | null,
+                             handler: () => void) {
+    new PersonalPetManagement(credential).open().then(petPage => {
+        for (const pet of petPage.petList!) {
+            pet.location = "P";
+            allPetList.push(pet);
+        }
+        __parseCagePets(credential, allPetList, goldenCage, castle, handler);
+    });
+}
+
+function __parseCagePets(credential: Credential,
+                         allPetList: Pet[],
+                         goldenCage: Equipment | null,
+                         castle: Castle | null,
+                         handler: () => void) {
+    if (goldenCage === null) {
+        __parseRanchPets(credential, allPetList, castle, handler);
+    } else {
+        new GoldenCage(credential).open(goldenCage.index!).then(cagePage => {
+            for (const pet of cagePage.petList!) {
+                pet.location = "C";
+                allPetList.push(pet);
+            }
+            __parseRanchPets(credential, allPetList, castle, handler);
+        });
+    }
+}
+
+function __parseRanchPets(credential: Credential,
+                          allPetList: Pet[],
+                          castle: Castle | null,
+                          handler: () => void) {
+    if (castle === null) {
+        handler();
+    } else {
+        new CastleRanch(credential).enter().then(ranchPage => {
+            for (const pet of ranchPage.ranchPetList!) {
+                pet.location = "R";
+                allPetList.push(pet);
+            }
+            handler();
+        });
     }
 }
 
