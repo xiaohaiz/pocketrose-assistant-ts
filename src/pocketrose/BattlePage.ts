c1a4d75885fd8dab957a034a869cbf392a2c43bf
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-21 19:42:44 +0800

    parsePage

---
+++ src/pocketrose/BattlePage.ts
@@ -0,0 +1,11 @@
+class BattlePage {
+
+    treasureBattle?: boolean;
+    primaryBattle?: boolean;
+    juniorBattle?: boolean;
+    seniorBattle?: boolean;
+    zodiacBattle?: boolean;
+
+}
+
+export = BattlePage;
--- src/processor/internal/BattlePageProcessor2.ts
+++ src/processor/internal/BattlePageProcessor2.ts
@@ -1,3 +1,4 @@
+import BattlePage from "../../pocketrose/BattlePage";
 import Credential from "../../util/Credential";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
@@ -5,8 +6,28 @@ import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
 class BattlePageProcessor2 extends PageProcessorCredentialSupport {
 
     doProcess(credential: Credential, context?: PageProcessorContext): void {
+        const page = parsePage();
     }
 
+}
+
+function parsePage() {
+    const page = new BattlePage();
+
+    const battleField = $("table:first")
+        .find("tbody:first")
+        .find("tr:first")
+        .find("td:first")
+        .find("font:first")
+        .find("b:first")
+        .text();
+    page.treasureBattle = battleField.includes("秘宝之岛");
+    page.primaryBattle = battleField.includes("初级之森");
+    page.juniorBattle = battleField.includes("中级之塔");
+    page.seniorBattle = battleField.includes("上级之洞窟");
+    page.zodiacBattle = battleField.includes("十二神殿");
+
+    return page;
 }
 
 export = BattlePageProcessor2;
