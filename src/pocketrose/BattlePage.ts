47904c3fb0b8079108f7158f77b0a43798288e9e
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-21 20:04:30 +0800

    解析装备剩余的耐久度

--- src/pocketrose/BattlePage.ts
+++ src/pocketrose/BattlePage.ts
@@ -1,11 +1,20 @@
 class BattlePage {
 
-    treasureBattle?: boolean;
-    primaryBattle?: boolean;
-    juniorBattle?: boolean;
-    seniorBattle?: boolean;
-    zodiacBattle?: boolean;
+    treasureBattle?: boolean;       // 秘宝之岛
+    primaryBattle?: boolean;        // 初级之森
+    juniorBattle?: boolean;         // 中级之塔
+    seniorBattle?: boolean;         // 上级之洞窟
+    zodiacBattle?: boolean;         // 十二神殿
+    lowestEndure?: number;          // 战斗后装备剩余最低耐久度（排除大师球、宗师球、超力怪兽球、宠物蛋）
 
+    constructor() {
+        this.treasureBattle = false;
+        this.primaryBattle = false;
+        this.juniorBattle = false;
+        this.seniorBattle = false;
+        this.zodiacBattle = false;
+        this.lowestEndure = 999;
+    }
 }
 
 export = BattlePage;
--- src/processor/internal/BattlePageProcessor2.ts
+++ src/processor/internal/BattlePageProcessor2.ts
@@ -1,5 +1,7 @@
+import _ from "lodash";
 import BattlePage from "../../pocketrose/BattlePage";
 import Credential from "../../util/Credential";
+import StringUtils from "../../util/StringUtils";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
 
@@ -26,6 +28,28 @@ function parsePage() {
     page.juniorBattle = battleField.includes("中级之塔");
     page.seniorBattle = battleField.includes("上级之洞窟");
     page.zodiacBattle = battleField.includes("十二神殿");
+
+    const endureList: number[] = [];
+    for (const s of _.split($("#ueqtweixin").text(), "\n")) {
+        if (_.endsWith(s, "耐久度")) {
+            if (!s.includes("大师球") &&
+                !s.includes("宗师球") &&
+                !s.includes("超力怪兽球") &&
+                !s.includes("宠物蛋")) {
+                let t = StringUtils.substringBetween(s, "剩余", "耐久度");
+                let n = parseInt(t);
+                endureList.push(n);
+            }
+        }
+        if (_.endsWith(s, "回)")) {
+            let t = StringUtils.substringBetween(s, "(剩余", "回)");
+            let n = parseInt(t);
+            endureList.push(n);
+        }
+    }
+    if (endureList.length > 0) {
+        page.lowestEndure = _.min(endureList);
+    }
 
     return page;
 }
