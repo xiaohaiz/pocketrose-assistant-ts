eb830d93d247575a0660a8a6eabc7d5cef08d397
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-21 11:13:34 +0800

    TownDashboardPageInterceptor

--- src/interceptor/internal/TownDashboardPageInterceptor.ts
+++ src/interceptor/internal/TownDashboardPageInterceptor.ts
@@ -1,10 +1,10 @@
 import LocationStateMachine from "../../core/LocationStateMachine";
-import TownDashboardPageProcessor2 from "../../processor/internal/TownDashboardPageProcessor2";
+import TownDashboardPageProcessor from "../../processor/internal/TownDashboardPageProcessor";
 import PageInterceptor from "../PageInterceptor";
 
 class TownDashboardPageInterceptor implements PageInterceptor {
 
-    readonly #processor = new TownDashboardPageProcessor2();
+    readonly #processor = new TownDashboardPageProcessor();
 
     accept(cgi: string, pageText: string): boolean {
         if (cgi === "status.cgi" || cgi === "town.cgi") {
