c4db52889c572c557450f147b7f2daf67e68a68e
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-23 22:34:03 +0800

    版本 3.4.5

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const now = new Date();
-const buildVersion = '3.0.0-SNAPSHOT'
+const buildVersion = '3.4.5'
 const buildDate = `${now.toLocaleString()}`;
 
 // noinspection JSUnresolvedReference
