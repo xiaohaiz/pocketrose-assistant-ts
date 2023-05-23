8026565a221a6c3879af38618ddde9594a3dc2ac
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-23 12:06:03 +0800

    模拟团队宠物升级的功能。

--- src/processor/internal/AbstractPersonalTeamPageProcessor.ts
+++ src/processor/internal/AbstractPersonalTeamPageProcessor.ts
@@ -5,10 +5,13 @@ import EquipmentLocalStorage from "../../core/EquipmentLocalStorage";
 import FastLoginManager from "../../core/FastLoginManager";
 import NpcLoader from "../../core/NpcLoader";
 import PetLocalStorage from "../../core/PetLocalStorage";
+import PetProfileLoader from "../../core/PetProfileLoader";
+import Pokemon from "../../core/Pokemon";
 import Credential from "../../util/Credential";
 import MessageBoard from "../../util/MessageBoard";
 import PageUtils from "../../util/PageUtils";
 import StorageUtils from "../../util/StorageUtils";
+import StringUtils from "../../util/StringUtils";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
 
@@ -131,6 +134,7 @@ abstract class AbstractPersonalTeamPageProcessor extends PageProcessorCredential
             $("#messageBoardManager").html(imageHtml);
             $("#messageBoard").html(this.#welcomeMessageHtml());
             $("#information").html("").parent().hide();
+            $(".simulationButton").off("click");
         });
     }
 
@@ -167,6 +171,8 @@ abstract class AbstractPersonalTeamPageProcessor extends PageProcessorCredential
 
     #bindListEquipmentButton() {
         $("#listEquipmentButton").on("click", () => {
+            $(".simulationButton").off("click");
+
             let html = "";
             html += "<table style='margin:auto;border-width:0;text-align:center;background-color:#888888;width:100%'>";
             html += "<tbody id='equipmentStatusList'>";
@@ -229,6 +235,8 @@ abstract class AbstractPersonalTeamPageProcessor extends PageProcessorCredential
 
     #bindListPetButton() {
         $("#listPetButton").on("click", () => {
+            $(".simulationButton").off("click");
+
             let html = "";
             html += "<table style='margin:auto;border-width:0;text-align:center;background-color:#888888;width:100%'>";
             html += "<tbody id='petStatusList'>";
@@ -244,10 +252,14 @@ abstract class AbstractPersonalTeamPageProcessor extends PageProcessorCredential
             html += "<th style='background-color:#F8F0E0'>精神</th>";
             html += "<th style='background-color:#F8F0E0'>速度</th>";
             html += "<th style='background-color:#F8F0E0'>位置</th>";
+            html += "<th style='background-color:#F8F0E0'>模拟</th>";
             html += "</tr>";
             html += "</tbody>";
             html += "</table>";
             $("#information").html(html).parent().hide();
+
+            const allPetList: Pet[] = [];
+            let petIndex = 0;
 
             const configs = FastLoginManager.getAllFastLogins();
             for (const config of configs) {
@@ -257,14 +269,15 @@ abstract class AbstractPersonalTeamPageProcessor extends PageProcessorCredential
                     continue;
                 }
                 const pets = _.split(value, "$$");
-
                 let html = "";
                 let row = 0;
-
                 pets
                     .map(it => Pet.parse(it))
                     .sort(Pet.sorter)
                     .forEach(it => {
+                        it.index = petIndex++;
+                        allPetList.push(it);
+
                         html += "<tr>";
                         if (row === 0) {
                             html += "<td style='background-color:#F8F0E0;vertical-align:center' rowspan='" + (pets.length) + "'>" + config.name + "</td>";
@@ -279,14 +292,120 @@ abstract class AbstractPersonalTeamPageProcessor extends PageProcessorCredential
                         html += "<td style='background-color:#E8E8B0'>" + it.specialDefenseHtml + "</td>";
                         html += "<td style='background-color:#E8E8D0'>" + it.speedHtml + "</td>";
                         html += "<td style='background-color:#E8E8B0'>" + it.location + "</td>";
+                        html += "<td style='background-color:#E8E8D0'>";
+                        html += "<button role='button' class='simulationButton' id='simulate-" + it.index + "'>模拟</button>";
+                        html += "</td>";
                         html += "</tr>";
                         row++;
                     });
 
                 $("#petStatusList").append($(html));
-
             }
+
+            html = "";
+            html += "<tr style='display:none'>";
+            html += "<td id='simulation' style='background-color:#F8F0E0' colspan='12'></td>";
+            html += "</tr>";
+            $("#petStatusList").append($(html));
+            this.#bindSimulationButton(allPetList);
+
             $("#information").parent().show();
+        });
+    }
+
+    #bindSimulationButton(allPetList: Pet[]) {
+        allPetList.forEach(it => {
+            const buttonId = "simulate-" + it.index;
+            if (it.level! === 100) {
+                // 宠物已经满级了
+                $("#" + buttonId).prop("disabled", true);
+            } else {
+                if (!Pokemon.isInitialPetName(it.name)) {
+                    // 宠物已经改名了，不认识了
+                    $("#" + buttonId).prop("disabled", true);
+                } else {
+                    this.#doPetSimulation(it, buttonId);
+                }
+            }
+        });
+    }
+
+    #doPetSimulation(pet: Pet, buttonId: string) {
+        $("#" + buttonId).on("click", () => {
+            const code = StringUtils.substringBetween(pet.name!, "(", ")");
+            const profile = PetProfileLoader.load(code)!;
+
+            let html = "";
+            html += "<table style='margin:auto;border-width:0;text-align:center;background-color:#888888;width:100%'>";
+            html += "<tbody>";
+            html += "<tr>";
+            html += "<th style='background-color:#F8F0E0'></th>";
+            html += "<th style='background-color:#F8F0E0'>模拟</th>";
+            html += "<th style='background-color:#F8F0E0'>名字</th>";
+            html += "<th style='background-color:#F8F0E0'>性别</th>";
+            html += "<th style='background-color:#F8F0E0'>等级</th>";
+            html += "<th style='background-color:#F8F0E0'>生命</th>";
+            html += "<th style='background-color:#F8F0E0'>攻击</th>";
+            html += "<th style='background-color:#F8F0E0'>防御</th>";
+            html += "<th style='background-color:#F8F0E0'>智力</th>";
+            html += "<th style='background-color:#F8F0E0'>精神</th>";
+            html += "<th style='background-color:#F8F0E0'>速度</th>";
+            html += "<th style='background-color:#F8F0E0'>能力</th>";
+            html += "</tr>";
+
+            for (let i = 0; i < 10; i++) {
+                let p = _.clone(pet);
+
+                const delta = 100 - p.level!;
+                for (let j = 0; j < delta; j++) {
+                    p.level = p.level! + 1;
+
+                    // health
+                    let max = 20 + (profile.healthEffort! * 10);
+                    let add = _.random(0, max);
+                    p.maxHealth = p.maxHealth! + add;
+                    // attack
+                    max = profile.attackEffort! + 1;
+                    add = _.random(0, max);
+                    p.attack = p.attack! + add;
+                    // defense
+                    max = profile.defenseEffort! + 1;
+                    add = _.random(0, max);
+                    p.defense = p.defense! + add;
+                    // special attack
+                    max = profile.specialAttackEffort! + 1;
+                    add = _.random(0, max);
+                    p.specialAttack = p.specialAttack! + add;
+                    // special defense
+                    max = profile.specialDefenseEffort! + 1;
+                    add = _.random(0, max);
+                    p.specialDefense = p.specialDefense! + add;
+                    // speed
+                    max = profile.speedEffort! + 1;
+                    add = _.random(0, max);
+                    p.speed = p.speed! + add;
+                }
+
+                html += "<tr>";
+                html += "<td style='background-color:#F8F0E0;width:64px;height:64px'>" + profile.imageHtml + "</td>";
+                html += "<td style='background-color:#E8E8D0;font-weight:bold'>#" + (i + 1) + "</td>";
+                html += "<td style='background-color:#E8E8B0;text-align:left'>" + p.nameHtml + "</td>";
+                html += "<td style='background-color:#E8E8D0'>" + p.gender + "</td>";
+                html += "<td style='background-color:#E8E8B0'>" + p.levelHtml + "</td>";
+                html += "<td style='background-color:#E8E8D0'>" + p.maxHealth + "</td>";
+                html += "<td style='background-color:#E8E8B0'>" + p.attackHtml + "</td>";
+                html += "<td style='background-color:#E8E8D0'>" + p.defenseHtml + "</td>";
+                html += "<td style='background-color:#E8E8B0'>" + p.specialAttackHtml + "</td>";
+                html += "<td style='background-color:#E8E8D0'>" + p.specialDefenseHtml + "</td>";
+                html += "<td style='background-color:#E8E8B0'>" + p.speedHtml + "</td>";
+                html += "<td style='background-color:#E8E8D0;font-weight:bold;color:red'>" + p.capacity + "</td>";
+                html += "</tr>";
+            }
+
+            html += "</tbody>";
+            html += "</table>";
+
+            $("#simulation").html(html).parent().show();
         });
     }
 }
