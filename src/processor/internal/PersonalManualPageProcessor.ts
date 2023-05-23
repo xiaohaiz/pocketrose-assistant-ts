b982b5d7567db9164061767bed9d4479606254e7
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-23 20:59:07 +0800

    口袋助手用户手册页面初始化。

--- src/processor/internal/PersonalManualPageProcessor.ts
+++ src/processor/internal/PersonalManualPageProcessor.ts
@@ -1,10 +1,54 @@
 import Credential from "../../util/Credential";
+import StringUtils from "../../util/StringUtils";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
 
 class PersonalManualPageProcessor extends PageProcessorCredentialSupport {
 
     doProcess(credential: Credential, context?: PageProcessorContext): void {
+        $("table[height='100%']").removeAttr("height");
+        $("form[action='status.cgi']").remove();
+
+        $("td:first")
+            .attr("id", "pageTitle")
+            .removeAttr("width")
+            .removeAttr("height")
+            .removeAttr("bgcolor")
+            .css("text-align", "center")
+            .css("font-size", "150%")
+            .css("font-weight", "bold")
+            .css("background-color", "navy")
+            .css("color", "yellowgreen")
+            .text("＜＜  口 袋 助 手 用 户 手 册  ＞＞")
+            .parent()
+            .after("<tr><td id='version'></td></tr>");
+
+        // @ts-ignore
+        const version = __VERSION__;
+        $("#version")
+            .css("background-color", "wheat")
+            .css("color", "navy")
+            .css("font-weight", "bold")
+            .css("text-align", "center")
+            .text(version);
+
+        let s = $("center:contains('现在的所持金')").text();
+        s = StringUtils.substringBefore(s, " 现在的所持金");
+        const roleName = s.substring(2);
+
+        $("table:first")
+            .find("tbody:first")
+            .find("> tr:eq(2)")
+            .find("td:first")
+            .find("table:first")
+            .find("tbody:first")
+            .find("> tr:first")
+            .find("> td:eq(1)")
+            .find("table:first")
+            .find("tbody:first")
+            .find("> tr:eq(1)")
+            .find("td:first")
+            .html(roleName);
     }
 
 }
