6ee53568a520f91d3fd5fefff32b51f3d3209aa8
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-25 07:53:48 +0800

    修复国民一览中破掉的链接。

--- src/processor/internal/RoleInformationPageProcessor.ts
+++ src/processor/internal/RoleInformationPageProcessor.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import PageUtils from "../../util/PageUtils";
 import PageProcessor from "../PageProcessor";
 import PageProcessorContext from "../PageProcessorContext";
@@ -8,6 +9,17 @@ class RoleInformationPageProcessor implements PageProcessor {
         PageUtils.fixCurrentPageBrokenImages();
         PageUtils.removeUnusedHyperLinks();
         PageUtils.removeGoogleAnalyticsScript();
+
+        $("a")
+            .filter((idx, a) => {
+                const href = $(a).attr("href");
+                return _.startsWith(href, "cgi/ranking");
+            })
+            .each((idx, a) => {
+                let href = $(a).attr("href")!;
+                href = href.substring(4);
+                $(a).attr("href", href);
+            })
     }
 
 }
