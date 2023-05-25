a28e111e429c7e912dc711d776c727d241e49445
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-25 12:57:13 +0800

    renderEventBoard

--- src/processor/internal/MetroDashboardPageProcessor.ts
+++ src/processor/internal/MetroDashboardPageProcessor.ts
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import SetupLoader from "../../config/SetupLoader";
+import EventHandler from "../../core/EventHandler";
 import RankTitleLoader from "../../core/RankTitleLoader";
 import MetroDashboardPage from "../../pocketrose/MetroDashboardPage";
 import Credential from "../../util/Credential";
@@ -86,10 +87,51 @@ class MetroDashboardPageProcessor extends PageProcessorCredentialSupport {
                 });
         }
 
-
+        renderEventBoard();
     }
 
 
+}
+
+function renderEventBoard() {
+    $("td:contains('最近发生的事件')")
+        .filter(function () {
+            return $(this).text() === "最近发生的事件";
+        })
+        .parent()
+        .next()
+        .find("td:first")
+        .attr("id", "eventBoard");
+
+    const eventHtmlList: string[] = [];
+    $("#eventBoard").html()
+        .split("<br>")
+        .filter(it => it.endsWith(")"))
+        .map(function (it) {
+            // noinspection HtmlDeprecatedTag,XmlDeprecatedElement,HtmlDeprecatedAttribute
+            const header: string = "<font color=\"navy\">●</font>";
+            return StringUtils.substringAfter(it, header);
+        })
+        .map(function (it) {
+            return EventHandler.handleWithEventHtml(it);
+        })
+        .forEach(it => eventHtmlList.push(it));
+
+    let html = "";
+    html += "<table style='border-width:0;width:100%;height:100%;margin:auto'>";
+    html += "<tbody>";
+    eventHtmlList.forEach(it => {
+        html += "<tr>";
+        html += "<th style='color:navy;vertical-align:top'>●</th>";
+        html += "<td style='width:100%'>";
+        html += it;
+        html += "</td>";
+        html += "</tr>";
+    });
+    html += "</tbody>";
+    html += "</table>";
+
+    $("#eventBoard").html(html);
 }
 
 export = MetroDashboardPageProcessor;
