54450af6a574fa110fa5c741030f82856fbb585b
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-25 11:01:38 +0800

    动员指令中的身份修改。

--- src/processor/internal/MapDashboardPageProcessor.ts
+++ src/processor/internal/MapDashboardPageProcessor.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import SetupLoader from "../../config/SetupLoader";
 import EventHandler from "../../core/EventHandler";
 import MapBuilder from "../../core/MapBuilder";
@@ -22,6 +23,34 @@ class MapDashboardPageProcessor extends PageProcessorCredentialSupport {
         }
 
         const page = MapDashboardPage.parse(PageUtils.currentPageHtml());
+
+        $("center:first")
+            .attr("id", "systemAnnouncement");
+        $("#systemAnnouncement")
+            .after($("<div id='version' style='color:navy;font-weight:bold;text-align:center;width:100%'></div>"));
+        // @ts-ignore
+        $("#version").html(__VERSION__);
+
+        if (SetupLoader.isQiHanTitleEnabled()) {
+            $("table:first")
+                .find("tbody:first")
+                .find("> tr:eq(1)")
+                .find("font:first")
+                .each((idx, font) => {
+                    let c = $(font).text();
+
+                    let b = StringUtils.substringAfterLast(c, "(");
+                    let a = StringUtils.substringBefore(c, "(" + b);
+                    b = StringUtils.substringBefore(b, ")");
+                    const ss = _.split(b, " ");
+                    const b1 = _.replace(ss[0], "部队", "");
+                    const b2 = RankTitleLoader.transformTitle(ss[1]);
+                    const b3 = ss[2];
+
+                    const s = a + "(" + b1 + " " + b2 + " " + b3 + ")";
+                    $(font).text(s);
+                });
+        }
 
         $("table:first")
             .find("tbody:first")
