cf6c3d77a7bcc73b1957afcf1443670380959b44
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-27 16:43:40 +0800

    Fix bad role name.

--- src/processor/internal/CountryTownStrengthenPageProcessor.ts
+++ src/processor/internal/CountryTownStrengthenPageProcessor.ts
@@ -1,3 +1,4 @@
+import PersonalStatus from "../../pocketrose/PersonalStatus";
 import Credential from "../../util/Credential";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
@@ -5,6 +6,28 @@ import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
 class CountryTownStrengthenPageProcessor extends PageProcessorCredentialSupport {
 
     doProcess(credential: Credential, context?: PageProcessorContext): void {
+        $("table[height='100%']").removeAttr("height");
+        $("table:first")
+            .find("> tbody:first")
+            .find("> tr:first")
+            .find("> td:first")
+            .attr("id", "pageTitle")
+            .parent()
+            .next()
+            .find("> td:first")
+            .find("> table:first")
+            .find("> tbody:first")
+            .find("> tr:first")
+            .find("> td:eq(1)")
+            .find("> table:first")
+            .find("> tbody:first")
+            .find("> tr:eq(1)")
+            .find("> td:first")
+            .each((i, td) => {
+                new PersonalStatus(credential, context?.get("townId")).load().then(role => {
+                    $(td).text(role.name!);
+                });
+            });
     }
 
 }
