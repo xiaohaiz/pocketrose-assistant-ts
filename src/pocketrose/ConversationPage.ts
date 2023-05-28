55dfaaca9497c3cd2f39e5c007fcb64a04048973
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-28 22:56:26 +0800

    ConversationPage.parse

--- src/pocketrose/ConversationPage.ts
+++ src/pocketrose/ConversationPage.ts
@@ -2,9 +2,7 @@ class ConversationPage {
 
     static parse(html: string) {
         const tr = $(html)
-            .find("body:first")
-            .find("> table:first")
-            .find("> tbody:first")
+            .find("tbody:first")
             .find("> tr:last");
 
         return new ConversationPage();
--- src/processor/internal/PersonalSetupPageProcessor_Town.ts
+++ src/processor/internal/PersonalSetupPageProcessor_Town.ts
@@ -1,3 +1,4 @@
+import Conversation from "../../pocketrose/Conversation";
 import Credential from "../../util/Credential";
 import PageUtils from "../../util/PageUtils";
 import PersonalSetupPageProcessor from "./PersonalSetupPageProcessor";
@@ -5,11 +6,14 @@ import PersonalSetupPageProcessor from "./PersonalSetupPageProcessor";
 class PersonalSetupPageProcessor_Town extends PersonalSetupPageProcessor {
 
     doGenerateHiddenForm(credential: Credential, containerId: string): void {
+        new Conversation(credential).open().then()
         const html = PageUtils.generateReturnTownForm(credential);
         $("#" + containerId).html(html);
     }
 
     doBindReturnButton(returnButtonId: string): void {
+
+
         $("#" + returnButtonId).on("click", function () {
             $("#returnTown").trigger("click");
         });
