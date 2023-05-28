f72b8d69b2e11d5128fa2325ecbbce96834b918c
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-28 22:56:39 +0800

    ConversationPage.parse

--- src/processor/internal/PersonalSetupPageProcessor_Town.ts
+++ src/processor/internal/PersonalSetupPageProcessor_Town.ts
@@ -1,4 +1,3 @@
-import Conversation from "../../pocketrose/Conversation";
 import Credential from "../../util/Credential";
 import PageUtils from "../../util/PageUtils";
 import PersonalSetupPageProcessor from "./PersonalSetupPageProcessor";
@@ -6,14 +5,11 @@ import PersonalSetupPageProcessor from "./PersonalSetupPageProcessor";
 class PersonalSetupPageProcessor_Town extends PersonalSetupPageProcessor {
 
     doGenerateHiddenForm(credential: Credential, containerId: string): void {
-        new Conversation(credential).open().then()
         const html = PageUtils.generateReturnTownForm(credential);
         $("#" + containerId).html(html);
     }
 
     doBindReturnButton(returnButtonId: string): void {
-
-
         $("#" + returnButtonId).on("click", function () {
             $("#returnTown").trigger("click");
         });
