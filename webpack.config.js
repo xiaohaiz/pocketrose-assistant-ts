06a3a25fa949c3b86d5545d0af355c625099750e
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-29 23:29:29 +0800

    3.5.2-SNAPSHOT

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const now = new Date();
-const buildVersion = '3.5.1'
+const buildVersion = '3.5.2-SNAPSHOT'
 const buildDate = `${now.toLocaleString()}`;
 
 // noinspection JSUnresolvedReference
