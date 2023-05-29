74f165560eeac8bbf7ee898a75a8abf1f7d21bb9
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-29 12:03:19 +0800

    TownDashboardPageProcessor2

---
+++ src/processor/internal/TownDashboardPageProcessor2.ts
@@ -0,0 +1,13 @@
+import Credential from "../../util/Credential";
+import PageProcessorContext from "../PageProcessorContext";
+import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
+
+class TownDashboardPageProcessor2 extends PageProcessorCredentialSupport {
+
+    doProcess(credential: Credential, context?: PageProcessorContext): void {
+
+    }
+
+}
+
+export = TownDashboardPageProcessor2;
