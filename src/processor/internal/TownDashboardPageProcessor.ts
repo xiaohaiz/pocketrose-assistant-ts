90db256b2a640169c4b758dd161000f8d9d0764d
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-31 07:48:28 +0800

    更换手机极简布局默认的按钮样式。

--- src/processor/internal/TownDashboardPageProcessor.ts
+++ src/processor/internal/TownDashboardPageProcessor.ts
@@ -19,7 +19,7 @@ const LAYOUT_MANAGER = new TownDashboardLayoutManager();
 class TownDashboardPageProcessor extends PageProcessorCredentialSupport {
 
     doLoadButtonStyles(): number[] {
-        return [16, 10005, 10007, 10008, 10016, 10024, 10028, 10032, 10033, 10035, 10062];
+        return [10005, 10007, 10008, 10016, 10024, 10028, 10032, 10033, 10035, 10062];
     }
 
     doProcess(credential: Credential, context?: PageProcessorContext): void {
