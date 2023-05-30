864d5034c36d8ce755362f451d1104579ca49a20
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-30 17:24:03 +0800

    3.5.7-SNAPSHOT

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const now = new Date();
-const buildVersion = '3.5.6'
+const buildVersion = '3.5.7-SNAPSHOT'
 const buildDate = `${now.toLocaleString()}`;
 
 // noinspection JSUnresolvedReference
