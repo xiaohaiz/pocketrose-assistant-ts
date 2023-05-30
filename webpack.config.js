821eb46811974033f8b737487358488f4dfeb87c
xiaohaiz <xiaohaiz@hotmail.com>
2023-05-30 14:57:00 +0800

    战斗布局（体验版），未来不一定会继续细化。

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const now = new Date();
-const buildVersion = '3.5.4-SNAPSHOT'
+const buildVersion = '3.5.4'
 const buildDate = `${now.toLocaleString()}`;
 
 // noinspection JSUnresolvedReference
