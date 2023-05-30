e10ee2edfaeff11c6ee0a0c8f765578658a3b44c
張曉海 <xiaohai.zhang@17zuoye.com>
2023-05-30 22:45:28 +0800

    开始战斗后，禁用其他的按钮

--- src/layout/TownDashboardLayout005.ts
+++ src/layout/TownDashboardLayout005.ts
@@ -101,6 +101,15 @@ class TownDashboardLayout005 extends TownDashboardLayout {
         $("#battleButton")
             .attr("type", "button")
             .on("click", () => {
+                // 开始战斗后，禁用其他的按钮
+                $("#returnButton").hide();
+                $("#battleButton")
+                    .prop("disabled", true)
+                    .css("color", "grey");
+                $("input:submit")
+                    .prop("disabled", true)
+                    .css("color", "grey");
+
                 const request = credential.asRequestMap();
                 $("#battleCell")
                     .find("> form[action='battle.cgi']")
