948082f25e6c63c04bb7fcce469cf77f416c460d
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-01 12:11:17 +0800

    渲染上个回合的战斗。

--- src/layout/TownDashboardLayout006.ts
+++ src/layout/TownDashboardLayout006.ts
@@ -1,5 +1,6 @@
 import TownDashboardPage from "../pocketrose/TownDashboardPage";
 import Credential from "../util/Credential";
+import StorageUtils from "../util/StorageUtils";
 import TownDashboardLayout from "./TownDashboardLayout";
 import TownDashboardLayout003 from "./TownDashboardLayout003";
 
@@ -13,8 +14,93 @@ class TownDashboardLayout006 extends TownDashboardLayout {
 
     render(credential: Credential, page: TownDashboardPage): void {
         this.#layout003.render(credential, page);
+
+        $("table:first")
+            .next()
+            .find("> tbody:first")
+            .find("> tr:first")
+            .before($("" +
+                "<tr style='display:none'><td id='hidden-1'></td></tr>" +
+                "<tr style='display:none'><td id='hidden-2'></td></tr>" +
+                "<tr style='display:none'><td id='hidden-3'></td></tr>" +
+                "<tr style='display:none'><td id='hidden-4'></td></tr>" +
+                "<tr style='display:none'><td id='hidden-5'></td></tr>" +
+                "<tr><td id='battlePanel' style='text-align:center'></td></tr>"));
+
+        generateDepositForm(credential);
+        generateRepairForm(credential);
+        generateLodgeForm(credential);
+
+        const lastBattle = StorageUtils.getString("_lb_" + credential.id);
+        if (lastBattle !== "") {
+            if (StorageUtils.getBoolean("_pa_055")) {
+                const children: JQuery[] = [];
+                $("#battlePanel")
+                    .html(lastBattle)
+                    .find("> *")
+                    .each((idx, child) => {
+                        const element = $(child);
+                        if (element.is("p") || element.is("b")) {
+                            element.hide();
+                            children.push(element);
+                        }
+                    });
+                _showReportElement(children, 0);
+            } else {
+                $("#battlePanel")
+                    .html(lastBattle);
+            }
+        }
     }
 
+}
+
+function generateDepositForm(credential: Credential) {
+    let form = "";
+    // noinspection HtmlUnknownTarget
+    form += "<form action='town.cgi' method='post'>";
+    form += "<input type='hidden' name='id' value='" + credential.id + "'>";
+    form += "<input type='hidden' name='pass' value='" + credential.pass + "'>"
+    form += "<input type='hidden' name='azukeru' value='all'>";
+    form += "<input type='hidden' name='mode' value='BANK_SELL'>";
+    form += "<input type='submit' id='deposit'>";
+    form += "</form>";
+    $("#hidden-2").html(form);
+}
+
+function generateRepairForm(credential: Credential) {
+    let form = "";
+    // noinspection HtmlUnknownTarget
+    form += "<form action='town.cgi' method='post'>";
+    form += "<input type='hidden' name='id' value='" + credential.id + "'>";
+    form += "<input type='hidden' name='pass' value='" + credential.pass + "'>"
+    form += "<input type='hidden' name='arm_mode' value='all'>";
+    form += "<input type='hidden' name='mode' value='MY_ARM2'>";
+    form += "<input type='submit' id='repair'>";
+    form += "</form>";
+    $("#hidden-3").html(form);
+}
+
+function generateLodgeForm(credential: Credential) {
+    let form = "";
+    // noinspection HtmlUnknownTarget
+    form += "<form action='town.cgi' method='post'>";
+    form += "<input type='hidden' name='id' value='" + credential.id + "'>";
+    form += "<input type='hidden' name='pass' value='" + credential.pass + "'>"
+    form += "<input type='hidden' name='mode' value='RECOVERY'>";
+    form += "<input type='submit' id='lodge'>";
+    form += "</form>";
+    $("#hidden-4").html(form);
+}
+
+function _showReportElement(children: JQuery[], index: number) {
+    if (index === children.length) {
+        return;
+    }
+    const child = children[index];
+    child.show("fast", "linear", () => {
+        _showReportElement(children, index + 1);
+    });
 }
 
 export = TownDashboardLayout006;
