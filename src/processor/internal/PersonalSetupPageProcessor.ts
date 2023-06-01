18a9e41771e256a4a4b4eb6f6e9157b0fa1dece4
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-01 11:33:52 +0800

    title color.

--- src/processor/internal/PersonalSetupPageProcessor.ts
+++ src/processor/internal/PersonalSetupPageProcessor.ts
@@ -156,7 +156,7 @@ abstract class PersonalSetupPageProcessor extends PageProcessorCredentialSupport
         let html = "";
         html += "<table style='background-color:#888888;width:100%;text-align:center'>";
         html += "<tbody style='background-color:#F8F0E0' id='setup_item_table'>";
-        html += "<tr style='background-color:red;color:white'>";
+        html += "<tr style='background-color:green;color:white'>";
         html += "<th>名字</th>";
         html += "<th>专属</th>";
         html += "<th>设置</th>";
