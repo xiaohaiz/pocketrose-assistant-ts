36de49496844744210fc259dbff6427cafa3df6e
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-02 11:22:28 +0800

    RoleEquipmentStatusStorage

--- src/role/RoleStorageManager.ts
+++ src/role/RoleStorageManager.ts
@@ -1,3 +1,4 @@
+import RoleEquipmentStatusStorage from "./RoleEquipmentStatusStorage";
 import RolePetMapStorage from "./RolePetMapStorage";
 import RolePetStatusStorage from "./RolePetStatusStorage";
 
@@ -10,9 +11,14 @@ class RoleStorageManager {
     static getRolePetStatusStorage() {
         return rolePetStatusStorage;
     }
+
+    static getRoleEquipmentStatusStorage() {
+        return roleEquipmentStatusStorage;
+    }
 }
 
 const rolePetMapStorage = new RolePetMapStorage();
 const rolePetStatusStorage = new RolePetStatusStorage();
+const roleEquipmentStatusStorage = new RoleEquipmentStatusStorage();
 
 export = RoleStorageManager;
