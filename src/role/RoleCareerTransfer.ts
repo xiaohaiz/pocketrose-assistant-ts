c28d0df60c80c3cd32535fdbfe3e95fed94bbd64
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-03 14:46:32 +0800

    RoleCareerTransfer

---
+++ src/role/RoleCareerTransfer.ts
@@ -0,0 +1,29 @@
+class RoleCareerTransfer {
+
+    id?: number;
+    roleId?: string;
+    createTime?: number;
+
+    career_1?: string;
+    level_1?: number;
+    health_1?: number;
+    mana_1?: number;
+    attack_1?: number;
+    defense_1?: number;
+    specialAttack_1?: number;
+    specialDefense_1?: number;
+    speed_1?: number;
+
+    career_2?: string;
+    level_2?: number;
+    health_2?: number;
+    mana_2?: number;
+    attack_2?: number;
+    defense_2?: number;
+    specialAttack_2?: number;
+    specialDefense_2?: number;
+    speed_2?: number;
+
+}
+
+export = RoleCareerTransfer;
