ee2ab158ed42bee4d5c8d0674a16da5ca5507ede
張曉海 <xiaohai.zhang@17zuoye.com>
2023-06-04 20:03:14 +0800

    RoleState.asObject

---
+++ src/core/state/RoleState.ts
@@ -0,0 +1,45 @@
+class RoleState {
+
+    id?: string;
+    updateTime?: number;
+    location?: string;
+    townId?: string;
+    battleCount?: string;
+    castleName?: string;
+    coordinate?: string;
+
+    asObject() {
+        const data: {} = {
+            id: this.id
+        };
+        if (this.updateTime !== undefined) {
+            // @ts-ignore
+            data.updateTime = this.updateTime;
+        }
+        if (this.location !== undefined) {
+            // @ts-ignore
+            data.location = this.location;
+        }
+        if (this.townId !== undefined) {
+            // @ts-ignore
+            data.townId = this.townId;
+        }
+        if (this.battleCount !== undefined) {
+            // @ts-ignore
+            data.battleCount = this.battleCount;
+        }
+        if (this.castleName !== undefined) {
+            // @ts-ignore
+            data.castleName = this.castleName;
+        }
+        if (this.coordinate !== undefined) {
+            // @ts-ignore
+            data.coordinate = this.coordinate;
+        }
+
+        return data;
+    }
+
+}
+
+export = RoleState;
