74b22d44ad96315d9dfe770fa5d2970fd8e8679d
張曉海 <xiaohai.zhang@17zuoye.com>
2023-06-04 21:42:17 +0800

    RoleStateMachine

--- src/core/state/RoleStateMachine.ts
+++ src/core/state/RoleStateMachine.ts
@@ -4,8 +4,40 @@ class RoleStateMachine {
 
     readonly #state: RoleState | null;
 
+    #inTownHandler?: (state?: RoleState) => void;
+    #inCastleHandler?: (state?: RoleState) => void;
+    #inMapHandler?: (state?: RoleState) => void;
+    #inMetroHandler?: (state?: RoleState) => void;
+    #inTangHandler?: (state?: RoleState) => void;
+
     constructor(state: RoleState | null) {
         this.#state = state;
+    }
+
+
+    whenInTown(value: (state?: RoleState) => void): RoleStateMachine {
+        this.#inTownHandler = value;
+        return this;
+    }
+
+    whenInCastle(value: (state?: RoleState) => void): RoleStateMachine {
+        this.#inCastleHandler = value;
+        return this;
+    }
+
+    whenInMap(value: (state?: RoleState) => void): RoleStateMachine {
+        this.#inMapHandler = value;
+        return this;
+    }
+
+    whenInMetro(value: (state?: RoleState) => void): RoleStateMachine {
+        this.#inMetroHandler = value;
+        return this;
+    }
+
+    whenInTang(value: (state?: RoleState) => void): RoleStateMachine {
+        this.#inTangHandler = value;
+        return this;
     }
 }
 
