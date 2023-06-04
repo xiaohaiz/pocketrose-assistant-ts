a6faaff61981f8233937a8d0f38bf5457a632109
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-04 15:06:36 +0800

    BattleReturnInterceptor.doBeforeReturn

--- src/battle/BattleReturnInterceptor.ts
+++ src/battle/BattleReturnInterceptor.ts
@@ -1,4 +1,38 @@
+import EquipmentLocalStorage from "../core/EquipmentLocalStorage";
+import PetLocalStorage from "../core/PetLocalStorage";
+import Credential from "../util/Credential";
+
 class BattleReturnInterceptor {
+
+    readonly #credential: Credential;
+    readonly #battleCount: number;
+
+    constructor(credential: Credential, battleCount: number) {
+        this.#credential = credential;
+        this.#battleCount = battleCount;
+    }
+
+    async doBeforeReturn(): Promise<void> {
+        return await (() => {
+            return new Promise<void>(resolve => {
+                const petLocalStorage = new PetLocalStorage(this.#credential);
+                petLocalStorage
+                    .triggerUpdatePetMap(this.#battleCount)
+                    .then(() => {
+                        petLocalStorage
+                            .triggerUpdatePetStatus(this.#battleCount)
+                            .then(() => {
+                                new EquipmentLocalStorage(this.#credential)
+                                    .triggerUpdateEquipmentStatus(this.#battleCount)
+                                    .then(() => {
+                                        resolve();
+                                    });
+                            });
+                    });
+            });
+        })();
+    }
+
 }
 
 export = BattleReturnInterceptor;
