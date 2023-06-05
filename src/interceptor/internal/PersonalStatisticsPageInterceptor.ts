bb726c1c3d3ace8380776e70811da8137b94dffb
張曉海 <xiaohai.zhang@17zuoye.com>
2023-06-06 07:43:44 +0800

    PersonalStatisticsPageProcessor_Town

--- src/interceptor/internal/PersonalStatisticsPageInterceptor.ts
+++ src/interceptor/internal/PersonalStatisticsPageInterceptor.ts
@@ -1,11 +1,11 @@
 import RoleStateMachineManager from "../../core/state/RoleStateMachineManager";
-import PersonalStatisticsPageProcessor from "../../processor/internal/PersonalStatisticsPageProcessor";
+import PersonalStatisticsPageProcessor_Town from "../../processor/internal/PersonalStatisticsPageProcessor_Town";
 import PageProcessor from "../../processor/PageProcessor";
 import PageInterceptor from "../PageInterceptor";
 
 class PersonalStatisticsPageInterceptor implements PageInterceptor {
 
-    readonly #processor: PageProcessor = new PersonalStatisticsPageProcessor();
+    readonly #processor: PageProcessor = new PersonalStatisticsPageProcessor_Town();
 
     accept(cgi: string, pageText: string): boolean {
         if (cgi === "mydata.cgi") {
---
+++ src/processor/internal/PersonalStatisticsPageProcessor_Town.ts
@@ -0,0 +1,6 @@
+import PersonalStatisticsPageProcessor from "./PersonalStatisticsPageProcessor";
+
+class PersonalStatisticsPageProcessor_Town extends PersonalStatisticsPageProcessor {
+}
+
+export = PersonalStatisticsPageProcessor_Town;
