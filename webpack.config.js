f11dd57cf4709b2e70f07cf1211825ff8f236f47
張曉海 <xiaohai.zhang@17zuoye.com>
2023-06-05 23:24:43 +0800

    版本 3.7.4

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const now = new Date();
-const buildVersion = '3.7.4-SNAPSHOT'
+const buildVersion = '3.7.4'
 const buildDate = `${now.toLocaleString()}`;
 
 // noinspection JSUnresolvedReference
