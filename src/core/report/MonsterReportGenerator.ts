e159fe72221af7ec57d113e452dc7a29792ad01d
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-06 14:42:11 +0800

    怪 物 统 计 （ 胜 率 排 序 ）

--- src/core/report/MonsterReportGenerator.ts
+++ src/core/report/MonsterReportGenerator.ts
@@ -176,6 +176,82 @@ class MonsterReportGenerator {
         html += "</tr>";
 
         // --------------------------------------------------------------------
+        // 怪 物 统 计 （ 胜 率 排 序 ）
+        // --------------------------------------------------------------------
+        html += "<tr>";
+        html += "<td>";
+        html += "<table style='background-color:#888888;text-align:center;margin:auto;width:100%'>";
+        html += "<tbody>";
+        html += "<tr>";
+        html += "<th style='background-color:navy;color:greenyellow' colspan='16'>怪 物 统 计 （ 胜 率 排 序 ）</th>"
+        html += "</tr>";
+        html += "<tr>";
+        html += "<th style='background-color:green;color:white' colspan='1'>名字</th>"
+        html += "<th style='background-color:green;color:white' colspan='10'>怪物</th>"
+        html += "</tr>";
+
+        if (this.#target === undefined || this.#target === "") {
+            const list = sortByWinRatio(monsterCount);
+            html += "<tr>";
+            html += "<th style='background-color:black;color:white' rowspan='2'>全团队</th>"
+            for (let i = 0; i < 10; i++) {
+                const it = list[i]
+                if (it === undefined) {
+                    html += "<td style='background-color:wheat;width:64px'></td>"
+                } else {
+                    // @ts-ignore
+                    const profile: PetProfile = it[0];
+                    html += "<td style='background-color:wheat;width:64px'>" + profile.imageHtml + "</td>"
+                }
+            }
+            html += "</tr>";
+            html += "<tr>";
+            for (let i = 0; i < 10; i++) {
+                const it = list[i]
+                if (it === undefined) {
+                    html += "<td style='background-color:wheat;width:64px'></td>"
+                } else {
+                    // @ts-ignore
+                    html += "<td style='background-color:wheat;width:64px'>" + (it[1] * 100).toFixed(2) + "%</td>"
+                }
+            }
+            html += "</tr>";
+        }
+
+        roles.forEach(it => {
+            const list = sortByWinRatio(it.monsterCount);
+            html += "<tr>";
+            html += "<th style='background-color:black;color:white' rowspan='2'>" + it.roleName + "</th>"
+            for (let i = 0; i < 10; i++) {
+                const it = list[i]
+                if (it === undefined) {
+                    html += "<td style='background-color:#F8F0E0;width:64px'></td>"
+                } else {
+                    // @ts-ignore
+                    const profile: PetProfile = it[0];
+                    html += "<td style='background-color:#F8F0E0;width:64px'>" + profile.imageHtml + "</td>"
+                }
+            }
+            html += "</tr>";
+            html += "<tr>";
+            for (let i = 0; i < 10; i++) {
+                const it = list[i]
+                if (it === undefined) {
+                    html += "<td style='background-color:#F8F0E0;width:64px'></td>"
+                } else {
+                    // @ts-ignore
+                    html += "<td style='background-color:#F8F0E0;width:64px'>" + (it[1] * 100).toFixed(2) + "%</td>"
+                }
+            }
+            html += "</tr>";
+        });
+
+        html += "</tbody>";
+        html += "</table>";
+        html += "</td>";
+        html += "</tr>";
+
+        // --------------------------------------------------------------------
         // 四 天 王 统 计
         // --------------------------------------------------------------------
         html += "<tr>";
@@ -286,6 +362,25 @@ function sortByBattleCount(monsterCount: Map<string, number[]>) {
     list = list.sort((a, b) => {
         // @ts-ignore
         const ret = b[1] - a[1];
+        if (ret !== 0) {
+            return ret;
+        }
+        // @ts-ignore
+        return a[0].code.localeCompare(b[0].code);
+    });
+    return list;
+}
+
+function sortByWinRatio(monsterCount: Map<string, number[]>) {
+    let list: [][] = [];
+    monsterCount.forEach((counts, code) => {
+        const profile = PetProfileLoader.load(code)!;
+        // @ts-ignore
+        list.push([profile, counts[1] / counts[0]]);
+    });
+    list = list.sort((a, b) => {
+        // @ts-ignore
+        const ret = a[1] - b[1];
         if (ret !== 0) {
             return ret;
         }
