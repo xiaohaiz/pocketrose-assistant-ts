1169c4f34fa853dba3c8c61fc09903b8b10a0f94
張曉海 <xiaohai.zhang@17zuoye.com>
2023-06-06 22:39:49 +0800

    3.8.1-SNAPSHOT

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const now = new Date();
-const buildVersion = '3.8.0'
+const buildVersion = '3.8.1-SNAPSHOT'
 const buildDate = `${now.toLocaleString()}`;
 
 // noinspection JSUnresolvedReference
