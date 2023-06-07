c53fce6adbd9eb15c8743015586cf4235963b8f9
張曉海 <xiaohai.zhang@17zuoye.com>
2023-06-07 08:31:26 +0800

    BattleLogStorage.findByCreateTime

--- src/core/battle/BattleLogStorage.ts
+++ src/core/battle/BattleLogStorage.ts
@@ -19,6 +19,58 @@ class BattleLogStorage {
             });
         })();
     }
+
+    /**
+     * Find battle logs by createTime in specified time range.
+     * @param start Start time.
+     * @param end End time, use current if not specified.
+     */
+    async findByCreateTime(start: number, end?: number) {
+        const db = await PocketDatabase.connectDatabase();
+        return await (() => {
+            return new Promise((resolve, reject) => {
+                let range: IDBKeyRange;
+                if (end === undefined) {
+                    range = IDBKeyRange.lowerBound(start);
+                } else {
+                    range = IDBKeyRange.bound(start, end);
+                }
+
+                const request = db
+                    .transaction(["BattleLog"], "readonly")
+                    .objectStore("BattleLog")
+                    .index("createTime")
+                    .getAll(range);
+
+                request.onerror = reject;
+                request.onsuccess = () => {
+                    const logList: BattleLog[] = [];
+                    if (request.result) {
+                        request.result.forEach(it => {
+                            const log = new BattleLog();
+                            log.id = it.id;
+                            log.createTime = it.createTime;
+                            log.roleId = it.roleId;
+                            log.monster = it.monster;
+                            log.result = it.result;
+                            log.catch = it.catch;
+                            log.photo = it.photo;
+                            if (it.treasures) {
+                                log.treasures = new Map<string, number>();
+                                Object.keys(it.treasures)
+                                    .forEach(code => {
+                                        const count = it.treasures[code];
+                                        log.treasures!.set(code, count);
+                                    });
+                            }
+                            logList.push(log);
+                        });
+                    }
+                    resolve(logList);
+                };
+            });
+        })();
+    }
 }
 
 export = BattleLogStorage;
