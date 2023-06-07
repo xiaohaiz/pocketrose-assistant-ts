83cb7d21b63baa2502bfccdb42a215801bc1abe3
張曉海 <xiaohai.zhang@17zuoye.com>
2023-06-07 08:01:53 +0800

    TeamManager.isMaster 检查指定的id是否团队主号

---
+++ src/core/team/TeamManager.ts
@@ -0,0 +1,22 @@
+import StorageUtils from "../../util/StorageUtils";
+import FastLoginLoader from "../FastLoginLoader";
+
+class TeamManager {
+
+    static isMaster(id: string): boolean {
+        const masterId = StorageUtils.getInt("_tm_", -1);
+        if (masterId < 0) {
+            // No team master specified, return false
+            return false;
+        }
+        const config = FastLoginLoader.loadFastLogin(masterId);
+        if (config === null) {
+            // Specified masterId not configured, return false
+            return false;
+        }
+        return id === config.id;
+    }
+
+}
+
+export = TeamManager;
