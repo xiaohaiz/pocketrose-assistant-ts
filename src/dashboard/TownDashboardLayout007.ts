12ba4bc921eae832bf60b7fd0a2e3a0e62aaafae
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-10 16:02:39 +0800

    TownDashboardLayout007 extends from 战斗模式

--- src/dashboard/TownDashboardLayout007.ts
+++ src/dashboard/TownDashboardLayout007.ts
@@ -1,14 +1,18 @@
 import TownDashboardPage from "../pocketrose/TownDashboardPage";
 import Credential from "../util/Credential";
 import TownDashboardLayout from "./TownDashboardLayout";
+import TownDashboardLayout005 from "./TownDashboardLayout005";
 
 class TownDashboardLayout007 extends TownDashboardLayout {
+
+    readonly #layout005: TownDashboardLayout = new TownDashboardLayout005();
 
     id(): number {
         return 7;
     }
 
     render(credential: Credential, page: TownDashboardPage): void {
+        this.#layout005.render(credential, page);
     }
 
 
