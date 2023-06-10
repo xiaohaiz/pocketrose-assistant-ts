7ffb75c65ff02b82ddba9dc44dd80f84bf465647
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-10 15:09:17 +0800

    LayoutConfigLoader

---
+++ src/dashboard/LayoutConfig.ts
@@ -0,0 +1,13 @@
+class LayoutConfig {
+
+    readonly id: number;
+    readonly name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
+
+export = LayoutConfig;
