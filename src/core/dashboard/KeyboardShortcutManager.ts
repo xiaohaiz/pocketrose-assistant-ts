38e6eed19379987c39b01389264785f42f42d0f0
張曉海 <xiaohai.zhang@17zuoye.com>
2023-06-13 21:38:42 +0800

    KeyboardShortcutManager

--- src/core/dashboard/KeyboardShortcutManager.ts
+++ src/core/dashboard/KeyboardShortcutManager.ts
@@ -53,44 +53,32 @@ function _mode2() {
             return;
         }
 
-        if (key === "w") {
-            $("#battleButton").trigger("focus");
-            return;
-        }
-
-        if (key !== "q") {
-            buffer = "";
-            return;
-        }
-
-        buffer += "q";
-        if (buffer.length < 2) {
-            $("#battleButton").trigger("blur");
-            return;
-        }
-        if (buffer.length === 2) {
-            // 满足按键条件
-            buffer = "";
-
-            // 如果战斗选项中有“秘宝之岛”，则恒定选择
-            let islandFound = false;
-            $(selectBattle).find("option")
-                .each((idx, option) => {
-                    if ($(option).text() === "秘宝之岛") {
-                        $(option).prop("selected", true);
-                        islandFound = true;
-                    }
-                });
-            if (islandFound) {
+        if (key === "q") {
+            buffer += "q";
+            if (buffer.length === 1) {
                 $("#battleButton").trigger("focus");
-                return;
+            } else if (buffer.length === 2) {
+                // 满足按键条件
+                buffer = "";
+                // 如果战斗选项中有“秘宝之岛”，则恒定选择
+                let islandFound = false;
+                $(selectBattle).find("option")
+                    .each((idx, option) => {
+                        if ($(option).text() === "秘宝之岛") {
+                            $(option).prop("selected", true);
+                            islandFound = true;
+                        }
+                    });
+                if (islandFound) {
+                    $("#battleButton").trigger("focus");
+                } else {
+                    // 在option中切换
+                    optionIdx = optionIdx === 0 ? 1 : 0;
+                    $(selectBattle).find("> option:eq(" + optionIdx + ")")
+                        .prop("selected", true);
+                    $("#battleButton").trigger("focus");
+                }
             }
-
-            // 在option中切换
-            optionIdx = optionIdx === 0 ? 1 : 0;
-            $(selectBattle).find("> option:eq(" + optionIdx + ")")
-                .prop("selected", true);
-            $("#battleButton").trigger("focus");
         }
     });
 }
