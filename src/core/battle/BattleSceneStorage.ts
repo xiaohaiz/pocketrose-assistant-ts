fd96a49326e7a0dccf493088c534ccbf508b3a11
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-16 15:14:00 +0800

    BattleSceneStorage.loadLast

--- src/core/battle/BattleSceneStorage.ts
+++ src/core/battle/BattleSceneStorage.ts
@@ -3,6 +3,31 @@ import BattleScene from "./BattleScene";
 
 class BattleSceneStorage {
 
+    async loadLast(): Promise<BattleScene | null> {
+        const db = await PocketDatabase.connectDatabase();
+        return await (() => {
+            return new Promise<BattleScene | null>((resolve, reject) => {
+                const request = db
+                    .transaction(["BattleScene"], "readonly")
+                    .objectStore("BattleScene")
+                    .get("LAST");
+                request.onerror = reject;
+                request.onsuccess = () => {
+                    let data: BattleScene | null = null;
+                    if (request.result) {
+                        data = new BattleScene();
+                        data.id = request.result.id;
+                        data.updateTime = request.result.updateTime;
+                        data.roleId = request.result.roleId;
+                        data.request = request.result.request;
+                        data.beforePage = request.result.beforePage;
+                        data.afterPage = request.result.afterPage;
+                    }
+                    resolve(data);
+                };
+            });
+        })();
+    }
 
     async writeLast(data: BattleScene) {
         const db = await PocketDatabase.connectDatabase();
