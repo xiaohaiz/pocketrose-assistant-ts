15a50ec7534cc4a27af6274653768851e975a08c
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-20 11:15:09 +0800

    CareerChangeLogStorage.insert

--- src/core/career/CareerChangeLogStorage.ts
+++ src/core/career/CareerChangeLogStorage.ts
@@ -1,10 +1,24 @@
+import ObjectID from "bson-objectid";
+import PocketDatabase from "../../util/PocketDatabase";
 import CareerChangeLog from "./CareerChangeLog";
 
 class CareerChangeLogStorage {
 
-
     async insert(data: CareerChangeLog) {
-
+        const db = await PocketDatabase.connectDatabase();
+        const document = data.asDocument();
+        // @ts-ignore
+        document.id = ObjectID().toHexString();
+        // @ts-ignore
+        document.createTime = Date.now();
+        return new Promise<void>((resolve, reject) => {
+            const request = db
+                .transaction(["CareerChangeLog"], "readwrite")
+                .objectStore("CareerChangeLog")
+                .add(document);
+            request.onerror = reject;
+            request.onsuccess = () => resolve();
+        });
     }
 
 }
