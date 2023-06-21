dd32d21e5129d8fe9fbc507b411f5eb58c1699fe
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-21 11:03:18 +0800

    TaskGuide.asDocument

--- src/core/task/TaskGuide.ts
+++ src/core/task/TaskGuide.ts
@@ -4,6 +4,16 @@ class TaskGuide {
     task?: string;
     createTime?: number;
 
+    asDocument() {
+        const document = {};
+        // @ts-ignore
+        thid.id && (document.id = this.id);
+        // @ts-ignore
+        thid.task && (document.task = this.task);
+        // @ts-ignore
+        (this.createTime !== undefined) && (document.createTime = this.createTime);
+        return document;
+    }
 }
 
 export = TaskGuide;
