a8b8d9b441aa9cc8cf4795b85a1e5a30fb96f2d4
xiaohaiz <xiaohaiz@hotmail.com>
2023-06-27 11:53:49 +0800

    webpack mode => production

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         filename: 'pocketrose.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    mode: "none",
+    mode: "production",
     module: {
         rules: [
             {
