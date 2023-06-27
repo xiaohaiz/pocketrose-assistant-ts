835cb1722e36810e89d7466e9b9e4af86e9aa948
張曉海 <xiaohai.zhang@17zuoye.com>
2023-06-27 08:08:18 +0800

    EquipmentConsecrateLogStorage.loads

--- src/core/equipment/EquipmentConsecrateLogStorage.ts
+++ src/core/equipment/EquipmentConsecrateLogStorage.ts
@@ -8,6 +8,31 @@ class EquipmentConsecrateLogStorage {
         return instance;
     }
 
+    async loads(): Promise<EquipmentConsecrateLog[]> {
+        const db = await PocketDatabase.connectDatabase();
+        return new Promise<EquipmentConsecrateLog[]>((resolve, reject) => {
+            const request = db
+                .transaction(["EquipmentConsecrateLog"], "readonly")
+                .objectStore("EquipmentConsecrateLog")
+                .getAll();
+            request.onerror = reject;
+            request.onsuccess = () => {
+                const dataList: EquipmentConsecrateLog[] = [];
+                if (request.result && request.result.length > 0) {
+                    request.result.forEach(it => {
+                        const data = new EquipmentConsecrateLog();
+                        data.id = it.id;
+                        data.createTime = it.createTime;
+                        data.roleId = it.roleId;
+                        data.equipments = it.equipments;
+                        dataList.push(data);
+                    });
+                }
+                resolve(dataList);
+            };
+        });
+    }
+
     async insert(data: EquipmentConsecrateLog) {
         const db = await PocketDatabase.connectDatabase();
         const document = data.asDocument();
