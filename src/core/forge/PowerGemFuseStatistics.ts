a8bc48e0b350ba9367be8aa1346a1c11c319b8b3
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-01 11:15:28 +0800

    砸威力宝石的统计数据结构。

---
+++ src/core/forge/PowerGemFuseStatistics.ts
@@ -0,0 +1,41 @@
+class PowerGemFuseStatistics {
+
+    roleName?: string;
+
+    m10: number = 0;
+    m9: number = 0;
+    m8: number = 0;
+    m7: number = 0;
+    m6: number = 0;
+    m5: number = 0;
+    m4: number = 0;
+    m3: number = 0;
+    m2: number = 0;
+    m1: number = 0;
+    z: number = 0;
+    p1: number = 0;
+    p2: number = 0;
+    p3: number = 0;
+    p4: number = 0;
+    p5: number = 0;
+    p6: number = 0;
+    p7: number = 0;
+    p8: number = 0;
+    p9: number = 0;
+    p10: number = 0;
+    p11: number = 0;
+    p12: number = 0;
+    p13: number = 0;
+    p14: number = 0;
+    p15: number = 0;
+    p16: number = 0;
+    p17: number = 0;
+    p18: number = 0;
+    p19: number = 0;
+    p20: number = 0;
+
+    count: number = 0;
+    totalEffort: number = 0;
+}
+
+export = PowerGemFuseStatistics;
