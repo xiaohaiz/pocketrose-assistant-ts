9eb8ae58ffe190fe441f242c11f436873b1ccdbc
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-06 11:54:45 +0800

    4.2.20-SNAPSHOT

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const now = new Date();
-const buildVersion = '4.2.19'
+const buildVersion = '4.2.20-SNAPSHOT'
 const buildDate = `${now.toLocaleString()}`;
 
 // noinspection JSUnresolvedReference
