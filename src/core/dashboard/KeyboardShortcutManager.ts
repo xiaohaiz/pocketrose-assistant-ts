99346e6c684e5143e019932f5c5377af45bb2f9b
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-06 11:35:48 +0800

    安全战斗按钮功能开启时，q直接切换战斗选项。

--- src/core/dashboard/KeyboardShortcutManager.ts
+++ src/core/dashboard/KeyboardShortcutManager.ts
@@ -35,10 +35,9 @@ function doBind(credential: Credential, bindBattle?: boolean, page?: TownDashboa
 
         if (bindBattle) {
             if (key === "q") {
-                if ($("#battleButton:focus").length === 0) {
-                    $("#battleButton").trigger("focus");
-                } else {
-                    // 如果战斗选项中有“秘宝之岛”，则恒定选择
+                if ($("#battleButton:hidden").length > 0) {
+                    // 安全战斗按钮功能开启，此时可能不显示战斗按钮
+                    // 直接进入切换战斗选项的流程
                     let islandFound = false;
                     $(selectBattle).find("option")
                         .each((idx, option) => {
@@ -47,14 +46,33 @@ function doBind(credential: Credential, bindBattle?: boolean, page?: TownDashboa
                                 islandFound = true;
                             }
                         });
-                    if (islandFound) {
-                        $("#battleButton").trigger("focus");
-                    } else {
-                        // 在option中切换
+                    if (!islandFound) {
                         optionIdx = optionIdx === 0 ? 1 : 0;
                         $(selectBattle).find("> option:eq(" + optionIdx + ")")
                             .prop("selected", true);
+                    }
+                } else {
+                    if ($("#battleButton:focus").length === 0) {
                         $("#battleButton").trigger("focus");
+                    } else {
+                        // 如果战斗选项中有“秘宝之岛”，则恒定选择
+                        let islandFound = false;
+                        $(selectBattle).find("option")
+                            .each((idx, option) => {
+                                if ($(option).text() === "秘宝之岛") {
+                                    $(option).prop("selected", true);
+                                    islandFound = true;
+                                }
+                            });
+                        if (islandFound) {
+                            $("#battleButton").trigger("focus");
+                        } else {
+                            // 在option中切换
+                            optionIdx = optionIdx === 0 ? 1 : 0;
+                            $(selectBattle).find("> option:eq(" + optionIdx + ")")
+                                .prop("selected", true);
+                            $("#battleButton").trigger("focus");
+                        }
                     }
                 }
                 return;
