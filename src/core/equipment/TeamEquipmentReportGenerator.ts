9e308a6e58b25214646cbd0715045356715819a1
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-09 16:03:29 +0800

    TeamEquipmentReportGenerator

---
+++ src/core/equipment/TeamEquipmentReportGenerator.ts
@@ -0,0 +1,88 @@
+import TeamMemberLoader from "../team/TeamMemberLoader";
+import RoleEquipmentStatusStorage from "./RoleEquipmentStatusStorage";
+import Equipment from "./Equipment";
+
+class TeamEquipmentReportGenerator {
+
+    readonly #includeExternal: boolean;
+    readonly #searchName?: string;
+    readonly #equipmentFilter?: (equipment: Equipment) => boolean;
+
+    constructor(includeExternal: boolean,
+                searchName?: string,
+                equipmentFilter?: (equipment: Equipment) => boolean) {
+        this.#includeExternal = includeExternal;
+        this.#searchName = searchName;
+        this.#equipmentFilter = equipmentFilter;
+    }
+
+    generate() {
+        let html = "";
+        html += "<table style='margin:auto;border-width:0;text-align:center;background-color:#888888;width:100%'>";
+        html += "<tbody id='equipmentStatusList'>";
+        html += "<tr>";
+        html += "<th style='background-color:#F8F0E0'>队员</th>";
+        html += "<th style='background-color:#F8F0E0'>名字</th>";
+        html += "<th style='background-color:#F8F0E0'>种类</th>";
+        html += "<th style='background-color:#F8F0E0'>效果</th>";
+        html += "<th style='background-color:#F8F0E0'>重量</th>";
+        html += "<th style='background-color:#F8F0E0'>耐久</th>";
+        html += "<th style='background-color:#F8F0E0'>威＋</th>";
+        html += "<th style='background-color:#F8F0E0'>重＋</th>";
+        html += "<th style='background-color:#F8F0E0'>幸＋</th>";
+        html += "<th style='background-color:#F8F0E0'>经验</th>";
+        html += "<th style='background-color:#F8F0E0'>位置</th>";
+        html += "</tr>";
+        html += "</tbody>";
+        html += "</table>";
+        $("#information").html(html).parent().hide();
+
+        const configs = TeamMemberLoader.loadTeamMembers()
+            .filter(it => this.#includeExternal || it.external === undefined || !it.external);
+        const idList = configs.map(it => it.id!);
+        RoleEquipmentStatusStorage.getInstance()
+            .loads(idList)
+            .then(dataMap => {
+                for (const config of configs) {
+                    const data = dataMap.get(config.id!);
+                    if (data === undefined) {
+                        continue;
+                    }
+                    const equipments: string[] = JSON.parse(data.json!);
+
+                    let html = "";
+                    let row = 0;
+
+                    const equipmentList = equipments
+                        .map(it => Equipment.parse(it))
+                        .sort(Equipment.sorter)
+                        .filter(it => this.#searchName === undefined || this.#searchName === "" || it.fullName.includes(this.#searchName))
+                        .filter(it => this.#equipmentFilter === undefined || this.#equipmentFilter(it));
+                    equipmentList.forEach(it => {
+                        html += "<tr>";
+                        if (row === 0) {
+                            html += "<td style='background-color:black;color:white;white-space:nowrap;font-weight:bold;vertical-align:center' " +
+                                "rowspan='" + (equipmentList.length) + "'>" + config.name + "</td>";
+                        }
+                        html += "<td style='background-color:#E8E8D0;text-align:left'>" + it.fullName + "</td>";
+                        html += "<td style='background-color:#E8E8B0'>" + it.category + "</td>";
+                        html += "<td style='background-color:#E8E8D0'>" + it.power + "</td>";
+                        html += "<td style='background-color:#E8E8B0'>" + it.weight + "</td>";
+                        html += "<td style='background-color:#E8E8D0'>" + it.endure + "</td>";
+                        html += "<td style='background-color:#E8E8B0'>" + it.additionalPowerHtml + "</td>";
+                        html += "<td style='background-color:#E8E8D0'>" + it.additionalWeightHtml + "</td>";
+                        html += "<td style='background-color:#E8E8B0'>" + it.additionalLuckHtml + "</td>";
+                        html += "<td style='background-color:#E8E8D0'>" + it.experienceHTML + "</td>";
+                        html += "<td style='background-color:#E8E8B0'>" + it.location + "</td>";
+                        html += "</tr>";
+                        row++;
+                    });
+
+                    $("#equipmentStatusList").append($(html));
+                }
+                $("#information").parent().show();
+            });
+    }
+}
+
+export = TeamEquipmentReportGenerator;
