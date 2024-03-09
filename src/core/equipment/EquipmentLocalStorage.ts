8e89444dd2b4dd542e293a23651b498865490f65
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-09 15:33:24 +0800

    自动触发装备库记录更新的战数尾数调整为19/37/59/79/97

--- src/core/equipment/EquipmentLocalStorage.ts
+++ src/core/equipment/EquipmentLocalStorage.ts
@@ -21,9 +21,10 @@ class EquipmentLocalStorage {
     async triggerUpdateEquipmentStatus(battleCount: number): Promise<void> {
         return await (() => {
             return new Promise<void>(resolve => {
-                // 自动保存启用时，战数尾数为13/47/79/97时，触发装备保存
-                const doStorage = (battleCount % 100 === 13)
-                    || (battleCount % 100 === 47)
+                // 自动保存启用时，战数尾数为19/37/59/79/97时，触发装备保存
+                const doStorage = (battleCount % 100 === 19)
+                    || (battleCount % 100 === 37)
+                    || (battleCount % 100 === 59)
                     || (battleCount % 100 === 79)
                     || (battleCount % 100 === 97);
                 if (doStorage) {
