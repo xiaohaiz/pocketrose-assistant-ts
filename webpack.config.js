da53f0d349a3ae350d2a560901202d25e02cfcec
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-12 14:21:35 +0800

    4.2.25

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const now = new Date();
-const buildVersion = '4.2.25-SNAPSHOT'
+const buildVersion = '4.2.25'
 const buildDate = `${now.toLocaleString()}`;
 
 // noinspection JSUnresolvedReference
@@ -13,7 +13,7 @@ module.exports = {
         filename: 'pocketrose.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    mode: "none",
+    mode: "production",
     module: {
         rules: [
             {
