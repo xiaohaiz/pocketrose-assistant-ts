44070246666694071510fb4a9350a4dce864506a
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-12 14:02:03 +0800

    团队装备列表显示计数。

--- src/core/equipment/TeamEquipmentReportGenerator.ts
+++ src/core/equipment/TeamEquipmentReportGenerator.ts
@@ -1,6 +1,6 @@
 import TeamMemberLoader from "../team/TeamMemberLoader";
-import RoleEquipmentStatusStorage from "./RoleEquipmentStatusStorage";
 import Equipment from "./Equipment";
+import RoleEquipmentStatusStorage from "./RoleEquipmentStatusStorage";
 
 class TeamEquipmentReportGenerator {
 
@@ -20,10 +20,25 @@ class TeamEquipmentReportGenerator {
         let html = "";
         html += "<table style='margin:auto;border-width:0;text-align:center;background-color:#888888;width:100%'>";
         html += "<tbody id='equipmentStatusList'>";
+        html += "<tr>";
+        html += "<th style='background-color:skyblue'>队员</th>";
+        html += "<th style='background-color:skyblue'>装备</th>";
+        html += "<th style='background-color:skyblue'>名字</th>";
+        html += "<th style='background-color:skyblue'>种类</th>";
+        html += "<th style='background-color:skyblue'>效果</th>";
+        html += "<th style='background-color:skyblue'>重量</th>";
+        html += "<th style='background-color:skyblue'>耐久</th>";
+        html += "<th style='background-color:skyblue'>威＋</th>";
+        html += "<th style='background-color:skyblue'>重＋</th>";
+        html += "<th style='background-color:skyblue'>幸＋</th>";
+        html += "<th style='background-color:skyblue'>经验</th>";
+        html += "<th style='background-color:skyblue'>位置</th>";
+        html += "</tr>";
         html += "</tbody>";
         html += "</table>";
         $("#information").html(html).parent().hide();
 
+        let totalCount = 0;
         const configs = TeamMemberLoader.loadTeamMembers()
             .filter(it => this.#includeExternal || it.external === undefined || !it.external);
         const idList = configs.map(it => it.id!);
@@ -48,18 +63,7 @@ class TeamEquipmentReportGenerator {
                     equipmentList.forEach(it => {
                         if (row === 0) {
                             html += "<tr>";
-                            html += "<th style='background-color:skyblue'>队员</th>";
-                            html += "<th style='background-color:skyblue'>装备</th>";
-                            html += "<th style='background-color:skyblue'>名字</th>";
-                            html += "<th style='background-color:skyblue'>种类</th>";
-                            html += "<th style='background-color:skyblue'>效果</th>";
-                            html += "<th style='background-color:skyblue'>重量</th>";
-                            html += "<th style='background-color:skyblue'>耐久</th>";
-                            html += "<th style='background-color:skyblue'>威＋</th>";
-                            html += "<th style='background-color:skyblue'>重＋</th>";
-                            html += "<th style='background-color:skyblue'>幸＋</th>";
-                            html += "<th style='background-color:skyblue'>经验</th>";
-                            html += "<th style='background-color:skyblue'>位置</th>";
+                            html += "<td style='background-color:darkgreen;height:2px' colspan='12'></td>";
                             html += "</tr>";
                             html += "<tr>";
                             html += "<td style='background-color:black;color:white;white-space:nowrap;font-weight:bold;vertical-align:center' " +
@@ -80,9 +84,21 @@ class TeamEquipmentReportGenerator {
                         html += "<td style='background-color:#E8E8B0'>" + it.location + "</td>";
                         html += "</tr>";
                         row++;
+                        totalCount++;
                     });
-
                     $("#equipmentStatusList").append($(html));
+                }
+                if (totalCount > 0) {
+                    let footer = "";
+                    footer += "<tr>";
+                    footer += "<td style='background-color:darkgreen;height:2px' colspan='12'></td>";
+                    footer += "</tr>";
+                    footer += "<tr>";
+                    footer += "<td style='background-color:#E8E8D0;font-weight:bold;text-align:center' colspan='12'>";
+                    footer += "发现满足条件的装备总数：" + totalCount;
+                    footer += "</td>";
+                    footer += "</tr>";
+                    $("#equipmentStatusList").append($(footer));
                 }
                 $("#information").parent().show();
             });
