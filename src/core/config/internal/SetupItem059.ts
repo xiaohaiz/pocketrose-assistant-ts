759054d6328c9d5c09817d3e6f0f0bd767da4a5e
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-14 09:18:51 +0800

    SetupItem059

--- src/core/config/internal/SetupItem059.ts
+++ src/core/config/internal/SetupItem059.ts
@@ -5,54 +5,60 @@ import SetupLoader from "../SetupLoader";
 
 class SetupItem059 implements SetupItem {
 
+    readonly #code: string = "059";
+    readonly #name: string = "智能选择战斗场";
+    readonly #key: string = "_pa_059";
+
     code(): string {
-        return code;
+        return this.#code;
     }
 
     render(id?: string): void {
-        doRender();
+        this.#doRender();
     }
 
-}
+    #doRender() {
+        let html = "";
+        html += "<tr>";
+        html += "<th style='background-color:#E8E8D0'>" + this.#name + "</th>";
+        html += "<td style='background-color:#E8E8D0'></td>";
+        html += "<td style='background-color:#EFE0C0'></td>";
+        html += "<td style='background-color:#E0D0B0;text-align:left' colspan='2'>" + this.#doGenerateSetupItem() + "</td>";
+        html += "</tr>";
+        $("#setup_item_table").append($(html));
 
-const code: string = "059";
-const name: string = "智能选择战斗场";
-const key: string = "_pa_" + code;
+        this.#doProcessButton();
+    }
 
-function doRender() {
-    let html = "";
-    html += "<tr>";
-    html += "<th style='background-color:#E8E8D0'>" + name + "</th>";
-    html += "<td style='background-color:#E8E8D0'></td>";
-    html += "<td style='background-color:#EFE0C0'><input type='button' class='dynamic_button' id='setup_" + code + "' value='设置'></td>";
-    html += "<td style='background-color:#E0D0B0;text-align:left'>" + doGenerateSetupItem() + "</td>";
-    html += "<td style='background-color:#E8E8D0;text-align:left'>战斗布局专属设置，战斗结果报告中胜利者站在左边。</td>";
-    html += "</tr>";
+    #doGenerateSetupItem() {
+        let html = "";
+        html += "<input type='button' class='dynamic_button _059_button' id='_059_button_1' value='启用'>";
+        html += "<input type='button' class='dynamic_button _059_button' id='_059_button_2' value='禁用'>";
+        return html;
+    }
 
-    $("#setup_item_table").append($(html));
+    #doProcessButton() {
+        const value = SetupLoader.isAutoSetBattleFieldEnabled();
+        if (value) {
+            $("#_059_button_1").css("color", "blue").prop("disabled", true);
+        } else {
+            $("#_059_button_2").css("color", "blue").prop("disabled", true);
+        }
 
-    const value = SetupLoader.isWinnerLeftEnabled();
-    $(".option_class_" + code + "[value='" + Number(value) + "']").prop("selected", true);
-
-    $("#setup_" + code).on("click", function () {
-        doSaveSetupItem();
-    });
-}
-
-function doGenerateSetupItem() {
-    let html = "";
-    html += "<select id='select_" + code + "'>";
-    html += "<option class='option_class_" + code + "' value='1'>启用</option>";
-    html += "<option class='option_class_" + code + "' value='0'>禁用</option>";
-    html += "</select>";
-    return html;
-}
-
-function doSaveSetupItem() {
-    const value = $("#select_" + code).val();
-    StorageUtils.set(key, value!.toString());
-    MessageBoard.publishMessage("<b style='color:red'>" + name + "</b>已经设置。");
-    $("#refreshButton").trigger("click");
+        $("._059_button").on("click", event => {
+            $("._059_button").off("click");
+            const buttonId = $(event.target).attr("id")!;
+            if (buttonId === "_059_button_1") {
+                StorageUtils.set(this.#key, "1");
+            } else if (buttonId === "_059_button_2") {
+                StorageUtils.set(this.#key, "0");
+            } else {
+                return;
+            }
+            MessageBoard.publishMessage("<b style='color:red'>" + this.#name + "</b>已经设置。");
+            $("#refreshButton").trigger("click");
+        });
+    }
 }
 
 export = SetupItem059;
