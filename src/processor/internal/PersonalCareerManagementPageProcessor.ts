4c6a38d49310a44df9a63114f51fdc79ae2ec465
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-15 14:00:54 +0800

    PersonalCareerManagementPageProcessor.doBindKeyboardShortcut

--- src/processor/internal/PersonalCareerManagementPageProcessor.ts
+++ src/processor/internal/PersonalCareerManagementPageProcessor.ts
@@ -1,6 +1,7 @@
 import PersonalCareerManagement from "../../core/career/PersonalCareerManagement";
 import PersonalCareerManagementPage from "../../core/career/PersonalCareerManagementPage";
 import Credential from "../../util/Credential";
+import KeyboardShortcutBuilder from "../../util/KeyboardShortcutBuilder";
 import PageUtils from "../../util/PageUtils";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
@@ -10,7 +11,14 @@ abstract class PersonalCareerManagementPageProcessor extends PageProcessorCreden
     async doProcess(credential: Credential, context?: PageProcessorContext): Promise<void> {
         const page = PersonalCareerManagement.parsePage(PageUtils.currentPageHtml());
         this.doProcessPageParsed(credential, page, context);
-        PageUtils.onEscapePressed(() => $("#returnButton").trigger("click"));
+        this.doBindKeyboardShortcut();
+    }
+
+    doBindKeyboardShortcut() {
+        new KeyboardShortcutBuilder()
+            .onEscapePressed(() => $("#returnButton").trigger("click"))
+            .withDefaultPredicate()
+            .bind();
     }
 
     abstract doProcessPageParsed(credential: Credential, page: PersonalCareerManagementPage, context?: PageProcessorContext): void;
