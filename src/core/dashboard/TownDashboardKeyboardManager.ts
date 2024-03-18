d37c0c043ee574ba0187411b7bdcf6ad7106418b
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-18 14:32:29 +0800

    TownDashboardKeyboardManager

--- src/core/dashboard/TownDashboardKeyboardManager.ts
+++ src/core/dashboard/TownDashboardKeyboardManager.ts
@@ -1,5 +1,5 @@
 import Credential from "../../util/Credential";
-import BattleFieldManager from "./BattleFieldManager";
+import KeyboardShortcutBuilder from "../../util/KeyboardShortcutBuilder";
 import TownDashboardPage from "./TownDashboardPage";
 
 class TownDashboardKeyboardManager {
@@ -8,6 +8,8 @@ class TownDashboardKeyboardManager {
     readonly #bindBattle?: boolean;
     readonly #page?: TownDashboardPage;
 
+    #optionIndex = 0;
+
     constructor(credential: Credential, bindBattle?: boolean, page?: TownDashboardPage) {
         this.#credential = credential;
         this.#bindBattle = bindBattle;
@@ -15,157 +17,65 @@ class TownDashboardKeyboardManager {
     }
 
     bind() {
-        this.#doBind(this.#credential, this.#bindBattle, this.#page);
+        new KeyboardShortcutBuilder()
+            .onKeyPressed("q", () => this.#processKeyPressed_q())
+            .onEscapePressed(() => this.#processKeyPressed_Escape())
+            .withDefaultPredicate()
+            .bind();
     }
 
-    #doBind(credential: Credential, bindBattle?: boolean, page?: TownDashboardPage) {
+    #processKeyPressed_q() {
+        if (!this.#bindBattle) {
+            return;
+        }
         let formBattle = $("form[action='battle.cgi']");
         let selectBattle = formBattle.find('select[name="level"]');
-        let optionIdx = 0;
-        $(document).off('keydown.city').on('keydown.city', event => {
-            if ($("#messageInputText:focus").length > 0) {
-                // 当前的焦点在消息框，禁用按键辅助
-                return;
-            }
-            const key = event.key;
-            if (!key) {
-                return;
-            }
-
-            if (bindBattle) {
-                if (key === "q") {
-                    if ($("#battleButton:hidden").length > 0) {
-                        // 安全战斗按钮功能开启，此时可能不显示战斗按钮
-                        // 直接进入切换战斗选项的流程
-                        let islandFound = false;
-                        $(selectBattle).find("option")
-                            .each((idx, option) => {
-                                if ($(option).text() === "秘宝之岛") {
-                                    $(option).prop("selected", true);
-                                    islandFound = true;
-                                }
-                            });
-                        if (!islandFound) {
-                            optionIdx = optionIdx === 0 ? 1 : 0;
-                            $(selectBattle).find("> option:eq(" + optionIdx + ")")
-                                .prop("selected", true);
-                        }
-                    } else {
-                        if ($("#battleButton:focus").length === 0) {
-                            $("#battleButton").trigger("focus");
-                        } else {
-                            // 如果战斗选项中有“秘宝之岛”，则恒定选择
-                            let islandFound = false;
-                            $(selectBattle).find("option")
-                                .each((idx, option) => {
-                                    if ($(option).text() === "秘宝之岛") {
-                                        $(option).prop("selected", true);
-                                        islandFound = true;
-                                    }
-                                });
-                            if (islandFound) {
-                                $("#battleButton").trigger("focus");
-                            } else {
-                                // 在option中切换
-                                optionIdx = optionIdx === 0 ? 1 : 0;
-                                $(selectBattle).find("> option:eq(" + optionIdx + ")")
-                                    .prop("selected", true);
-                                $("#battleButton").trigger("focus");
-                            }
-                        }
+        if ($("#battleButton:hidden").length > 0) {
+            // 安全战斗按钮功能开启，此时可能不显示战斗按钮
+            // 直接进入切换战斗选项的流程
+            let islandFound = false;
+            $(selectBattle).find("option")
+                .each((idx, option) => {
+                    if ($(option).text() === "秘宝之岛") {
+                        $(option).prop("selected", true);
+                        islandFound = true;
                     }
-                    return;
+                });
+            if (!islandFound) {
+                this.#optionIndex = this.#optionIndex === 0 ? 1 : 0;
+                $(selectBattle).find("> option:eq(" + this.#optionIndex + ")")
+                    .prop("selected", true);
+            }
+        } else {
+            if ($("#battleButton:focus").length === 0) {
+                $("#battleButton").trigger("focus");
+            } else {
+                // 如果战斗选项中有“秘宝之岛”，则恒定选择
+                let islandFound = false;
+                $(selectBattle).find("option")
+                    .each((idx, option) => {
+                        if ($(option).text() === "秘宝之岛") {
+                            $(option).prop("selected", true);
+                            islandFound = true;
+                        }
+                    });
+                if (islandFound) {
+                    $("#battleButton").trigger("focus");
+                } else {
+                    // 在option中切换
+                    this.#optionIndex = this.#optionIndex === 0 ? 1 : 0;
+                    $(selectBattle).find("> option:eq(" + this.#optionIndex + ")")
+                        .prop("selected", true);
+                    $("#battleButton").trigger("focus");
                 }
             }
-
-            if (key === "r") {
-                new BattleFieldManager(credential)
-                    .autoSetBattleField()
-                    .then(() => $("#refreshButton").trigger("click"));
-                return;
-            }
-
-            if (key === "s") {
-                $("option[value='ITEM_SHOP']").prop("selected", true);
-                $("#townButton").trigger("click");
-                return;
-            }
-
-            if (key === "p") {
-                $("option[value='PET_TZ']").prop("selected", true);
-                $("#townButton").trigger("click");
-                return;
-            }
-
-            if (key === "m") {
-                $("option[value='CHANGEMAP']").prop("selected", true);
-                $("#townButton").trigger("click");
-                return;
-            }
-
-            if (key === "k") {
-                $("option[value='SINGLE_BATTLE']").prop("selected", true);
-                $("#townButton").trigger("click");
-                return;
-            }
-
-
-            if (key === "g") {
-                $("option[value='PETMAP']").prop("selected", true);
-                $("#townButton").trigger("click");
-                return;
-            }
-
-            if (key === "e") {
-                $("option[value='USE_ITEM']").prop("selected", true);
-                $("#personalButton").trigger("click");
-                return;
-            }
-
-            if (key === "u") {
-                $("option[value='PETSTATUS']").prop("selected", true);
-                $("#personalButton").trigger("click");
-                return;
-            }
-
-            if (key === "z") {
-                $("option[value='CHANGE_OCCUPATION']").prop("selected", true);
-                $("#personalButton").trigger("click");
-                return;
-            }
-
-            if (key === "i") {
-                $("option[value='RANK_REMAKE']").prop("selected", true);
-                $("#personalButton").trigger("click");
-                return;
-            }
-
-            if (key === "t") {
-                $("option[value='BATTLE_MES']").prop("selected", true);
-                $("#personalButton").trigger("click");
-                return;
-            }
-
-            if (key === "b") {
-                $("option[value='BANK']").prop("selected", true);
-                $("#townButton").trigger("click");
-                return;
-            }
-
-            if (key === "x") {
-                $("option[value='LETTER']").prop("selected", true);
-                $("#personalButton").trigger("click");
-                return;
-            }
-
-            if (key === "Escape") {
-                $("option[value='INN']").prop("selected", true);
-                $("#townButton").trigger("click");
-                return;
-            }
-        });
+        }
     }
 
+    #processKeyPressed_Escape() {
+        $("option[value='INN']").prop("selected", true);
+        $("#townButton").trigger("click");
+    }
 }
 
 export = TownDashboardKeyboardManager;
