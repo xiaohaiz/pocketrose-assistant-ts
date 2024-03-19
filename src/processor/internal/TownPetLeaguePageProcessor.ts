5f9ea01af0f52f1491db7e30bf56ae658dff1bbf
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-19 11:32:37 +0800

    TownPetLeaguePageProcessor

--- src/processor/internal/TownPetLeaguePageProcessor.ts
+++ src/processor/internal/TownPetLeaguePageProcessor.ts
@@ -1,33 +1,37 @@
 import Credential from "../../util/Credential";
-import PageUtils from "../../util/PageUtils";
+import KeyboardShortcutBuilder from "../../util/KeyboardShortcutBuilder";
 import PageProcessorContext from "../PageProcessorContext";
 import PageProcessorCredentialSupport from "../PageProcessorCredentialSupport";
+import PageProcessorUtils from "../PageProcessorUtils";
 
 class TownPetLeaguePageProcessor extends PageProcessorCredentialSupport {
 
     async doProcess(credential: Credential, context?: PageProcessorContext): Promise<void> {
-        processPetLeaguePage();
-        PageUtils.onEscapePressed(() => $("#returnButton").trigger("click"));
+        await this.#processPage(credential, context);
+        new KeyboardShortcutBuilder()
+            .onEscapePressed(() => $("#returnButton").trigger("click"))
+            .withDefaultPredicate()
+            .bind();
     }
 
-}
+    async #processPage(credential: Credential, context?: PageProcessorContext) {
+        $("table:eq(1)")
+            .find("td:first")
+            .attr("id", "title_cell")
+            .removeAttr("width")
+            .removeAttr("height")
+            .removeAttr("bgcolor")
+            .css("text-align", "center")
+            .css("font-size", "150%")
+            .css("font-weight", "bold")
+            .css("background-color", "navy")
+            .css("color", "yellowgreen")
+            .text("＜＜  宠 物 联 赛 会 场  ＞＞");
 
-function processPetLeaguePage() {
-    $("table:eq(1)")
-        .find("td:first")
-        .attr("id", "title_cell")
-        .removeAttr("width")
-        .removeAttr("height")
-        .removeAttr("bgcolor")
-        .css("text-align", "center")
-        .css("font-size", "150%")
-        .css("font-weight", "bold")
-        .css("background-color", "navy")
-        .css("color", "yellowgreen")
-        .text("＜＜  宠 物 联 赛 会 场  ＞＞");
-
-    $("input:submit[value='返回城市']")
-        .attr("id", "returnButton");
+        $("input:submit[value='返回城市']")
+            .attr("id", "returnButton")
+            .val(PageProcessorUtils.generateReturnTownButtonTitle(context) + "(Esc)");
+    }
 }
 
 export = TownPetLeaguePageProcessor;
