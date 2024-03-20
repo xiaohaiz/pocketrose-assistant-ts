fea9281b5a865304bf45d4eb491db9f7be673fc3
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-20 17:16:30 +0800

    TownPersonalChampionPageProcessor.#renderCandidates

--- src/processor/internal/TownPersonalChampionPageProcessor.ts
+++ src/processor/internal/TownPersonalChampionPageProcessor.ts
@@ -1,6 +1,9 @@
 import _ from "lodash";
+import PersonalChampionRole from "../../core/champion/PersonalChampionRole";
 import TownPersonalChampionPage from "../../core/champion/TownPersonalChampionPage";
 import TownPersonalChampionPageParser from "../../core/champion/TownPersonalChampionPageParser";
+import TownInformation from "../../core/dashboard/TownInformation";
+import TownStatus from "../../core/town/TownStatus";
 import Credential from "../../util/Credential";
 import KeyboardShortcutBuilder from "../../util/KeyboardShortcutBuilder";
 import PageUtils from "../../util/PageUtils";
@@ -11,7 +14,7 @@ class TownPersonalChampionPageProcessor extends PageProcessorCredentialSupport {
 
     async doProcess(credential: Credential, context?: PageProcessorContext): Promise<void> {
         const page = await new TownPersonalChampionPageParser().parse(PageUtils.currentPageHtml());
-        this.#processPage(page)
+        await this.#processPage(page)
             .then(() => {
                 KeyboardShortcutBuilder.newInstance()
                     .onEscapePressed(() => $("#returnButton").trigger("click"))
@@ -37,20 +40,83 @@ class TownPersonalChampionPageProcessor extends PageProcessorCredentialSupport {
         $("input:submit[value='返回城市']")
             .attr("id", "returnButton");
 
-        await this.#renderWinners(page);
-    }
+        $("th:contains('比武对手一览')")
+            .filter((_idx, th) => {
+                const s = $(th).text();
+                return _.startsWith(s, "比武对手一览");
+            })
+            .closest("table")
+            .parent()
+            .parent()
+            .parent()
+            .parent()
+            .attr("id", "candidates")
+            .removeAttr("align")
+            .removeAttr("cellpadding")
+            .removeAttr("cellspacing")
+            .removeAttr("border")
+            .css("background-color", "#888888")
+            .css("text-align", "center");
 
-    async #renderWinners(page: TownPersonalChampionPage) {
         $("th:contains('-- 历 代 优 胜 者 --')")
             .filter((_idx, th) => {
                 const s = $(th).text();
                 return _.startsWith(s, "-- 历 代 优 胜 者 --");
             })
             .closest("table")
-            .attr("id", "winners");
+            .attr("id", "winners")
+            .removeAttr("cellspacing")
+            .removeAttr("bgcolor")
+            .css("background-color", "#888888");
+
+        await this.#renderCandidates(page);
+        await this.#renderWinners(page);
+    }
+
+    async #renderCandidates(page: TownPersonalChampionPage) {
+        const roles = new Map<string, PersonalChampionRole>();
+        for (const role of page.candidates!) {
+            roles.set(role.townName!, role);
+        }
+        const townInformationPage = await new TownInformation().open();
+        let maxSize = 0;
+        const countries = townInformationPage.countries;
+        for (const c of countries) {
+            const list = townInformationPage.getTownList(c);
+            maxSize = _.max([maxSize, list.length])!;
+        }
 
         let html = "";
-        html += "<table style='border-width:0;margin:auto'></table>";
+        html += "<table style='border-width:0;margin:auto'>";
+        html += "<tbody>";
+        for (const c of countries) {
+            const list = townInformationPage.getTownList(c);
+            html += "<tr>";
+            html += "<th style='background-color:black;color:white;white-space:nowrap'>";
+            html += (c === "") ? "在野" : c;
+            html += "</th>";
+            for (let i = 0; i < maxSize; i++) {
+                let status: TownStatus | undefined = undefined;
+                if (i < list.length) {
+                    status = list[i];
+                }
+                html += "<td>";
+                html += this.#generateCandidate(status, roles);
+                html += "</td>";
+            }
+            html += "</tr>";
+        }
+        html += "</tbody>";
+        html += "</table>";
+
+        $("#candidates")
+            .find("> tbody:first")
+            .html("<tr><td>" + html + "</td></tr>");
+    }
+
+    async #renderWinners(page: TownPersonalChampionPage) {
+        let html = "";
+        html += "<table style='border-width:0;margin:auto'>";
         html += "<tbody>";
         html += "<tr>";
         html += "<th colspan='" + page.winners!.length + "' " +
@@ -69,11 +135,37 @@ class TownPersonalChampionPageProcessor extends PageProcessorCredentialSupport {
         html += "</table>";
 
         $("#winners")
-            .removeAttr("cellspacing")
-            .removeAttr("bgcolor")
-            .css("background-color", "#888888")
             .find("> tbody:first")
             .html("<tr><td>" + html + "</td></tr>");
+    }
+
+    #generateCandidate(status: TownStatus | undefined, roles: Map<string, PersonalChampionRole>): string {
+        let role: PersonalChampionRole | undefined = undefined;
+        if (status) {
+            role = roles.get(status.name!);
+        }
+        let html = "";
+        html += "<table style='border-width:0;background-color:#F8F0E0;text-align:center'>";
+        html += "<tbody>";
+        html += "<tr>";
+        html += "<th style='width:80px;background-color:skyblue;white-space:nowrap'>";
+        if (status) {
+            html += status.name;
+        } else {
+            html += "&nbsp;";
+        }
+        html += "</th>";
+        html += "</tr>";
+        html += "<tr>";
+        html += "<td style='width:80px;height:64px'>";
+        if (role) {
+            html += role.imageHtml;
+        }
+        html += "</td>";
+        html += "</tr>";
+        html += "</tbody>";
+        html += "</table>";
+        return html;
     }
 }
 
