aecadda9c362ee3b04c04980d983fc753616d032
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-20 16:07:43 +0800

    PersonalChampionRole.imageHtml

--- src/core/champion/PersonalChampionRole.ts
+++ src/core/champion/PersonalChampionRole.ts
@@ -1,9 +1,15 @@
+import Constants from "../../util/Constants";
+
 class PersonalChampionRole {
 
     name?: string;
     image?: string;
     townName?: string;
 
+    get imageHtml(): string {
+        const src = Constants.POCKET_DOMAIN + "/image/head/" + this.image;
+        return "<img src='" + src + "' alt='" + this.name + "' width='64' height='64'>";
+    }
 }
 
 export = PersonalChampionRole;
