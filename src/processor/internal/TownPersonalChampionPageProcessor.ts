75fe905b467e21f6c24fca30ebbdc988c520e47b
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-20 16:27:50 +0800

    TownPersonalChampionPageProcessor.#renderWinners

--- src/processor/internal/TownPersonalChampionPageProcessor.ts
+++ src/processor/internal/TownPersonalChampionPageProcessor.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+import TownPersonalChampionPage from "../../core/champion/TownPersonalChampionPage";
 import TownPersonalChampionPageParser from "../../core/champion/TownPersonalChampionPageParser";
 import Credential from "../../util/Credential";
 import KeyboardShortcutBuilder from "../../util/KeyboardShortcutBuilder";
@@ -9,32 +11,77 @@ class TownPersonalChampionPageProcessor extends PageProcessorCredentialSupport {
 
     async doProcess(credential: Credential, context?: PageProcessorContext): Promise<void> {
         const page = await new TownPersonalChampionPageParser().parse(PageUtils.currentPageHtml());
-
-        processPersonalChampionPage();
-        KeyboardShortcutBuilder.newInstance()
-            .onEscapePressed(() => $("#returnButton").trigger("click"))
-            .withDefaultPredicate()
-            .bind();
+        this.#processPage(page)
+            .then(() => {
+                KeyboardShortcutBuilder.newInstance()
+                    .onEscapePressed(() => $("#returnButton").trigger("click"))
+                    .withDefaultPredicate()
+                    .bind();
+            });
     }
 
-}
+    async #processPage(page: TownPersonalChampionPage) {
+        $("table:eq(1)")
+            .find("td:first")
+            .attr("id", "pageTitle")
+            .removeAttr("width")
+            .removeAttr("height")
+            .removeAttr("bgcolor")
+            .css("text-align", "center")
+            .css("font-size", "150%")
+            .css("font-weight", "bold")
+            .css("background-color", "navy")
+            .css("color", "yellowgreen")
+            .text("＜＜  个 人 天 真  ＞＞");
 
-function processPersonalChampionPage() {
-    $("table:eq(1)")
-        .find("td:first")
-        .attr("id", "title_cell")
-        .removeAttr("width")
-        .removeAttr("height")
-        .removeAttr("bgcolor")
-        .css("text-align", "center")
-        .css("font-size", "150%")
-        .css("font-weight", "bold")
-        .css("background-color", "navy")
-        .css("color", "yellowgreen")
-        .text("＜＜  个 人 天 真  ＞＞");
+        $("input:submit[value='返回城市']")
+            .attr("id", "returnButton");
 
-    $("input:submit[value='返回城市']")
-        .attr("id", "returnButton");
+        await this.#renderWinners(page);
+    }
+
+    async #renderWinners(page: TownPersonalChampionPage) {
+        $("th:contains('-- 历 代 优 胜 者 --')")
+            .filter((_idx, th) => {
+                const s = $(th).text();
+                return _.startsWith(s, "-- 历 代 优 胜 者 --");
+            })
+            .closest("table")
+            .attr("id", "winners");
+
+        let html = "";
+        html += "<table style='border-width:0;margin:auto'></table>";
+        html += "<tbody>";
+        html += "<tr>";
+        html += "<th colspan='" + page.winners!.length + "' " +
+            "style='background-color:#E0D0B0;color:red;font-size:120%'>";
+        html += "-- 历 代 优 胜 者 --";
+        html += "</th>";
+        html += "</tr>";
+        html += "<tr>";
+        for (const winner of page.winners!) {
+            html += "<td style='background-color:#F8F0E0;width:64px'>";
+            html += winner.imageHtml;
+            html += "</td>";
+        }
+        html += "</tr>";
+        html += "<tr>";
+        for (const winner of page.winners!) {
+            html += "<th style='background-color:#F8F0E0;width:64px;text-align:center'>";
+            html += winner.name;
+            html += "</th>";
+        }
+        html += "</tr>";
+        html += "</tbody>";
+        html += "</table>";
+
+        $("#winners")
+            .removeAttr("cellspacing")
+            .removeAttr("bgcolor")
+            .css("background-color", "#888888")
+            .find("> tbody:first")
+            .html("<tr><td>" + html + "</td></tr>");
+    }
 }
 
 export = TownPersonalChampionPageProcessor;
