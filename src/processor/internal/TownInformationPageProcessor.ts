5495bcd2b575e6cb76b291eb7626facd1bc3350d
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-20 11:54:41 +0800

    可以查看城市详情。

--- src/processor/internal/TownInformationPageProcessor.ts
+++ src/processor/internal/TownInformationPageProcessor.ts
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import TownInformation from "../../core/dashboard/TownInformation";
+import TownInformationPage from "../../core/dashboard/TownInformationPage";
 import MapBuilder from "../../core/map/MapBuilder";
 import Town from "../../core/town/Town";
 import TownLoader from "../../core/town/TownLoader";
@@ -16,7 +17,6 @@ class TownInformationPageProcessor implements PageProcessor {
         PageUtils.fixCurrentPageBrokenImages();
         PageUtils.removeUnusedHyperLinks();
         PageUtils.removeGoogleAnalyticsScript();
-        doProcess();
         this.#processPage().then();
     }
 
@@ -33,6 +33,9 @@ class TownInformationPageProcessor implements PageProcessor {
         html += "<tr>";
         html += "<td id='map' style='background-color:#F8F0E0'></td>";
         html += "<td id='town' style='background-color:#F8F0E0;width:100%;text-align:center;vertical-align:center'></td>";
+        html += "</tr>";
+        html += "<tr>";
+        html += "<td id='command' style='background-color:#F8F0E0;text-align:center' colspan='2'></td>";
         html += "</tr>";
         html += "<tr>";
         html += "<td id='townList' style='background-color:#F8F0E0;text-align:center' colspan='2'></td>";
@@ -62,6 +65,8 @@ class TownInformationPageProcessor implements PageProcessor {
                 })
                 .on("mouseleave", () => $("#town").html(""));
         }
+
+        this.#renderTownList(page);
     }
 
     #generateTown(town: Town, status: TownStatus): string {
@@ -164,17 +169,72 @@ class TownInformationPageProcessor implements PageProcessor {
         html += "</table>";
         return html;
     }
-}
 
-function doProcess() {
-    $("td:contains('枫丹')")
-        .filter(function () {
-            return $(this).text() === "枫丹";
-        })
-        .next()
-        .next()
-        .find("font:first")
-        .text("- GOLD");
+    #renderTownList(page: TownInformationPage) {
+        $(".townStatusButton").off("click");
+        let html = "";
+        html += "<table style='background-color:#888888;margin:auto;border-width:0;width:240px'>";
+        html += "<tbody style='background-color:#F8F0E0;white-space:nowrap'>";
+        html += "<tr style='background-color:skyblue;text-align:center'>";
+        html += "<th>国家</th>"
+        html += "<th>首都</th>"
+        html += "<th>城市</th>"
+        html += "<th>坐标</th>"
+        html += "<th>属性</th>"
+        html += "<th>收益</th>"
+        html += "<th>查看</th>"
+        html += "</tr>";
+        for (const c of page.countries) {
+            const townList = page.getTownList(c);
+            for (let i = 0; i < townList.length; i++) {
+                const status = townList[i];
+                const town = TownLoader.load(status.name)!;
+                html += "<tr>";
+                if (i === 0) {
+                    html += "<th rowspan='" + (townList.length) + "' " +
+                        "style='background-color:black;vertical-align:center;color:white'>";
+                    html += (status.country === "" ? "在野" : status.country);
+                    html += "</th>";
+                }
+                html += "<td>";
+                if (status.capital) {
+                    html += "★";
+                }
+                html += "</td>";
+                html += "<td style='background-color:" + status.bgcolor + ";color:" + status.color + "'>";
+                html += status.name;
+                html += "</td>";
+                html += "<td style='background-color:" + status.bgcolor + ";color:" + status.color + "'>";
+                html += town.coordinate.asText();
+                html += "</td>";
+                html += "<td style='background-color:" + status.bgcolor + ";color:" + status.color + "'>";
+                html += status.attribute;
+                html += "</td>";
+                html += "<td style='background-color:" + status.bgcolor + ";color:" + status.color + "'>";
+                if (status.country === "") {
+                    html += "PRIVACY";
+                } else {
+                    html += (status.tax + " GOLD");
+                }
+                html += "</td>";
+                html += "<td style='background-color:" + status.bgcolor + ";color:" + status.color + "'>";
+                html += "<input type='button' id='town_" + town.id + "' class='townStatusButton' value='详情'>"
+                html += "</td>";
+                html += "</tr>";
+            }
+        }
+        html += "</tbody>";
+        html += "</table>";
+        $("#townList").html(html);
+        $(".townStatusButton").on("click", event => {
+            const buttonId = $(event.target).attr("id") as string;
+            const townId = StringUtils.substringAfter(buttonId, "_");
+            const town = TownLoader.load(townId)!;
+            const status = page.findByName(town.name)!;
+            $("#town").html(this.#generateTown(town, status));
+            PageUtils.scrollIntoView("town");
+        });
+    }
 }
 
 export = TownInformationPageProcessor;
