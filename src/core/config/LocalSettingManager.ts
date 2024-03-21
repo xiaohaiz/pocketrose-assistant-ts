5ec8206fc25fac39599d4cfa4253c46603f820c9
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-21 16:10:21 +0800

    LocalSettingManager

--- src/core/config/LocalSettingManager.ts
+++ src/core/config/LocalSettingManager.ts
@@ -1,7 +1,14 @@
 import StorageUtils from "../../util/StorageUtils";
 
 /**
+ * ----------------------------------------------------------------------------
  * 基于LocalStorage的设置管理。
+ * ----------------------------------------------------------------------------
+ * _ts_001       : 是否包含编外人员
+ * _ts_002_${id} : 是否提醒武器满级
+ * _ts_003_${id} : 是否提醒防具满级
+ * _ts_004_${id} : 是否提醒饰品满级
+ * ----------------------------------------------------------------------------
  */
 class LocalSettingManager {
 
@@ -11,6 +18,30 @@ class LocalSettingManager {
 
     static setIncludeExternal(value: boolean) {
         StorageUtils.set("_ts_001", Number(value).toString());
+    }
+
+    static isWeaponExperienceMax(id: string) {
+        return StorageUtils.getBoolean("_ts_002_" + id);
+    }
+
+    static setWeaponExperienceMax(id: string, value: boolean) {
+        StorageUtils.set("_ts_002_" + id, Number(value).toString());
+    }
+
+    static isArmorExperienceMax(id: string) {
+        return StorageUtils.getBoolean("_ts_003_" + id);
+    }
+
+    static setArmorExperienceMax(id: string, value: boolean) {
+        StorageUtils.set("_ts_003_" + id, Number(value).toString());
+    }
+
+    static isAccessoryExperienceMax(id: string) {
+        return StorageUtils.getBoolean("_ts_004_" + id);
+    }
+
+    static setAccessoryExperienceMax(id: string, value: boolean) {
+        StorageUtils.set("_ts_004_" + id, Number(value).toString());
     }
 
 }
