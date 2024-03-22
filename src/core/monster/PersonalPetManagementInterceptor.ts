ef5fb14a52e095c3202ad21211485d35ae4c9979
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-22 20:09:51 +0800

    PersonalPetManagementInterceptor.#role

--- src/core/monster/PersonalPetManagementInterceptor.ts
+++ src/core/monster/PersonalPetManagementInterceptor.ts
@@ -2,13 +2,20 @@ import Credential from "../../util/Credential";
 import PersonalPetManagement from "./PersonalPetManagement";
 import LocalSettingManager from "../config/LocalSettingManager";
 import BattleFieldManager from "../battle/BattleFieldManager";
+import Role from "../role/Role";
 
 class PersonalPetManagementInterceptor {
 
     readonly #credential: Credential;
+    #role?: Role;
 
     constructor(credential: Credential) {
         this.#credential = credential;
+    }
+
+    withRole(value: Role | undefined): PersonalPetManagementInterceptor {
+        this.#role = value;
+        return this;
     }
 
     async beforeExitPersonalPetManagement() {
@@ -19,7 +26,10 @@ class PersonalPetManagementInterceptor {
         LocalSettingManager.setPetCapacityMax(this.#credential.id, (petCount === 3));
 
         // 宠物状态有可能发生改变，有可能触发智能战斗场所切换
-        await new BattleFieldManager(this.#credential).withPetPage(page).autoSetBattleField();
+        await new BattleFieldManager(this.#credential)
+            .withRole(this.#role)
+            .withPetPage(page)
+            .autoSetBattleField();
     }
 }
 
