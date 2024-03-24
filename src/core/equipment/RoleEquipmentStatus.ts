884140f290cf52a57baf924fe018e5381b0b0ca1
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-24 22:47:27 +0800

    RoleEquipmentStatus.asDocument

--- src/core/equipment/RoleEquipmentStatus.ts
+++ src/core/equipment/RoleEquipmentStatus.ts
@@ -4,6 +4,17 @@ class RoleEquipmentStatus {
     json?: string;
     updateTime?: number;
 
+    asDocument() {
+        const document: any = {};
+        (this.id) && (document.id = this.id);
+        (this.json) && (document.json = this.json);
+        if (this.updateTime !== undefined) {
+            document.updateTime = this.updateTime;
+        } else {
+            document.updateTime = new Date().getTime();
+        }
+        return document;
+    }
 }
 
 export = RoleEquipmentStatus;
