d74caa63565d280877bf9a4d382ea7affb12fc01
xiaohaiz <xiaohaiz@hotmail.com>
2024-03-27 11:26:43 +0800

    口袋助手接管分身试管页面。

---
+++ src/interceptor/internal/PersonalMirrorPageInterceptor.ts
@@ -0,0 +1,33 @@
+import RoleStateMachineManager from "../../core/state/RoleStateMachineManager";
+import PersonalMirrorPageProcessor from "../../processor/internal/PersonalMirrorPageProcessor";
+import PageProcessorContext from "../../processor/PageProcessorContext";
+import PageInterceptor from "../PageInterceptor";
+
+class PersonalMirrorPageInterceptor implements PageInterceptor {
+
+    readonly #processor = new PersonalMirrorPageProcessor();
+
+    accept(cgi: string, pageText: string): boolean {
+        if (cgi === "mydata.cgi") {
+            return pageText.includes("* 分身试管 *");
+        }
+        return false;
+    }
+
+    intercept(): void {
+        RoleStateMachineManager.create()
+            .load()
+            .then(machine => {
+                machine.start()
+                    .whenInTown(state => {
+                        const context = new PageProcessorContext()
+                            .withTownId(state?.townId);
+                        this.#processor.process(context);
+                    })
+                    .process();
+            });
+    }
+
+}
+
+export = PersonalMirrorPageInterceptor;
